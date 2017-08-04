3b1f19d187477a29e54d5c78cb5f5d2ba060282a
Joshua Letellier <slimjim86me@yahoo.com>
2017-08-04 15:19:29 -0700

    Added Mystic Grotto to the Safe Zones

--- Monumenta/src/pe/project/locations/safezones/SafeZoneConstants.java
+++ Monumenta/src/pe/project/locations/safezones/SafeZoneConstants.java
@@ -27,33 +27,37 @@ public class SafeZoneConstants {
 		Hermy(13),
 		
 		Monument(14),
+		
+		MysticGrotto(15),
 
-		Total(15);
+		Total(16);
 		
 		public int mValue;
 		private SafeZones(int value)	{	this.mValue = value;	}
 	}
 	
 	static final AreaBounds[] mSafeZoneBounds = {
-			new AreaBounds("Capital", new Point(-1130, 0,-284), new Point(-498, 256, 344)),			//	Capital
-			new AreaBounds("Nyr", new Point(-179, 0, -166), new Point(-79, 256, 14)),				//	Nyr
-			new AreaBounds("Farr", new Point(538, 0, 100), new Point(655, 256, 229)),				//	Farr
-			new AreaBounds("Highwatch", new Point(1131, 0, -156), new Point(1221, 256, -76)),		//	Highwatch
+			new AreaBounds("Capital", new Point(-1130, 0,-284), new Point(-498, 256, 344)),		//	Capital
+			new AreaBounds("Nyr", new Point(-179, 0, -166), new Point(-79, 256, 14)),		//	Nyr
+			new AreaBounds("Farr", new Point(538, 0, 100), new Point(655, 256, 229)),		//	Farr
+			new AreaBounds("Highwatch", new Point(1131, 0, -156), new Point(1221, 256, -76)),	//	Highwatch
 			
-			new AreaBounds("", new Point(136, 53, -186), new Point(176, 83, -120)),					//	White Wool Lobby
-			new AreaBounds("", new Point(27, 64, 164), new Point(67, 94, 229)),						//	Orange Wool Lobby
-			new AreaBounds("", new Point(453, 12, 5), new Point(493, 42, 70)),						//	Magenta Wool Lobby
-			new AreaBounds("", new Point(770, 76, -366), new Point(810, 106, -301)),				//	Light Blue Wool Lobby
-			new AreaBounds("", new Point(1141, 39, 3), new Point(1181, 69, 68)),					//	Yellow Wool Lobby
-			new AreaBounds("", new Point(295, 10, -163), new Point(335, 40, -98)),					//	Bonus Wool Lobby
+			new AreaBounds("", new Point(136, 53, -186), new Point(176, 83, -120)),			//	White Wool Lobby
+			new AreaBounds("", new Point(27, 64, 164), new Point(67, 94, 229)),			//	Orange Wool Lobby
+			new AreaBounds("", new Point(453, 12, 5), new Point(493, 42, 70)),			//	Magenta Wool Lobby
+			new AreaBounds("", new Point(770, 76, -366), new Point(810, 106, -301)),		//	Light Blue Wool Lobby
+			new AreaBounds("", new Point(1141, 39, 3), new Point(1181, 69, 68)),			//	Yellow Wool Lobby
+			new AreaBounds("", new Point(295, 10, -163), new Point(335, 40, -98)),			//	Bonus Wool Lobby
 			
-			new AreaBounds("", new Point(-1584, 0, -1632), new Point(-1329, 255, -1377)),			//	Commands
+			new AreaBounds("", new Point(-1584, 0, -1632), new Point(-1329, 255, -1377)),		//	Commands
 			
-			new AreaBounds("", new Point(1505, 102, -178), new Point(1631, 256, -16)),				// Siege Of Highwatch
-			new AreaBounds("", new Point(232, 10, 294), new Point(249, 256, 318)),					// Ctaz
-			new AreaBounds("", new Point(-331, 86, 334), new Point(-310, 110, 355)),				// Hermy
+			new AreaBounds("", new Point(1505, 102, -178), new Point(1631, 256, -16)),		//	Siege Of Highwatch
+			new AreaBounds("", new Point(232, 10, 294), new Point(249, 256, 318)),			//	Ctaz
+			new AreaBounds("", new Point(-331, 86, 334), new Point(-310, 110, 355)),		//	Hermy
 			
-			new AreaBounds("Monument", new Point(1160, 0, -320), new Point(1400, 256, -115)),		//	Monument
+			new AreaBounds("Monument", new Point(1160, 0, -320), new Point(1400, 256, -115)),	//	Monument
+	
+			new AreaBounds("", new Point(317, 61, 309), new Point(383, 106, 392)),			//	Mystic Grotto
 	};
 	
 	public static AreaBounds getSafeZone(SafeZones city) {
