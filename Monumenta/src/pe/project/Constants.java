0359539c5bc461abb27ca11046af58381abd4465
Byron Marohn <combustible@live.com>
2017-08-05 20:39:12 -0700

    Tele strike'd players to RESET instead, moved reset up 4 blocks
    Signed-off-by: Byron Marohn <combustible@live.com>

--- Monumenta/src/pe/project/Constants.java
+++ Monumenta/src/pe/project/Constants.java
@@ -10,17 +10,16 @@ public class Constants {
 	public static final int HALF_TICKS_PER_SECOND = (int)(TICKS_PER_SECOND / 2.0);
 	public static final int QUARTER_TICKS_PER_SECOND = (int)(HALF_TICKS_PER_SECOND / 2.0);
 	public static final int TICKS_PER_MINUTE = TICKS_PER_SECOND * 60;
-	
+
 	public static final int TWO_MINUTES = TICKS_PER_MINUTE * 2;
 	public static final int THIRTY_SECONDS = TICKS_PER_SECOND * 30;
-	
-	
+
+
 	public static final int FIVE_MINUTES = TICKS_PER_MINUTE * 5;
 	public static final int TEN_MINUTES = TICKS_PER_MINUTE * 10;
-	
-	public static final Point SPAWN_POINT = new Point(-734, 107, 50);
-	public static final Point RESET_POINT = new Point(-1450, 237, -1498);
-	
+
+	public static final Point RESET_POINT = new Point(-1450, 241, -1498);
+
 	public static final PotionEffect CAPITAL_SPEED_EFFECT = new PotionEffect(PotionEffectType.SPEED, TICKS_PER_SECOND, 1, true, false);
 	public static final PotionEffect CITY_SATURATION_EFFECT = new PotionEffect(PotionEffectType.SATURATION, TICKS_PER_SECOND, 1, true, false);
 	public static final PotionEffect CITY_RESISTENCE_EFFECT = new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, TICKS_PER_SECOND, 4, true, false);
--- Monumenta/src/pe/project/utils/PlayerUtils.java
+++ Monumenta/src/pe/project/utils/PlayerUtils.java
@@ -19,43 +19,43 @@ public class PlayerUtils {
         player.getLocation().getBlock().getType() != Material.LADDER &&
         player.getLocation().getBlock().getType() != Material.VINE;
 	}
-	
+
 	public static boolean hasLineOfSight(Player player, LivingEntity mob) {
 		//	We want to check against two things since the players view isn't square,
 		//	first we'll test against the X (Yaw).
 		final double xDotMax = 0.65;
-		
+
 		Vector forwardX = player.getEyeLocation().getDirection().setY(0).normalize();
 		Vector toMobX = mob.getEyeLocation().subtract(player.getEyeLocation()).toVector().setY(0).normalize();
-		
+
 		double xz = toMobX.dot(forwardX);
 		boolean yawLos = xz > xDotMax;
-		
+
 		//	Then we'll test against the Y (Pitch)
 		final double yDotMax = 0.70;
 		final double yDotMin = -0.55;
-		
+
 		Vector forwardY = player.getEyeLocation().getDirection().normalize();
 		Vector toMobY = mob.getEyeLocation().subtract(player.getEyeLocation()).toVector().normalize();
-		
+
 		double valY = toMobY.subtract(forwardY).getY();
-		
+
 		boolean pitchLos = valY < yDotMax && valY > yDotMin;
-		
+
 		return yawLos && pitchLos;
 	}
-	
+
 	public static void awardStrike(Player player, String reason) {
 		int strikes = ScoreboardUtils.getScoreboardValue(player, "Strikes");
 		strikes++;
 		ScoreboardUtils.setScoreboardValue(player, "Strikes", strikes);
-		
+
 		Location loc = player.getLocation();
 		String OOBLoc = "[" + (int)loc.getX() + ", " + (int)loc.getY() + ", " + (int)loc.getZ() + "]";
-		
+
 		player.sendMessage(ChatColor.RED + "You've recieved a strike for "  + reason + " Location " + OOBLoc);
 		player.sendMessage(ChatColor.YELLOW + "If you feel that this strike is unjustified feel free to send a message and screenshot of this to a moderator on the Discord.");
-		
-		player.teleport(new Location(player.getWorld(), Constants.SPAWN_POINT.mX, Constants.SPAWN_POINT.mY, Constants.SPAWN_POINT.mZ));
+
+		player.teleport(new Location(player.getWorld(), Constants.RESET_POINT.mX, Constants.RESET_POINT.mY, Constants.RESET_POINT.mZ));
 	}
 }
