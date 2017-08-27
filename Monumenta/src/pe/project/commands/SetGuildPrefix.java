aa9e9976fe257620b5f3aa20fd57e9b22a1cd498
Byron Marohn <combustible@live.com>
2017-08-27 12:46:53 -0700

    Improve guild color name handling
    Signed-off-by: Byron Marohn <combustible@live.com>

--- Monumenta/src/pe/project/commands/SetGuildPrefix.java
+++ Monumenta/src/pe/project/commands/SetGuildPrefix.java
@@ -1,5 +1,6 @@
 package pe.project.commands;
 
+import java.util.Arrays;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -9,6 +10,9 @@ import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
 public class SetGuildPrefix implements CommandExecutor {
+	public static String[] getEnumNames(Class<? extends Enum<?>> e) {
+		return Arrays.stream(e.getEnumConstants()).map(Enum::name).toArray(String[]::new);
+	}
 
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
@@ -23,30 +27,54 @@ public class SetGuildPrefix implements CommandExecutor {
     	} else if (arg3[1].length() > 7) {
     		arg0.sendMessage(ChatColor.RED + "Guild Tag is too long!");
     	}
-		
+
 		Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
 		if (scoreboard != null) {
 			Team team = scoreboard.getTeam(arg3[0]);
 			if (team != null) {
 				if (arg3[2] != null) {
-					ChatColor color = ChatColor.valueOf(arg3[2]);
-					if (color != null) {
-						String second = "";
+					ChatColor tagColor;
+					/* Get the tag color or print an error if it is not a valid color */
+					try {
+						tagColor = ChatColor.valueOf(arg3[2]);
+					} catch (IllegalArgumentException e) {
+						arg0.sendMessage(ChatColor.RED + "Invalid tag color! Available colors are:");
+						for (String s: getEnumNames(ChatColor.class)) {
+							arg0.sendMessage(ChatColor.RED + "- " + s);
+						}
+						tagColor = null;
+					}
+
+					if (tagColor != null) {
+						String nameColorString = "";
+
+						/* Get the name color or print an error if it is not a valid color */
 						if (arg3.length > 3 && arg3[3] != null) {
-							ChatColor secondColor = ChatColor.valueOf(arg3[3]);
-							if (secondColor != null) {
-								second = "" + secondColor;
+							ChatColor nameColor;
+							try {
+								nameColor = ChatColor.valueOf(arg3[3]);
+							} catch (IllegalArgumentException e) {
+								arg0.sendMessage(ChatColor.RED + "Invalid player color! Available colors are:");
+								for (String s: getEnumNames(ChatColor.class)) {
+									arg0.sendMessage(ChatColor.RED + "- " + s);
+								}
+								nameColor = null;
+							}
+
+							if (nameColor != null) {
+								nameColorString = "" + nameColor;
 							}
 						}
-						
-						team.setPrefix(color + arg3[1] + ChatColor.RESET + second + " ");
+
+						team.setPrefix(tagColor + arg3[1] + ChatColor.RESET + nameColorString + " ");
+						team.setSuffix("" + ChatColor.RESET);
 					}
 				}
 			}
 		} else {
 			arg0.sendMessage(ChatColor.RED + "No team with this name!");
 		}
-		
+
 		return true;
 	}
 
