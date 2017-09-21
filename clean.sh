e02f3cd1f5eee24f44aaa475354e67a9049a1314
Byron Marohn <combustible@live.com>
2017-09-20 23:27:49 -0700

    Improved command-line ant script with optional PMD warnings
    Signed-off-by: Byron Marohn <combustible@live.com>

--- clean.sh
+++ clean.sh
@@ -2,14 +2,14 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR/Monumenta"
-ant pmd build jar
+ant clean
 retcode=$?
 if [[ $retcode -ne 0 ]]; then
 	exit $retcode
 fi
 
 cd "$SCRIPT_DIR/BungeeCord"
-ant pmd build jar
+ant clean
 retcode=$?
 if [[ $retcode -ne 0 ]]; then
 	exit $retcode
