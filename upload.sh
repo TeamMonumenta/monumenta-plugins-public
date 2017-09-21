f9cc8578561d12a17dfb9413c0aee7c0c1126039
Byron Marohn <combustible@live.com>
2017-09-20 23:28:16 -0700

    Added scripts to automate uploading compiled jars
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ upload.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+bungee_version="$(ls Monumenta-Bungee-Plugins*.jar | sed -e 's|^.*_\([0-9]*-[0-9]*\).jar|\1|g' | sort -n -r | head -n 1)"
+if [[ -z "$bungee_version" ]]; then
+	exit 1
+fi
+bungee="$(ls -r Monumenta-Bungee-Plugins*${bungee_version}.jar | head -n 1)"
+
+monumenta_version="$(ls Monumenta-Plugins*.jar | sed -e 's|^.*_\([0-9]*-[0-9]*\).jar|\1|g' | sort -n -r | head -n 1)"
+if [[ -z "$monumenta_version" ]]; then
+	exit 1
+fi
+monumenta="$(ls -r Monumenta-Plugins*${monumenta_version}.jar | head -n 1)"
+
+echo "Bungee version: $bungee"
+echo "Monumenta version: $monumenta"
+
+scp -P 9922 $bungee rock@50.39.100.13:/home/rock/project_epic/server_config/plugins/
+scp -P 9922 $monumenta rock@50.39.100.13:/home/rock/project_epic/server_config/plugins/
+ssh -p 9922 rock@50.39.100.13 "cd /home/rock/project_epic/server_config/plugins && rm -f Monumenta-Bungee-Plugins.jar ; ln -s $bungee Monumenta-Bungee-Plugins.jar"
+ssh -p 9922 rock@50.39.100.13 "cd /home/rock/project_epic/server_config/plugins && rm -f Monumenta-Plugins.jar ; ln -s $monumenta Monumenta-Plugins.jar"
+
+
