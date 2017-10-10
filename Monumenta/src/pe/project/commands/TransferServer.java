885d366c8799a2d180b67a765b379d385214bf88
Byron Marohn <combustible@live.com>
2017-10-09 22:11:56 -0700

    Transferserver no longer takes coords/scoreboard, has better messages

--- Monumenta/src/pe/project/commands/TransferServer.java
+++ Monumenta/src/pe/project/commands/TransferServer.java
@@ -8,9 +8,6 @@ import org.bukkit.entity.Player;
 
 import org.bukkit.ChatColor;
 import pe.project.Main;
-import pe.project.point.AreaBounds;
-import pe.project.utils.CommandUtils;
-import pe.project.utils.ScoreboardUtils;
 import pe.project.utils.NetworkUtils;
 
 //	/transferserver <server name> <x1> <y1> <z1> <x2> <y2> <z2>
@@ -24,100 +21,56 @@ public class TransferServer implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String arg2, String[] arg3) {
-		String scoreName = null;
-		int scoreMin = 0;
-		int scoreMax = 0;
-		boolean sendPlayerStuff = true;
 
-		if (arg3.length != 1 && arg3.length != 8 && arg3.length != 11) {
+		if (arg3.length != 1 && arg3.length != 2) {
 			sender.sendMessage(ChatColor.RED + "Invalid number of parameters!");
 			return false;
 		}
 
 		String server = arg3[0];
 
-		if (arg3.length == 1) {
-			if (sender instanceof Player) {
-				// Sender is requesting transfer to destination server with equipment
-				try {
-					sender.sendMessage("Transferring with playerdata to " + server);
-					NetworkUtils.transferPlayerData(mMain, (Player)sender, server);
-					return true;
-				} catch (Exception e) {
-					sender.sendMessage("Caught exception when transferring players");
-					return false;
-				}
-			} else if (sender instanceof ProxiedCommandSender) {
-				CommandSender callee = ((ProxiedCommandSender)sender).getCallee();
-				CommandSender caller = ((ProxiedCommandSender)sender).getCaller();
-				if (callee instanceof Player) {
-					// Sender is an /execute command targeting a player
-					try {
-						caller.sendMessage("Transferring " + callee.getName() + " with playerdata to " + server);
-						NetworkUtils.transferPlayerData(mMain, (Player)callee, server);
-						return true;
-					} catch (Exception e) {
-						caller.sendMessage("Caught exception when transferring players");
-						return false;
-					}
-				}
-			} else {
-				// Only players can be sent!
-				sender.sendMessage(ChatColor.RED + "Invalid number of parameters for non-player sender!");
-				return false;
-			}
-
-		}
-
 		// Default to sending equipment
-		if (arg3[1].equals("False") || arg3[1].equals("false")) {
-			sendPlayerStuff = false;
-		}
+		boolean sendPlayerStuff = true;
 
-		// Need to only match players with the given scoreName value
-		if (arg3.length == 11) {
-			try {
-				scoreName = arg3[8];
-				scoreMin = CommandUtils.parseIntFromString(sender, arg3[9]);
-				scoreMax = CommandUtils.parseIntFromString(sender, arg3[10]);
-			} catch (Exception e) {
-				return false;
+		if (arg3.length == 2) {
+			if (arg3[1].equals("False") || arg3[1].equals("false") || arg3[1].equals("f") || arg3[1].equals("F")) {
+				sendPlayerStuff = false;
 			}
 		}
 
-		AreaBounds bounds;
-		try {
-			bounds = CommandUtils.parseAreaFromString(sender, arg3[2], arg3[3],
-			                                          arg3[4], arg3[5], arg3[6], arg3[7]);
-		} catch (Exception e) {
+		if (sender instanceof Player) {
+			// Sender is requesting transfer to destination server with equipment
+			return _transferServer(sender, (Player)sender, sendPlayerStuff, server);
+		} else if (sender instanceof ProxiedCommandSender) {
+			CommandSender callee = ((ProxiedCommandSender)sender).getCallee();
+			CommandSender caller = ((ProxiedCommandSender)sender).getCaller();
+			if (callee instanceof Player) {
+				// Sender is an /execute command targeting a player
+				caller.sendMessage("Transferring " + callee.getName() + " with playerdata to " + server);
+				return _transferServer(caller, (Player)callee, sendPlayerStuff, server);
+			} else {
+				sender.sendMessage(ChatColor.RED + "Execute command detected with non-player target!");
+				return false;
+			}
+		} else {
+			// Only players can be sent!
+			sender.sendMessage(ChatColor.RED + "Invalid number of parameters for non-player sender!");
 			return false;
 		}
+	}
 
-		for (Player player : mMain.getServer().getOnlinePlayers()) {
-			if (bounds.within(player.getLocation())) {
-				if (scoreName != null) {
-					int score = ScoreboardUtils.getScoreboardValue(player, scoreName);
-
-					// Ignore this player if score was specified and it didn't match
-					if (score == -1 || score < scoreMin || score > scoreMax) {
-						continue;
-					}
-				}
-
-				try {
-					if (sendPlayerStuff == false) {
-						sender.sendMessage("Transferring " + player.getName() + " to " + server);
-						NetworkUtils.sendPlayer(mMain, player, server);
-					} else {
-						sender.sendMessage("Transferring " + player.getName() + " with playerdata to " + server);
-						NetworkUtils.transferPlayerData(mMain, player, server);
-					}
-				} catch (Exception e) {
-					sender.sendMessage("Caught exception when transferring players");
-					e.printStackTrace();
-					return false;
-				}
+	private boolean _transferServer(CommandSender sender, Player player, boolean sendPlayerStuff, String server) {
+		try {
+			if (sendPlayerStuff == true) {
+				player.sendMessage(ChatColor.GOLD + "Transferring you to " + server);
+				NetworkUtils.transferPlayerData(mMain, player, server);
+			} else {
+				player.sendMessage(ChatColor.GOLD + "Transferring you " + ChatColor.RED  + "without playerdata" + ChatColor.GOLD + " to " + server);
+				NetworkUtils.sendPlayer(mMain, player, server);
 			}
+		} catch (Exception e) {
+			sender.sendMessage("Caught exception when transferring players");
+			return false;
 		}
 
 		return true;
