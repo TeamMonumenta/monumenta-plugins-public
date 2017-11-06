05fdc48224b1453a88050a8aad8cbb4f58b0cd4f
NickNackGus <NickNackGus@gmail.com>
2017-11-06 14:43:52 -0500

    Fixed C'Taz's Plague quest compass message suggesting hallowed pond was to the North instead of East

--- Monumenta/src/pe/project/locations/quest/ThePlague.java
+++ Monumenta/src/pe/project/locations/quest/ThePlague.java
@@ -11,7 +11,7 @@ public class ThePlague extends Quest {
 		
 		addMarker(new LocationMarker(this, new Location(world, 233, 255, 282), "Nyr is infected with a mysterious illness! Only a Tlaxan Shaman can help save the town.", 5));
 		addMarker(new LocationMarker(this, new Location(world, 452, 255, 105), "C'Taz needs exactly 12 dark amber for the first ingredient of the medicine, hidden within a massive tree.", 6));
-		addMarker(new LocationMarker(this, new Location(world, 426, 255, 249), "Next C'Taz needs precisely 16 spirit flowers from the hallowed pond north of his hut.", 8));
+		addMarker(new LocationMarker(this, new Location(world, 426, 255, 249), "Next C'Taz needs precisely 16 spirit flowers from the hallowed pond East of his hut.", 8));
 		addMarker(new LocationMarker(this, new Location(world, 311, 255, -76), "Finally, bring C'Taz 10 pieces of Haunted Earth from the dangerous ruins east of Nyr.", 10));
 		addMarker(new LocationMarker(this, new Location(world, -105, 255, -114), "Hurry! Bring the medicine to Dr Laurie so that Nyr will be safe once more.", 16, 17));
 	}
