50c6742975f44daaace74815dea43f10ba0d8604
Byron Marohn <combustible@live.com>
2017-11-07 18:27:09 -0800

    Transferring data with UTF-8 section symbols now works correctly
    Signed-off-by: Byron Marohn <combustible@live.com>

--- Monumenta/src/pe/project/utils/FileUtils.java
+++ Monumenta/src/pe/project/utils/FileUtils.java
@@ -1,15 +1,17 @@
 package pe.project.utils;
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 
 public class FileUtils {
 	public static String readFile(String fileName) throws Exception, FileNotFoundException {
 		// Do not attempt to catch exceptions here - let them propagate to the caller
-		String content = null;
 		File file;
 
 		if (fileName == null || fileName.isEmpty()) {
@@ -21,19 +23,27 @@ public class FileUtils {
 			throw new FileNotFoundException("File '" + fileName + "' does not exist");
 		}
 
-		FileReader reader = null;
+		InputStreamReader reader = null;
+		final int bufferSize = 1024;
+		final char[] buffer = new char[bufferSize];
+		final StringBuilder content = new StringBuilder();
+
 		try {
-			reader = new FileReader(file);
-			char[] chars = new char[(int) file.length()];
-			reader.read(chars);
-			content = new String(chars);
+			reader = new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8);
+			while (true) {
+				int rsz = reader.read(buffer, 0, buffer.length);
+				if (rsz < 0) {
+					break;
+				}
+				content.append(buffer, 0, rsz);
+			}
 		} finally {
 			if (reader != null) {
 				reader.close();
 			}
 		}
 
-		return content;
+		return content.toString();
 	}
 
 	public static void writeFile(String fileName, String contents) throws IOException {
@@ -45,9 +55,9 @@ public class FileUtils {
 			file.createNewFile();
 		}
 
-		FileWriter writer = null;
+		OutputStreamWriter writer = null;
 		try {
-			writer = new FileWriter(file);
+			writer = new OutputStreamWriter(new FileOutputStream(fileName), StandardCharsets.UTF_8);
 			writer.write(contents);
 		} finally {
 			if (writer != null) {
