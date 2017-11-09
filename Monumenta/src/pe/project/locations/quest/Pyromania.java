a56555394af8d3e2bb6fc7202a6fd7af2ae91f78
NickNackGus <NickNackGus@gmail.com>
2017-11-08 22:09:05 -0500

    pyromania quest compass text correction

--- Monumenta/src/pe/project/locations/quest/Pyromania.java
+++ Monumenta/src/pe/project/locations/quest/Pyromania.java
@@ -11,6 +11,6 @@ public class Pyromania extends Quest {
 		
 		addMarker(new LocationMarker(this, new Location(world, 935, 255, -145), "Carbocius needs magma samples. Retrieve one from the volcano in the Highlands, and return when you have samples from 3 locations.", 3));
 		addMarker(new LocationMarker(this, new Location(world, 570, 255, -190), "Carbocius needs magma samples. Retrieve one from the fire cave, west of the Monastery, and return when you have samples from 3 locations.", 3));
-		addMarker(new LocationMarker(this, new Location(world, 744, 255, 88), "Carbocius needs magma samples. Retrieve one from the flame chasm, east of Farr, and return when you have samples from 3 locations.", 3));
+		addMarker(new LocationMarker(this, new Location(world, 744, 255, 88), "Carbocius needs magma samples. Retrieve one from the molten chasm, east of Farr, and return when you have samples from 3 locations.", 3));
 	}
 }
