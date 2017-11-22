57e9788954422a75c651cd79646869482e79bf04
Joshua Letellier <v-jolet@microsoft.com>
2017-11-22 10:45:18 -0800

    Minor cleanup post refactor

--- Monumenta/src/pe/project/items/MonsterEggOverride.java
+++ Monumenta/src/pe/project/items/MonsterEggOverride.java
@@ -1,6 +1,5 @@
 package pe.project.items;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -12,8 +11,6 @@ import pe.project.Plugin;
 public class MonsterEggOverride extends OverrideItem {
 	@Override
 	public boolean rightClickItemInteraction(Plugin plugin, Player player, Action action, ItemStack item, Block block) {
-		Bukkit.broadcastMessage("broken");
-
 		//	Are we clicking on a monster spawner?
 		return (block.getType() != Material.MOB_SPAWNER);
 	}
