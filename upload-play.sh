a24b21987328d02928d5fbfaa5f46ade189eb054
Byron Marohn <combustible@live.com>
2017-12-09 18:01:41 -0800

    Updated upload scripts for new server
    Signed-off-by: Byron Marohn <combustible@live.com>

--- upload-play.sh
+++ upload-play.sh
@@ -15,9 +15,5 @@ monumenta="$(ls -r Monumenta-Plugins*${monumenta_version}.jar | head -n 1)"
 echo "Bungee version: $bungee"
 echo "Monumenta version: $monumenta"
 
-scp -P 9922 $bungee rock@158.69.117.57:/home/rock/project_epic/server_config/plugins/
-scp -P 9922 $monumenta rock@158.69.117.57:/home/rock/project_epic/server_config/plugins/
-ssh -p 9922 rock@158.69.117.57 "cd /home/rock/project_epic/server_config/plugins && rm -f Monumenta-Bungee-Plugins.jar ; ln -s $bungee Monumenta-Bungee-Plugins.jar"
-ssh -p 9922 rock@158.69.117.57 "cd /home/rock/project_epic/server_config/plugins && rm -f Monumenta-Plugins.jar ; ln -s $monumenta Monumenta-Plugins.jar"
-
-
+scp -P 9922 $bungee $monumenta rock@beta.playmonumenta.com:/home/rock/project_epic/server_config/plugins/
+ssh -p 9922 rock@beta.playmonumenta.com "cd /home/rock/project_epic/server_config/plugins && rm -f Monumenta-Bungee-Plugins.jar Monumenta-Plugins.jar ; ln -s $bungee Monumenta-Bungee-Plugins.jar; ln -s $monumenta Monumenta-Plugins.jar"
--- upload.sh
+++ upload.sh
@@ -15,9 +15,7 @@ monumenta="$(ls -r Monumenta-Plugins*${monumenta_version}.jar | head -n 1)"
 echo "Bungee version: $bungee"
 echo "Monumenta version: $monumenta"
 
-scp -P 9922 $bungee rock@50.39.100.13:/home/rock/project_epic/server_config/plugins/
-scp -P 9922 $monumenta rock@50.39.100.13:/home/rock/project_epic/server_config/plugins/
-ssh -p 9922 rock@50.39.100.13 "cd /home/rock/project_epic/server_config/plugins && rm -f Monumenta-Bungee-Plugins.jar ; ln -s $bungee Monumenta-Bungee-Plugins.jar"
-ssh -p 9922 rock@50.39.100.13 "cd /home/rock/project_epic/server_config/plugins && rm -f Monumenta-Plugins.jar ; ln -s $monumenta Monumenta-Plugins.jar"
+scp -P 9922 $bungee $monumenta rock@50.39.100.13:/home/rock/project_epic/server_config/plugins/
+ssh -p 9922 rock@50.39.100.13 "cd /home/rock/project_epic/server_config/plugins && rm -f Monumenta-Bungee-Plugins.jar Monumenta-Plugins.jar ; ln -s $bungee Monumenta-Bungee-Plugins.jar ; ln -s $monumenta Monumenta-Plugins.jar"
 
 
