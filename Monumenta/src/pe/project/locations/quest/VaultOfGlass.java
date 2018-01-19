54b6d7d9d4b2f610c6fb2a6878a2bf227b056313
Kaladun <kaladun@gmail.com>
2018-01-19 15:18:49 -0700

    Update VaultOfGlass.java

--- Monumenta/src/pe/project/locations/quest/VaultOfGlass.java
+++ Monumenta/src/pe/project/locations/quest/VaultOfGlass.java
@@ -10,7 +10,7 @@ public class VaultOfGlass extends Quest {
 		super(world, "A Vault of Glass", "Quest18");
 		
 		addMarker(new LocationMarker(this, new Location(world, -689, 141, -3), "The urgent letter said to climb the tower in Sierhaven with the purple and black banners.", 1));
-		addMarker(new LocationMarker(this, new Location(world, 581, 99, 171), "Look around Farr for clues to where Carlton fled to.", 3));
+		addMarker(new LocationMarker(this, new Location(world, 558, 99, 168), "Look around Farr for clues to where Carlton fled to.", 3));
 		addMarker(new LocationMarker(this, new Location(world, 629, 97, 208), "Find your way through the maze beneath the Five Axes Trading Co warehouse.", 6))
 		addMarker(new LocationMarker(this, new Location(world, 629, 97, 208), "Find your way through the maze beneath the Five Axes Trading Co warehouse.", 7));
 		addMarker(new LocationMarker(this, new Location(world, -689, 141, -3), "Grab the ledger and head back to Argent! He needs to see it ASAP.", 8));
