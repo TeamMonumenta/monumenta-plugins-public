31e6af9824608a93c3a52a6a1c3fa67783e925bd
Tim Southwick <NickNackGus@gmail.com>
2018-01-19 23:02:25 -0500

    Fixed messing semicolon

--- Monumenta/src/pe/project/locations/quest/VaultOfGlass.java
+++ Monumenta/src/pe/project/locations/quest/VaultOfGlass.java
@@ -11,7 +11,7 @@ public class VaultOfGlass extends Quest {
 		
 		addMarker(new LocationMarker(this, new Location(world, -689, 141, -3), "The urgent letter said to climb the tower in Sierhaven with the purple and black banners.", 1));
 		addMarker(new LocationMarker(this, new Location(world, 558, 99, 168), "Look around Farr for clues to where Carlton fled to.", 3));
-		addMarker(new LocationMarker(this, new Location(world, 629, 97, 208), "Find your way through the maze beneath the Five Axes Trading Co warehouse.", 6))
+		addMarker(new LocationMarker(this, new Location(world, 629, 97, 208), "Find your way through the maze beneath the Five Axes Trading Co warehouse.", 6));
 		addMarker(new LocationMarker(this, new Location(world, 629, 97, 208), "Find your way through the maze beneath the Five Axes Trading Co warehouse.", 7));
 		addMarker(new LocationMarker(this, new Location(world, -689, 141, -3), "Grab the ledger and head back to Argent! He needs to see it ASAP.", 8));
 		addMarker(new LocationMarker(this, new Location(world, -689, 141, -3), "The ledger is gone. Tell Argent what happened.", 9));
