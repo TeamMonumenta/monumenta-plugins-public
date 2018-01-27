33bfbc3d8a594fbe9c4adc8de2ce0c7beeaf6db9
Joshua Letellier <v-jolet@microsoft.com>
2018-01-27 14:35:00 -0800

    Item Overrides: Enchantment Table - Enchantment tables can no longer be accessed by someone unless they're in creative mode

---
+++ Monumenta/src/pe/project/items/EnchantmentTableOverride.java
@@ -0,0 +1,16 @@
+package pe.project.items;
+
+import org.bukkit.GameMode;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.inventory.ItemStack;
+
+import pe.project.Plugin;
+
+public class EnchantmentTableOverride extends OverrideItem {
+	@Override
+	public boolean rightClickBlockInteraction(Plugin plugin, Player player, Action action, ItemStack item, Block block) {
+		return (player == null || player.getGameMode() == GameMode.CREATIVE);
+	}
+}
