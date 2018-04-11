450bfc271bf0b7fde4deb8444af7c71c2b7a8244
Byron Marohn <combustible@live.com>
2018-04-10 22:54:28 -0700

    Chests placed on barriers can not be broken except in creative
    Signed-off-by: Byron Marohn <combustible@live.com>

--- Monumenta/src/pe/project/items/ChestOverride.java
+++ Monumenta/src/pe/project/items/ChestOverride.java
@@ -1,14 +1,15 @@
 package pe.project.items;
 
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.GameMode;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
 
 import pe.project.Plugin;
 
@@ -36,6 +37,29 @@ public class ChestOverride extends OverrideItem {
 				player.sendMessage(ChatColor.GOLD + "This chest is empty or has a loot table!");
 				return false;
 			}
+		}
+
+		return true;
+	}
+
+	/* Chests placed on barriers can not be broken */
+	@Override
+	public boolean blockBreakInteraction(Plugin plugin, Player player, Block block) {
+		if ((player.getGameMode() == GameMode.CREATIVE) || _breakable(block)) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean blockExplodeInteraction(Plugin plugin, Block block) {
+		return _breakable(block);
+	}
+
+	private boolean _breakable(Block block) {
+		Block blockUnder = block.getLocation().add(0, -1, 0).getBlock();
+		if (blockUnder != null && blockUnder.getType() == Material.BARRIER) {
+			return false;
 		}
 
 		return true;
