0ca96f0c2c4ae7faa1467966ce85740d084ed761
Byron Marohn <combustible@live.com>
2018-06-21 20:14:37 -0700

    Refactor laser spells into a common framework
    Signed-off-by: Byron Marohn <combustible@live.com>

--- src/pe/bossfights/spells/SpellAxtalDeathRay.java
+++ src/pe/bossfights/spells/SpellAxtalDeathRay.java
@@ -1,122 +1,39 @@
 package pe.bossfights.spells;
 
-import java.util.List;
-import java.util.Random;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.Sound;
-import org.bukkit.util.Vector;
 
-import pe.bossfights.utils.Utils;
-
-public class SpellAxtalDeathRay implements Spell
+public class SpellAxtalDeathRay extends BaseLaser
 {
-	private Plugin mPlugin;
-	private Entity mLauncher;
-	private Random mRand = new Random();
-	private int g_sound;
-
-	public SpellAxtalDeathRay(Plugin plugin, Entity launcher)
+	public SpellAxtalDeathRay(Plugin plugin, Entity boss)
 	{
-		mPlugin = plugin;
-		mLauncher = launcher;
-	}
-
-	@Override
-	public void run()
-	{
-		g_sound = 0;
-		List<Player> players = Utils.playersInRange(mLauncher.getLocation(), 60);
-		Player target = players.get(mRand.nextInt(players.size()));
-		launch(target);
-		animation(target);
-	}
-
-	// TODO: These two methods need to be combined...
-	public void launch(Player target)
-	{
-		BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
-		Runnable teleport = new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				Location launLoc = mLauncher.getLocation().add(0, 1.6f, 0);
-				Location tarLoc = target.getLocation().add(0, 0.6f, 0);
-				Vector vect = new Vector(tarLoc.getX() - launLoc.getX(), tarLoc.getY() - launLoc.getY(), tarLoc.getZ() - launLoc.getZ());
-				vect.normalize();
-				Location summonLoc = tarLoc;
-				for (int i = 0; i < 200; i++)
-				{
-					double dist_to_player = launLoc.distance(tarLoc);
-					Vector baseVect = new Vector(vect.getX() / (400 / dist_to_player), vect.getY() / (400 / dist_to_player), vect.getZ() / (400 / dist_to_player));
-					Location tmpLoc = launLoc;
-					tmpLoc = launLoc;
-					Vector tmpVect = baseVect;
-					tmpVect = baseVect;
-					tmpLoc.add(tmpVect.multiply((double)i));
-					tmpLoc.getWorld().spawnParticle(Particle.SMOKE_NORMAL, tmpLoc, 1, 0.02, 0.02, 0.02, 0);
-					summonLoc = tmpLoc;
-					if (tmpLoc.getBlock().getType().isSolid())
-						break;
-					else if (launLoc.distance(tmpLoc) > launLoc.distance(tarLoc))
-						break;
-					else if (tarLoc.distance(tmpLoc) < 0.5)
-						break;
-				}
-				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "summon tnt " + summonLoc.getX() + " " + summonLoc.getY() + " " + summonLoc.getZ() + " {Fuse:1}");
-				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "summon tnt " + summonLoc.getX() + " " + summonLoc.getY() + " " + summonLoc.getZ() + " {Fuse:3}");
-				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "summon tnt " + summonLoc.getX() + " " + summonLoc.getY() + " " + summonLoc.getZ() + " {Fuse:5}");
-			}
-		};
-		scheduler.scheduleSyncDelayedTask(mPlugin, teleport, 140);
-	}
-
-	public void animation(Player target)
-	{
-		BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
-		Runnable teleport = new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				Location launLoc = mLauncher.getLocation().add(0, 1.6f, 0);
-				Location tarLoc = target.getLocation().add(0, 0.6f, 0);
-				tarLoc.getWorld().playSound(tarLoc, Sound.UI_TOAST_IN, 2, (0.5f + ((float)g_sound / 200f) * 1.5f));
-				launLoc.getWorld().playSound(launLoc, Sound.UI_TOAST_IN, 2, (0.5f + ((float)g_sound / 200f) * 1.5f));
-				tarLoc.getWorld().playSound(tarLoc, Sound.ENTITY_WITHER_SPAWN, 2, (0.5f + ((float)g_sound / 200f) * 1.5f));
-				launLoc.getWorld().playSound(launLoc, Sound.UI_TOAST_IN, 2, (0.5f + ((float)g_sound / 200f) * 1.5f));
-				g_sound++;
-				Vector vect = new Vector(tarLoc.getX() - launLoc.getX(), tarLoc.getY() - launLoc.getY(), tarLoc.getZ() - launLoc.getZ());
-				vect.normalize();
-				for (int i = 0; i < 200; i++)
-				{
-					double dist_to_player = launLoc.distance(tarLoc);
-					Vector baseVect = new Vector(vect.getX() / (100 / dist_to_player), vect.getY() / (100 / dist_to_player), vect.getZ() / (100 / dist_to_player));
-					Location tmpLoc = launLoc;
-					tmpLoc = launLoc;
-					Vector tmpVect = baseVect;
-					tmpVect = baseVect;
-					tmpLoc.add(tmpVect.multiply((double)i));
-					tmpLoc.getWorld().spawnParticle(Particle.SMOKE_NORMAL, tmpLoc, 1, 0.02, 0.02, 0.02, 0);
-					tmpLoc.getWorld().spawnParticle(Particle.SMOKE_LARGE, tmpLoc, 1, 0.02, 0.02, 0.02, 0);
-					tmpLoc.getWorld().spawnParticle(Particle.SPELL_MOB, tmpLoc, 1, 0.02, 0.02, 0.02, 1);
-					if (tmpLoc.getBlock().getType().isSolid())
-						break;
-					else if (launLoc.distance(tmpLoc) > launLoc.distance(tarLoc))
-						break;
-					else if (tarLoc.distance(tmpLoc) < 0.5)
-						break;
-				}
-			}
-		};
-		for (int j = 0; j < 140; j++)
-			scheduler.scheduleSyncDelayedTask(mPlugin, teleport, j);
+		super(plugin, boss, 60, 140, false, true,
+		      // Tick action per player
+		      (Player player, int ticks, boolean blocked) ->
+		        {
+		            player.playSound(player.getLocation(), Sound.UI_TOAST_IN, 2, (0.5f + ((float)ticks / 100f) * 1.5f));
+		            boss.getLocation().getWorld().playSound(boss.getLocation(), Sound.UI_TOAST_IN, 2, (0.5f + ((float)ticks / 100f) * 1.5f));
+		            player.playSound(player.getLocation(), Sound.ENTITY_WITHER_SPAWN, 2, (0.5f + ((float)ticks / 100f) * 1.5f));
+		            boss.getLocation().getWorld().playSound(boss.getLocation(), Sound.UI_TOAST_IN, 2, (0.5f + ((float)ticks / 100f) * 1.5f));
+		        },
+		      // Particles generated by the laser
+		      (Location loc) ->
+		        {
+		            loc.getWorld().spawnParticle(Particle.SMOKE_NORMAL, loc, 1, 0.02, 0.02, 0.02, 0);
+		            loc.getWorld().spawnParticle(Particle.SMOKE_LARGE, loc, 1, 0.02, 0.02, 0.02, 0);
+		            loc.getWorld().spawnParticle(Particle.SPELL_MOB, loc, 1, 0.02, 0.02, 0.02, 1);
+		        },
+		      // TNT generated at the end of the attack
+		      (Player player, Location loc, boolean blocked) ->
+		        {
+		            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "summon tnt " + loc.getX() + " " + loc.getY() + " " + loc.getZ() + " {Fuse:1}");
+		            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "summon tnt " + loc.getX() + " " + loc.getY() + " " + loc.getZ() + " {Fuse:3}");
+		            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "summon tnt " + loc.getX() + " " + loc.getY() + " " + loc.getZ() + " {Fuse:5}");
+		        });
 	}
 }
---
+++ src/pe/bossfights/spells/base/BaseLaser.java
@@ -0,0 +1,158 @@
+package pe.bossfights.spells;
+
+import java.util.List;
+import java.util.Random;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.Location;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import pe.bossfights.utils.Utils;
+
+public class BaseLaser implements Spell
+{
+	@FunctionalInterface
+	interface TickAction
+	{
+		/**
+		 * User function called once every two ticks while laser is running
+		 * @param player  Player being targeted
+		 * @param tick    Number of ticks since start of attack
+		 *      NOTE - Only even numbers are returned here!
+		 * @param blocked Whether the laser is obstructed (true) or hits the player (false)
+		 */
+		void run(Player player, int tick, boolean blocked);
+	}
+
+	@FunctionalInterface
+	interface ParticleAction
+	{
+		/**
+		 * User function called many times per tick with the location where
+		 * a laser particle should be spawned
+		 * @param loc Location to spawn a particle
+		 */
+		void run(Location loc);
+	}
+
+	@FunctionalInterface
+	interface FinishAction
+	{
+		/**
+		 * User function called once every two ticks while laser is running
+		 * @param player  Player being targeted
+		 * @param loc     Location where the laser ends (either at player or occluding block)
+		 * @param blocked Whether the laser is obstructed (true) or hits the player (false)
+		 */
+		void run(Player player, Location loc, boolean blocked);
+	}
+
+	private Plugin mPlugin;
+	private Entity mBoss;
+	private int mRange;
+	private int mNumTicks;
+	private boolean mStopWhenBlocked;
+	private boolean mSingleTarget;
+	TickAction mTickAction;
+	ParticleAction mParticleAction;
+	FinishAction mFinishAction;
+
+	/**
+	 * @param plugin          Plugin
+	 * @param boss            Boss
+	 * @param range           Range within which players may be targeted
+	 * @param numTicks        Total duration of the spell
+	 * @param stopWhenBlocked Whether the spell should abort if line of sight is broken
+	 * @param singleTarget    Target random player (true) or all players (false)
+	 * @param tickAction      Called once every two ticks for targeted player(s)
+	 * @param particleAction  Called many times per tick to generate particles for laser
+	 * @param finishAction    Called when the spell numTicks have elapsed
+	 */
+	public BaseLaser(Plugin plugin, Entity boss, int range, int numTicks, boolean stopWhenBlocked, boolean singleTarget,
+	                 TickAction tickAction, ParticleAction particleAction, FinishAction finishAction)
+	{
+		mPlugin = plugin;
+		mBoss = boss;
+		mRange = range;
+		mNumTicks = numTicks;
+		mStopWhenBlocked = stopWhenBlocked;
+		mSingleTarget = singleTarget;
+		mTickAction = tickAction;
+		mParticleAction = particleAction;
+		mFinishAction = finishAction;
+	}
+
+	@Override
+	public void run()
+	{
+		List<Player> players = Utils.playersInRange(mBoss.getLocation(), mRange);
+		if (mSingleTarget)
+			// Single target chooses a random player within range
+			launch(players.get((new Random()).nextInt(players.size())));
+		else
+			// Otherwise target all players within range
+			for (Player player : players)
+				launch(player);
+	}
+
+	private void launch(Player target)
+	{
+		new BukkitRunnable()
+		{
+			private int mTicks = 0;
+
+			@Override
+			public void run()
+			{
+				Location launLoc = mBoss.getLocation().add(0, 1.6f, 0);
+				Location tarLoc = target.getLocation().add(0, 0.6f, 0);
+				Location endLoc = launLoc;
+
+				Vector baseVect = new Vector(tarLoc.getX() - launLoc.getX(), tarLoc.getY() - launLoc.getY(), tarLoc.getZ() - launLoc.getZ());
+				baseVect = baseVect.normalize().multiply(0.5);
+
+				boolean blocked = false;
+				for (int i = 0; i < 200; i++)
+				{
+					endLoc.add(baseVect);
+
+					if (mParticleAction != null)
+						mParticleAction.run(endLoc);
+
+					if (endLoc.getBlock().getType().isSolid())
+					{
+						blocked = true;
+						break;
+					}
+					else if (launLoc.distance(endLoc) > launLoc.distance(tarLoc))
+						break;
+					else if (tarLoc.distance(endLoc) < 0.5)
+						break;
+				}
+
+				if (blocked && mStopWhenBlocked)
+				{
+					this.cancel();
+					return;
+				}
+
+				if (mTickAction != null)
+					mTickAction.run(target, mTicks, blocked);
+
+				if (mTicks >= mNumTicks)
+				{
+					if (mFinishAction != null)
+						mFinishAction.run(target, endLoc, blocked);
+
+					this.cancel();
+					return;
+				}
+
+				mTicks += 2;
+			}
+		} .runTaskTimer(mPlugin, 0, 2);
+	}
+}
