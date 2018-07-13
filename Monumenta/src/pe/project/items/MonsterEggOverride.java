e1f05afa1873d589d5f40940e507914d15aa1025
Byron Marohn <combustible@live.com>
2018-07-13 15:01:14 -0700

    Only players on plots can use spawn eggs
    Signed-off-by: Byron Marohn <combustible@live.com>

--- Monumenta/src/pe/project/items/MonsterEggOverride.java
+++ Monumenta/src/pe/project/items/MonsterEggOverride.java
@@ -1,18 +1,23 @@
 package pe.project.items;
 
-import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
+import org.bukkit.GameMode;
 import org.bukkit.inventory.ItemStack;
 
 import pe.project.Plugin;
+import pe.project.utils.LocationUtils;
+import pe.project.utils.LocationUtils.LocationType;
 
 public class MonsterEggOverride extends OverrideItem {
 	@Override
 	public boolean rightClickItemInteraction(Plugin plugin, Player player, Action action, ItemStack item, Block block) {
-		//	Are we clicking on a monster spawner?
-		return (player.getGameMode() == GameMode.CREATIVE) || (block == null || block.getType() != Material.MOB_SPAWNER);
+		// Only allow creative players or players in their plots (in capital and survival) to use spawn eggs
+		if ((player.getGameMode() == GameMode.CREATIVE) ||
+		    ((player.getGameMode() == GameMode.SURVIVAL) && (LocationUtils.getLocationType(plugin, player) == LocationType.Capital))) {
+			return true;
+		}
+		return false;
 	}
 }
