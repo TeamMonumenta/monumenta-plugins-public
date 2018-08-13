577b5c733cb6f5fe52460e22f7ea3581fe73e25f
Byron Marohn <combustible@live.com>
2018-08-12 17:01:47 -0700

    Update linux compile helper scripts for new maven build
    Signed-off-by: Byron Marohn <combustible@live.com>

--- compile.sh
+++ compile.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-./mvnw clean install
+# Update version number
+perl -p -i -e "s|<version>2.41.0</version>|<version>$(git describe --tags --always --dirty)</version>|g" pom.xml
+
+mvn clean install
 retcode=$?
 if [[ $retcode -ne 0 ]]; then
 	exit $retcode
--- upload.sh
+++ upload.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$SCRIPT_DIR/out"
+cd "$SCRIPT_DIR/target"
 
-version="$(ls monumenta-bossfights-*.jar | sed -e 's|^.*_\([0-9]*-[0-9]*\).jar|\1|g' | sort -n -r | head -n 1)"
-if [[ -z "$version" ]]; then
+plugin="$(ls -r monumenta-bossfights-*.jar | grep -v sources | head -n 1)"
+if [[ -z "$plugin" ]]; then
 	exit 1
 fi
-plugin="$(ls -r monumenta-bossfights-*${version}.jar | head -n 1)"
 
 echo "Plugin version: $plugin"
 
