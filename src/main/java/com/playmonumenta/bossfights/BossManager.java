2914a4e2fbae1518d7555bb28848d5fbf0f1c5ed
Byron Marohn <combustible@live.com>
2018-08-13 14:26:46 -0700

    Refactor bossmanager boss registration
    Signed-off-by: Byron Marohn <combustible@live.com>

--- src/main/java/com/playmonumenta/bossfights/BossManager.java
+++ src/main/java/com/playmonumenta/bossfights/BossManager.java
@@ -22,18 +22,7 @@ import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.Location;
 
-import com.playmonumenta.bossfights.bosses.Boss;
-import com.playmonumenta.bossfights.bosses.CAxtal;
-import com.playmonumenta.bossfights.bosses.ChargerBoss;
-import com.playmonumenta.bossfights.bosses.GenericBoss;
-import com.playmonumenta.bossfights.bosses.InfestedBoss;
-import com.playmonumenta.bossfights.bosses.InvisibleBoss;
-import com.playmonumenta.bossfights.bosses.FireResistantBoss;
-import com.playmonumenta.bossfights.bosses.Masked_1;
-import com.playmonumenta.bossfights.bosses.Masked_2;
-import com.playmonumenta.bossfights.bosses.PulseLaserBoss;
-import com.playmonumenta.bossfights.bosses.Virius;
-import com.playmonumenta.bossfights.bosses.Orangyboi;
+import com.playmonumenta.bossfights.bosses.*;
 import com.playmonumenta.bossfights.utils.SerializationUtils;
 import com.playmonumenta.bossfights.utils.Utils;
 import com.playmonumenta.bossfights.utils.Utils.ArgumentException;
@@ -42,6 +31,61 @@ public class BossManager implements Listener, CommandExecutor
 {
 	Plugin mPlugin;
 	Map<UUID, Boss> mBosses;
+
+@FunctionalInterface
+public interface StatelessBossConstructor
+{
+	Boss construct(Plugin plugin, LivingEntity entity);
+}
+
+@FunctionalInterface
+public interface StatefulBossConstructor
+{
+	Boss construct(Plugin plugin, LivingEntity entity, Location spawnLoc, Location endLoc);
+}
+
+@FunctionalInterface
+public interface BossDeserializer
+{
+	Boss deserialize(Plugin plugin, LivingEntity entity) throws Exception;
+}
+
+static Map<String, StatelessBossConstructor> mStatelessBosses;
+static Map<String, StatefulBossConstructor> mStatefulBosses;
+static Map<String, BossDeserializer> mBossDeserializers;
+static
+{
+	/* Stateless bosses are those that have no end location set where a redstone block would be spawned when they die */
+	mStatelessBosses = new HashMap<String, StatelessBossConstructor>();
+	mStatelessBosses.put(GenericBoss.identityTag, (Plugin p, LivingEntity e) -> new GenericBoss(p, e));
+	mStatelessBosses.put(InvisibleBoss.identityTag, (Plugin p, LivingEntity e) -> new InvisibleBoss(p, e));
+	mStatelessBosses.put(FireResistantBoss.identityTag, (Plugin p, LivingEntity e) -> new FireResistantBoss(p, e));
+	mStatelessBosses.put(PulseLaserBoss.identityTag, (Plugin p, LivingEntity e) -> new PulseLaserBoss(p, e));
+	mStatelessBosses.put(ChargerBoss.identityTag, (Plugin p, LivingEntity e) -> new ChargerBoss(p, e));
+	mStatelessBosses.put(InfestedBoss.identityTag, (Plugin p, LivingEntity e) -> new InfestedBoss(p, e));
+
+	/* Stateful bosses have a remembered spawn location and end location where a redstone block is set when they die */
+	mStatefulBosses = new HashMap<String, StatefulBossConstructor>();
+	mStatefulBosses.put(CAxtal.identityTag, (Plugin p, LivingEntity e, Location s, Location l) -> new CAxtal(p, e, s, l));
+	mStatefulBosses.put(Masked_1.identityTag, (Plugin p, LivingEntity e, Location s, Location l) -> new Masked_1(p, e, s, l));
+	mStatefulBosses.put(Masked_2.identityTag, (Plugin p, LivingEntity e, Location s, Location l) -> new Masked_2(p, e, s, l));
+	mStatefulBosses.put(Virius.identityTag, (Plugin p, LivingEntity e, Location s, Location l) -> new Virius(p, e, s, l));
+	mStatefulBosses.put(Orangyboi.identityTag, (Plugin p, LivingEntity e, Location s, Location l) -> new Orangyboi(p, e, s, l));
+
+	/* All bosses have a deserializer which gives the boss back their abilities when chunks re-load */
+	mBossDeserializers = new HashMap<String, BossDeserializer>();
+	mBossDeserializers.put(GenericBoss.identityTag, (Plugin p, LivingEntity e) -> GenericBoss.deserialize(p, e));
+	mBossDeserializers.put(InvisibleBoss.identityTag, (Plugin p, LivingEntity e) -> InvisibleBoss.deserialize(p, e));
+	mBossDeserializers.put(FireResistantBoss.identityTag, (Plugin p, LivingEntity e) -> FireResistantBoss.deserialize(p, e));
+	mBossDeserializers.put(PulseLaserBoss.identityTag, (Plugin p, LivingEntity e) -> PulseLaserBoss.deserialize(p, e));
+	mBossDeserializers.put(ChargerBoss.identityTag, (Plugin p, LivingEntity e) -> ChargerBoss.deserialize(p, e));
+	mBossDeserializers.put(InfestedBoss.identityTag, (Plugin p, LivingEntity e) -> InfestedBoss.deserialize(p, e));
+	mBossDeserializers.put(CAxtal.identityTag, (Plugin p, LivingEntity e) -> CAxtal.deserialize(p, e));
+	mBossDeserializers.put(Masked_1.identityTag, (Plugin p, LivingEntity e) -> Masked_1.deserialize(p, e));
+	mBossDeserializers.put(Masked_2.identityTag, (Plugin p, LivingEntity e) -> Masked_2.deserialize(p, e));
+	mBossDeserializers.put(Virius.identityTag, (Plugin p, LivingEntity e) -> Virius.deserialize(p, e));
+	mBossDeserializers.put(Orangyboi.identityTag, (Plugin p, LivingEntity e) -> Orangyboi.deserialize(p, e));
+}
 
 	public BossManager(Plugin plugin)
 	{
@@ -87,48 +131,22 @@ public class BossManager implements Listener, CommandExecutor
 			return false;
 		}
 
-		Boss boss;
-		switch (args[0].toLowerCase())
-		{
-		case GenericBoss.identityTag:
-			boss = new GenericBoss(mPlugin, (LivingEntity)targetEntity);
-			break;
-		case InvisibleBoss.identityTag:
-			boss = new InvisibleBoss(mPlugin, (LivingEntity)targetEntity);
-			break;
-		case FireResistantBoss.identityTag:
-			boss = new FireResistantBoss(mPlugin, (LivingEntity)targetEntity);
-			break;
-		case PulseLaserBoss.identityTag:
-			boss = new PulseLaserBoss(mPlugin, (LivingEntity)targetEntity);
-			break;
-		case ChargerBoss.identityTag:
-			boss = new ChargerBoss(mPlugin, (LivingEntity)targetEntity);
-			break;
-		case InfestedBoss.identityTag:
-			boss = new InfestedBoss(mPlugin, (LivingEntity)targetEntity);
-			break;
-		case CAxtal.identityTag:
-			boss = new CAxtal(mPlugin, (LivingEntity)targetEntity, targetEntity.getLocation(), endLoc);
-			break;
-		case Masked_1.identityTag:
-			boss = new Masked_1(mPlugin, (LivingEntity)targetEntity, targetEntity.getLocation(), endLoc);
-			break;
-		case Masked_2.identityTag:
-			boss = new Masked_2(mPlugin, (LivingEntity)targetEntity, targetEntity.getLocation(), endLoc);
-			break;
-		case Virius.identityTag:
-			boss = new Virius(mPlugin, (LivingEntity)targetEntity, targetEntity.getLocation(), endLoc);
-			break;
-		case Orangyboi.identityTag:
-			boss = new Orangyboi(mPlugin, (LivingEntity)targetEntity, targetEntity.getLocation(), endLoc);
-			break;
-		default:
+		Boss boss = null;
+		String requestedTag = args[0].toLowerCase();
+
+		StatelessBossConstructor stateless = mStatelessBosses.get(requestedTag);
+		if (stateless != null) {
+			boss = stateless.construct(mPlugin, (LivingEntity)targetEntity);
+		} else {
+			StatefulBossConstructor stateful = mStatefulBosses.get(requestedTag);
+			if (stateful != null) {
+				boss = stateful.construct(mPlugin, (LivingEntity)targetEntity, targetEntity.getLocation(), endLoc);
+			}
+		}
+
+		if (boss == null) {
 			send.sendMessage(ChatColor.RED + "Invalid boss name!");
-			send.sendMessage(ChatColor.RED + "Valid options are: [" + GenericBoss.identityTag + "," +
-			                 InvisibleBoss.identityTag + "," + FireResistantBoss.identityTag + "," + CAxtal.identityTag + "," +
-			                 Masked_1.identityTag + "," + Masked_2.identityTag + "," + InfestedBoss.identityTag + "," + ChargerBoss.identityTag + "," + PulseLaserBoss.identityTag + "," + Virius.identityTag + "," +
-							 Orangyboi.identityTag + "]");
+			send.sendMessage(ChatColor.RED + "Valid options are: [" + String.join(",", mBossDeserializers.keySet()) + "]");
 			return false;
 		}
 
@@ -166,36 +184,27 @@ public class BossManager implements Listener, CommandExecutor
 			Boss boss = null;
 			try
 			{
-				if (tags.contains(GenericBoss.identityTag))
-					boss = GenericBoss.deserialize(mPlugin, entity);
-				else if (tags.contains(InvisibleBoss.identityTag))
-					boss = InvisibleBoss.deserialize(mPlugin, entity);
-				else if (tags.contains(FireResistantBoss.identityTag))
-					boss = FireResistantBoss.deserialize(mPlugin, entity);
-				else if (tags.contains(CAxtal.identityTag))
-					boss = CAxtal.deserialize(mPlugin, entity);
-				else if (tags.contains(Masked_1.identityTag))
-					boss = Masked_1.deserialize(mPlugin, entity);
-				else if (tags.contains(Masked_2.identityTag))
-					boss = Masked_2.deserialize(mPlugin, entity);
-				else if (tags.contains(InfestedBoss.identityTag))
-					boss = InfestedBoss.deserialize(mPlugin, entity);
-				else if (tags.contains(ChargerBoss.identityTag))
-					boss = ChargerBoss.deserialize(mPlugin, entity);
-				else if (tags.contains(PulseLaserBoss.identityTag))
-					boss = PulseLaserBoss.deserialize(mPlugin, entity);
-				else if (tags.contains(Virius.identityTag))
-					boss = Virius.deserialize(mPlugin, entity);
-				else if (tags.contains(Orangyboi.identityTag))
-					boss = Orangyboi.deserialize(mPlugin, entity);
+				for (String tag : tags) {
+					BossDeserializer deserializer = mBossDeserializers.get(tag);
+					if (deserializer != null) {
+						boss = deserializer.deserialize(mPlugin, entity);
+						/* TODO
+						 *
+						 * Currently each boss can only have one boss ability. This is a fairly substantial limitation -
+						 * For example having a boss bar AND a charge attack isn't currently possible.
+						 *
+						 * Fixing this will require rethinking how the boss is tracked (map of a list of Boss?) and
+						 * also figuring out it works if one of the Boss types has nonempty serialization data
+						 */
+						mBosses.put(entity.getUniqueId(), boss);
+						break;
+					}
+				}
 			}
 			catch (Exception ex)
 			{
 				mPlugin.getLogger().log(Level.SEVERE, "Failed to load boss!", ex);
 			}
-
-			if (boss != null)
-				mBosses.put(entity.getUniqueId(), boss);
 		}
 	}
 
