c8849d8d6e0d6de04f91ee9491c34d4653ec9abe
Kaladun <kaladun@gmail.com>
2018-08-17 15:56:01 -0600

    Changed the numbers for loot scaling

--- src/main/java/com/playmonumenta/plugins/utils/ChestUtils.java
+++ src/main/java/com/playmonumenta/plugins/utils/ChestUtils.java
@@ -1,5 +1,7 @@
 package com.playmonumenta.plugins.utils;
 
+import java.util.Random;
+
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
@@ -10,7 +12,7 @@ import com.playmonumenta.plugins.managers.potion.PotionManager.PotionID;
 
 public class ChestUtils {
 
-	private static final int CHEST_LUCK_RADIUS = 64;
+	private static final int CHEST_LUCK_RADIUS = 128;
 
 	public static void chestScalingLuck(Plugin plugin, Player player, Block block) {
 		int chestLuck = ScoreboardUtils.getScoreboardValue(player, "ChestLuckToggle");
@@ -18,35 +20,34 @@ public class ChestUtils {
 			int playerCount = PlayerUtils.getNearbyPlayers(player.getLocation(), CHEST_LUCK_RADIUS).size();
 
 			int luckLevel;
-			switch (playerCount) {
-			case 0:
-				luckLevel = 0;
-				break;
-			case 1:
-				luckLevel = 0;
-				break;
-			case 2:
-				luckLevel = 1;
-				break;
-			case 3:
+
+			if (playerCount <= 1) {
+				luckLevel = -1;
+			} else if (playerCount == 2) {
+				Random mRandom = new Random();
+				double rand = mRandom.nextDouble();
+
+				if (rand < 0.6) {
+					luckLevel = 1;
+				} else {
+					luckLevel = 0;
+				}
+			} else if (playerCount == 3) {
+				luckLevel = 2;
+			} else {
 				luckLevel = 3;
-				break;
-			default:
-				luckLevel = 4;
-				break;
 			}
 
 			player.getPotionEffect(PotionEffectType.LUCK);
 
-			if (luckLevel > 0) {
+			if (luckLevel >= 0) {
 				plugin.mPotionManager.addPotion(player, PotionID.SAFE_ZONE, new PotionEffect(PotionEffectType.LUCK,
 				                                3, luckLevel, true, false));
 			}
 
 			int chestDebug = ScoreboardUtils.getScoreboardValue(player, "DEBUGChestLuck");
-			if (chestDebug > 0) {
-				int dll = luckLevel + 1;
-				MessagingUtils.sendActionBarMessage(plugin, player, "Generated " + dll + " extra items!");
+			if (chestDebug >= 0) {
+				MessagingUtils.sendActionBarMessage(plugin, player, playerCount + " players in range!");
 			}
 		}
 	}
