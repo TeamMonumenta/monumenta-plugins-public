7efeedca1e555afe3fc6a88fce7125e8e9e5c1d5
Byron Marohn <combustible@live.com>
2018-08-30 19:23:08 -0700

    Votifier debug
    Signed-off-by: Byron Marohn <combustible@live.com>

--- src/main/java/com/playmonumenta/plugins/integrations/VotifierIntegration.java
+++ src/main/java/com/playmonumenta/plugins/integrations/VotifierIntegration.java
@@ -5,6 +5,8 @@ import com.playmonumenta.plugins.Plugin;
 import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.model.VotifierEvent;
 
+import java.util.logging.Level;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -21,12 +23,18 @@ public class VotifierIntegration implements Listener {
     public void onVotifierEvent(VotifierEvent event) {
         Vote vote = event.getVote();
 
+		//TODO
+		mPlugin.getLogger().log(Level.WARNING, "Got vote: " + vote.toString());
+
 		//  Loop through all online players and find the matching one
 		//  and send them a message
 		for (Player player : mPlugin.getServer().getOnlinePlayers()) {
-			if (player.getName() == vote.getUsername()) {
+			if (player.getName().equals(vote.getUsername())) {
 				// TODO
 				player.sendMessage("Thanks for voting! Debug: " + vote.toString());
+			} else {
+				// TODO
+				player.sendMessage("Got vote from another player! Debug: " + vote.toString());
 			}
 		}
     }
