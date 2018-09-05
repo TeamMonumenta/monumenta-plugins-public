ff905fa65b3b3bcc9a982ba13c15fa325dadbe61
Byron Marohn <combustible@live.com>
2018-09-05 15:16:34 -0700

    Removed dead serialization code
    Signed-off-by: Byron Marohn <combustible@live.com>

--- src/main/java/com/playmonumenta/plugins/utils/InventoryUtils.java
+++ src/main/java/com/playmonumenta/plugins/utils/InventoryUtils.java
@@ -288,27 +288,6 @@ public class InventoryUtils {
 		        testForItemWithLore(item, "DN Key");
 	}
 
-	public static String toBase64(Inventory inventory) throws IllegalStateException {
-		try {
-			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-			BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
-
-			// Write the size of the inventory
-			dataOutput.writeInt(inventory.getSize());
-
-			// Save every element in the list
-			for (int i = 0; i < inventory.getSize(); i++) {
-				dataOutput.writeObject(inventory.getItem(i));
-			}
-
-			// Serialize that array
-			dataOutput.close();
-			return Base64Coder.encodeLines(outputStream.toByteArray());
-		} catch (Exception e) {
-			throw new IllegalStateException("Unable to save item stacks.", e);
-		}
-	}
-
 	public static String itemStackArrayToBase64(ItemStack[] items) throws IllegalStateException {
 		try {
 			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -327,24 +306,6 @@ public class InventoryUtils {
 			return Base64Coder.encodeLines(outputStream.toByteArray());
 		} catch (Exception e) {
 			throw new IllegalStateException("Unable to save item stacks.", e);
-		}
-	}
-
-	public static Inventory fromBase64(String data) throws IOException {
-		try {
-			ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
-			BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
-			Inventory inventory = Bukkit.getServer().createInventory(null, dataInput.readInt());
-
-			// Read the serialized inventory
-			for (int i = 0; i < inventory.getSize(); i++) {
-				inventory.setItem(i, (ItemStack) dataInput.readObject());
-			}
-
-			dataInput.close();
-			return inventory;
-		} catch (ClassNotFoundException e) {
-			throw new IOException("Unable to decode class type.", e);
 		}
 	}
 
