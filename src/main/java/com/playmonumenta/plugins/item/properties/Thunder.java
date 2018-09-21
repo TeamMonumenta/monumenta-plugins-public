238186d57bbdc2ea5a49ea59c4919af8315c9795
Byron Marohn <combustible@live.com>
2018-09-21 16:17:44 -0700

    Minor tweaks to Thunder Aspect
    Signed-off-by: Byron Marohn <combustible@live.com>

--- src/main/java/com/playmonumenta/plugins/item/properties/Thunder.java
+++ src/main/java/com/playmonumenta/plugins/item/properties/Thunder.java
@@ -1,22 +1,25 @@
 package com.playmonumenta.plugins.item.properties;
 
+import com.playmonumenta.plugins.item.properties.ItemPropertyManager.ItemSlot;
+import com.playmonumenta.plugins.Plugin;
+
 import java.util.EnumSet;
 import java.util.Random;
 
 import org.bukkit.ChatColor;
-import org.bukkit.World;
 import org.bukkit.entity.Guardian;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-
-import com.playmonumenta.plugins.Plugin;
-import com.playmonumenta.plugins.item.properties.ItemPropertyManager.ItemSlot;
+import org.bukkit.World;
 
 public class Thunder implements ItemProperty {
-	private static String PROPERTY_NAME = ChatColor.GRAY + "Thunder";
+	private static final String PROPERTY_NAME = ChatColor.GRAY + "Thunder Aspect";
+	private static final PotionEffect THUNDER_SLOWNESS = new PotionEffect(PotionEffectType.SLOW, 50, 8, false, true);
+	private static final PotionEffect THUNDER_WEAKNESS = new PotionEffect(PotionEffectType.WEAKNESS, 50, 8, false, true);
+	private Random mRandom = new Random();
 
 	@Override
 	public String getProperty() {
@@ -30,12 +33,11 @@ public class Thunder implements ItemProperty {
 
 	@Override
 	public double onAttack(Plugin plugin, World world, Player player, LivingEntity target, double damage, int level, DamageCause cause) {
-		Random mRandom = new Random();
 		double rand = mRandom.nextDouble();
 
 		if (damage >= 4 && rand < level * 0.1) {
-			target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 50, 8, false, true));
-			target.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 50, 8, false, true));
+			target.addPotionEffect(THUNDER_SLOWNESS);
+			target.addPotionEffect(THUNDER_WEAKNESS);
 
 			if (target instanceof Guardian) {
 				damage = damage + 1.0;
