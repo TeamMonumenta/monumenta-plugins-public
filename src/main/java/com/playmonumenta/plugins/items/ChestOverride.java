8713807f66b24df5663fa5e7eae5c12ba3e6183d
Byron Marohn <combustible@live.com>
2018-10-10 22:37:38 -0700

    Command/fancy trapped chests
    Signed-off-by: Byron Marohn <combustible@live.com>

--- src/main/java/com/playmonumenta/plugins/items/ChestOverride.java
+++ src/main/java/com/playmonumenta/plugins/items/ChestOverride.java
@@ -1,22 +1,29 @@
 package com.playmonumenta.plugins.items;
 
+import com.playmonumenta.plugins.Plugin;
+import com.playmonumenta.plugins.utils.ChestUtils;
+
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
+import org.bukkit.block.CommandBlock;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.GameMode;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.Location;
 import org.bukkit.Material;
-
-import com.playmonumenta.plugins.Plugin;
-import com.playmonumenta.plugins.utils.ChestUtils;
 
 public class ChestOverride extends OverrideItem {
 	@Override
 	public boolean rightClickBlockInteraction(Plugin plugin, Player player, Action action, ItemStack item, Block block) {
+		if (!command_chest(block)) {
+			return false;
+		}
+
 		if (player != null && player.getGameMode() != GameMode.SPECTATOR) {
 			ChestUtils.chestScalingLuck(plugin, player, block);
 		}
@@ -50,6 +57,9 @@ public class ChestOverride extends OverrideItem {
 	/* Chests placed on barriers can not be broken */
 	@Override
 	public boolean blockBreakInteraction(Plugin plugin, Player player, Block block) {
+		if (!command_chest(block)) {
+			return false;
+		}
 		if ((player.getGameMode() == GameMode.CREATIVE) || _breakable(block)) {
 			return true;
 		}
@@ -58,6 +68,9 @@ public class ChestOverride extends OverrideItem {
 
 	@Override
 	public boolean blockExplodeInteraction(Plugin plugin, Block block) {
+		if (!command_chest(block)) {
+			return false;
+		}
 		return _breakable(block);
 	}
 
@@ -66,7 +79,49 @@ public class ChestOverride extends OverrideItem {
 		if (blockUnder != null && blockUnder.getType() == Material.BARRIER) {
 			return false;
 		}
+		return true;
+	}
 
+	// If this returns false, the caller should also return false and stop processing the chest
+	private boolean command_chest(Block block) {
+		BlockState state = block.getState();
+		if (state != null && state instanceof Chest) {
+			Chest chest = (Chest)state;
+			String name = chest.getCustomName();
+			if (name != null && (name.toLowerCase().equals("trap") || name.toLowerCase().equals("function"))) {
+				// This is a function chest - run it!
+				// Run the first command block found in the 10 blocks under this block
+				Location loc = block.getLocation();
+				for (int y = 0; y < 10; y++) {
+					loc = loc.subtract(0, 1, 0);
+					Block testBlock = loc.getBlock();
+					BlockState testState = testBlock.getState();
+
+					if (testBlock.getType().equals(Material.COMMAND_BLOCK)
+					    && testState instanceof CommandBlock) {
+
+						// Run the command positioned at the chest block
+						String command = "execute positioned " +
+						                 Integer.toString((int)chest.getLocation().getX()) + " " +
+						                 Integer.toString((int)chest.getLocation().getY()) + " " +
+						                 Integer.toString((int)chest.getLocation().getZ()) + " run " +
+						                 ((CommandBlock)testState).getCommand();
+						Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
+						break;
+					}
+				}
+
+				if (name.toLowerCase().equals("trap")) {
+					// This was a trapped chest - clear its name and still let the player open it
+					chest.setCustomName(null);
+					chest.update();
+					return true;
+				} else {
+					// This was a function chest - don't let the player open it
+					return false;
+				}
+			}
+		}
 		return true;
 	}
 }
