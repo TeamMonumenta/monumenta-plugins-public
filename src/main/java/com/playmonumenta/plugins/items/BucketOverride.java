03fe5d1f2e616bd2d843508ea56d76bd94b263bb
Tim Southwick <NickNackGus@gmail.com>
2018-11-24 01:30:03 -0500

    Allow buckets in plots

--- src/main/java/com/playmonumenta/plugins/items/BucketOverride.java
+++ src/main/java/com/playmonumenta/plugins/items/BucketOverride.java
@@ -1,5 +1,9 @@
 package com.playmonumenta.plugins.items;
 
+import com.playmonumenta.plugins.Plugin;
+import com.playmonumenta.plugins.utils.LocationUtils;
+import com.playmonumenta.plugins.utils.LocationUtils.LocationType;
+
 import org.bukkit.GameMode;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -11,6 +15,12 @@ import com.playmonumenta.plugins.Plugin;
 public class BucketOverride extends OverrideItem {
 	@Override
 	public boolean rightClickItemInteraction(Plugin plugin, Player player, Action action, ItemStack item, Block block) {
-		return (player == null || player.getGameMode() == GameMode.CREATIVE);
+		if (player == null || player.getGameMode() == GameMode.CREATIVE) {
+			return true;
+		} else if (player.getGameMode() == GameMode.SURVIVAL && (LocationUtils.getLocationType(plugin, player) == LocationType.Capital)) {
+			return true;
+		}
+
+		return false;
 	}
 }
