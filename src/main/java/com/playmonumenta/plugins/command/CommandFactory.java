e410af99664a74861330caa081cb69fb8674f4b5
Byron Marohn <combustible@live.com>
2018-12-09 19:36:11 -0800

    Remove obsolete /checkemptyinventory
    Signed-off-by: Byron Marohn <combustible@live.com>

--- src/main/java/com/playmonumenta/plugins/command/CommandFactory.java
+++ src/main/java/com/playmonumenta/plugins/command/CommandFactory.java
@@ -19,7 +19,6 @@ public class CommandFactory {
 	 */
 	public static void createCommands(JavaPlugin plugin, ServerProperties properties, World world, PotionManager potionManager) {
 		if (Constants.COMMANDS_SERVER_ENABLED) {
-			createCommand(plugin, new CheckEmptyInventory(plugin));
 			createCommand(plugin, new DeathMsg(plugin));
 			createCommand(plugin, new GiveSoulbound(plugin));
 			createCommand(plugin, new IncrementDaily(plugin));
