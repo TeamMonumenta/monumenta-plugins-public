6074964bfa4f046a99cc73aa78ce5ed5a05dd394
Xinfu32 <frdrckq@gmail.com>
2019-03-17 11:55:11 -0400

    Sharpshooter fix

--- src/main/java/com/playmonumenta/plugins/abilities/scout/Sharpshooter.java
+++ src/main/java/com/playmonumenta/plugins/abilities/scout/Sharpshooter.java
@@ -14,15 +14,20 @@ import com.playmonumenta.plugins.abilities.Ability;
 import com.playmonumenta.plugins.utils.MessagingUtils;
 
 /*
- * - Sharpshooter:  Each successful arrow hit increases your arrow damage by +1,
- * up to a max of +5. This bonus lasts for 12s, and the time refreshes with each
- * arrow hit. At level 2, each hit increases damage by +2 up to +8, with a 15s
- * timer instead.
+ * Sharpshooter: Each successful, fully charged arrow hit increases your arrow
+ * damage by 1, up to a max of 5. After 4 seconds without a successful hit, the
+ * bonus is reduced by 1. At level 2, each hit increases damage by 2 up to 8.
  *
  * TODO: This damage buff
  * stacks with Enchanted Arrow and other bow skills.
  */
+
 public class Sharpshooter extends Ability {
+	private static final int SHARPSHOOTER_DECAY_TIMER = 20 * 4;
+	private static final int SHARPSHOOTER_1_MAX_BONUS = 5;
+	private static final int SHARPSHOOTER_2_MAX_BONUS = 8;
+	private static final int SHARPSHOOTER_1_INCREMENT = 1;
+	private static final int SHARPSHOOTER_2_INCREMENT = 2;
 
 	public Sharpshooter(Plugin plugin, World world, Random random, Player player) {
 		super(plugin, world, random, player);
@@ -30,6 +35,7 @@ public class Sharpshooter extends Ability {
 	}
 
 	private int sharpshot = 0;
+	private int t = 0;
 	private boolean volley = false;
 
 	@Override
@@ -45,43 +51,47 @@ public class Sharpshooter extends Ability {
 		if (!arrow.isCritical()) {
 			return true;
 		}
-		int sharpshooter = getAbilityScore();
-		int time = sharpshooter == 1 ? 20 * 12 : 20 * 15;
+
+		t = 0;
+
 		if (sharpshot <= 0) {
-			MessagingUtils.sendActionBarMessage(mPlugin, mPlayer, "You have begun to stack Sharpshooter!");
 			new BukkitRunnable() {
-				int t = 0;
 				@Override
 				public void run() {
 					t++;
 
-					if (t >= 20 * 4) {
-						sharpshot--;
+					if (t >= SHARPSHOOTER_DECAY_TIMER) {
 						t = 0;
-						if (sharpshot <= 0) {
-							MessagingUtils.sendActionBarMessage(mPlugin, mPlayer, "Your Sharpshooter stacks have expired");
-						}
+						sharpshot--;
+						MessagingUtils.sendActionBarMessage(mPlugin, mPlayer, "Sharpshooter bonus: " + sharpshot);
+					}
+
+					if (sharpshot <= 0) {
+						this.cancel();
 					}
 				}
 
 			}.runTaskTimer(mPlugin, 0, 1);
 		}
+
 		if (!volley) {
 			if (arrow.hasMetadata("Volley")) {
 				volley = true;
 			} else {
 				volley = false;
 			}
-			int max = sharpshooter == 1 ? 5 : 8;
-			if (sharpshot < max) {
-				if (sharpshooter > 1) {
-					sharpshot += 2;
-				} else if (sharpshooter > 0) {
-					sharpshot++;
-				}
+			int max = getAbilityScore() == 1 ? SHARPSHOOTER_1_MAX_BONUS : SHARPSHOOTER_2_MAX_BONUS;
+			int increment = getAbilityScore() == 1 ? SHARPSHOOTER_1_INCREMENT : SHARPSHOOTER_2_INCREMENT;
+			sharpshot += increment;
+			if (sharpshot > max) {
+				sharpshot = max;
 			}
+			MessagingUtils.sendActionBarMessage(mPlugin, mPlayer, "Sharpshooter bonus: " + sharpshot);
 		}
-		event.setDamage(event.getDamage() + sharpshot);
+
+		if (sharpshot > 0) {
+			event.setDamage(event.getDamage() + sharpshot);
+		}
 		return true;
 	}
 
