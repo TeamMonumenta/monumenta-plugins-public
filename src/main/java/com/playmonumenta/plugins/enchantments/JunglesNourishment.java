024aa691e7df58d0cf843f4943f6b420e80a207e
Byron Marohn <combustible@live.com>
2019-03-22 19:44:00 -0700

    Jungle's Nourishment apple cooldown increased to 20s
    Signed-off-by: Byron Marohn <combustible@live.com>

--- src/main/java/com/playmonumenta/plugins/enchantments/JunglesNourishment.java
+++ src/main/java/com/playmonumenta/plugins/enchantments/JunglesNourishment.java
@@ -35,15 +35,12 @@ public class JunglesNourishment implements BaseEnchantment {
 		if (InventoryUtils.testForItemWithLore(event.getItem(), "Jungle's Nourishment")) {
 			PlayerUtils.healPlayer(player, 8);
 			plugin.mPotionManager.addPotion(player, PotionID.ITEM,
-                    new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20 * 5,
-                                     0, true, true));
-			player.setCooldown(event.getItem().getType(), 20 * 8);
+                    new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20 * 5, 0, true, true));
+			player.setCooldown(event.getItem().getType(), 20 * 20);
 			player.setFoodLevel(24);
 			World world = player.getWorld();
-			world.spawnParticle(Particle.SPELL, player.getLocation().add(0, 1, 0), 20, 0.25, 0.5,
-					0.25, 1);
-			world.spawnParticle(Particle.SPELL_INSTANT, player.getLocation().add(0, 1, 0), 25, 0.5,
-					0.45, 0.25, 1);
+			world.spawnParticle(Particle.SPELL, player.getLocation().add(0, 1, 0), 20, 0.25, 0.5, 0.25, 1);
+			world.spawnParticle(Particle.SPELL_INSTANT, player.getLocation().add(0, 1, 0), 25, 0.5, 0.45, 0.25, 1);
 			world.playSound(player.getLocation(), Sound.ENTITY_ILLUSIONER_CAST_SPELL, 1, 1.25f);
 		}
 	}
