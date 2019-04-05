6b7b50f8a90e915af6c5b0cea1b3f839bd0b79f1
Byron Marohn <combustible@live.com>
2019-04-05 12:07:46 -0700

    Added missing boss scripts
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ plugins/bosses/upload-play.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR/target"
+
+plugin="$(ls -r MonumentaBosses_*.jar | grep -v sources | head -n 1)"
+if [[ -z "$plugin" ]]; then
+	exit 1
+fi
+
+echo "Plugin version: $plugin"
+
+scp -P 9922 $plugin play:/home/rock/project_epic/server_config/plugins/
+ssh -p 9922 play "cd /home/rock/project_epic/server_config/plugins && rm -f MonumentaBosses.jar ; ln -s $plugin MonumentaBosses.jar"
