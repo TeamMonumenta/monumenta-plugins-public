65a3412fc765368cd3058437f3399de0af2d5663
Byron Marohn <combustible@live.com>
2019-05-10 17:08:36 -0700

    Add vanish support to bungeecord messages
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugins/bungee/src/main/java/com/playmonumenta/bungeecord/listeners/EventListener.java
+++ plugins/bungee/src/main/java/com/playmonumenta/bungeecord/listeners/EventListener.java
@@ -1,29 +1,39 @@
 package com.playmonumenta.bungeecord.listeners;
 
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.Map;
 
 import com.playmonumenta.bungeecord.Main;
 import com.playmonumenta.bungeecord.utils.PacketUtils;
-import net.md_5.bungee.api.plugin.Listener;
-import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.ChatColor;
 
+import codecrafter47.bungeetablistplus.api.bungee.BungeeTabListPlusAPI;
+
+import de.myzelyam.api.vanish.BungeeVanishAPI;
+
+import fr.rhaz.socket4mc.Bungee.BungeeSocketHandshakeEvent;
+import fr.rhaz.socket4mc.Bungee.BungeeSocketJSONEvent;
+import fr.rhaz.socketapi.SocketAPI.Server.SocketMessenger;
+
+import net.md_5.bungee.api.Callback;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.PluginManager;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
-import fr.rhaz.socketapi.SocketAPI.Server.SocketMessenger;
-import fr.rhaz.socket4mc.Bungee.BungeeSocketHandshakeEvent;
-import fr.rhaz.socket4mc.Bungee.BungeeSocketJSONEvent;
-
-import net.md_5.bungee.api.Callback;
-import net.md_5.bungee.api.config.ServerInfo;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
-
 public class EventListener implements Listener {
+	final boolean mVanishEnabled;
+	final boolean mBungeeTabListPresent;
 
-	Main mMain;
+	final Main mMain;
 
 	// Collection of all connected sockets
 	private static ConcurrentHashMap<String, SocketMessenger> mSockets;
@@ -31,7 +41,86 @@ public class EventListener implements Listener {
 	public EventListener(Main main) {
 		mMain = main;
 		mSockets = new ConcurrentHashMap<>();
+
+		PluginManager mgr = mMain.getProxy().getPluginManager();
+
+		if (mgr.getPlugin("BungeeTabListPlus") != null) {
+			mMain.getLogger().info("Vanish support enabled - BungeeTabListPlus plugin detected");
+			mBungeeTabListPresent = true;
+		} else {
+			mMain.getLogger().info("Vanish support disabled - no plugin detected");
+			mBungeeTabListPresent = false;
+		}
+		if (mgr.getPlugin("PremiumVanish") != null) {
+			mMain.getLogger().info("Vanish support enabled - PremiumVanish plugin detected");
+			mVanishEnabled = true;
+		} else {
+			mMain.getLogger().info("Vanish support disabled - no plugin detected");
+			mVanishEnabled = false;
+		}
 	}
+
+	private void _joinLeaveEvent(ProxiedPlayer player, String operation, boolean isVanished) {
+		if (!isVanished) {
+			/* No vanish - send everyone the login message */
+
+			BaseComponent[] msg = new ComponentBuilder(player.getName())
+				.color(ChatColor.AQUA)
+				.append(operation)
+				.color(ChatColor.YELLOW)
+				.create();
+
+			for (ProxiedPlayer p : mMain.getProxy().getPlayers()) {
+				p.sendMessage(msg);
+			}
+
+		} else {
+			/*
+			 * Vanish is enabled and player joined vanished
+			 * Only send login message to other players that have perms to see it
+			 */
+			int useLevel = 0;
+			for (int i = 5; i > 0; i--) {
+				if (player.hasPermission("pv.use.level" + Integer.toString(i))) {
+					useLevel = i;
+					break;
+				}
+			}
+
+			BaseComponent[] msg = new ComponentBuilder(player.getName())
+				.color(ChatColor.AQUA)
+				.append(operation)
+				.color(ChatColor.YELLOW)
+				.append(" vanished")
+				.color(ChatColor.RED)
+				.create();
+
+			for (ProxiedPlayer p : mMain.getProxy().getPlayers()) {
+				int seeLevel = -1;
+				for (int i = 5; i > 0; i--) {
+					if (p.hasPermission("pv.see.level" + Integer.toString(i))) {
+						seeLevel = i;
+						break;
+					}
+				}
+				if (seeLevel >= useLevel) {
+					p.sendMessage(msg);
+				}
+			}
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+    public void postLoginEvent(PostLoginEvent event) {
+		_joinLeaveEvent(event.getPlayer(), " joined the game",
+		                mVanishEnabled && BungeeVanishAPI.isInvisible(event.getPlayer()));
+    }
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+    public void playerDisconnectEvent(PlayerDisconnectEvent event) {
+		_joinLeaveEvent(event.getPlayer(), " left the game",
+		                mBungeeTabListPresent && BungeeTabListPlusAPI.isHidden(event.getPlayer()));
+    }
 
 	// Every time a bungee server shard connects, add its socket to the list
 	@EventHandler(priority = EventPriority.LOWEST)
--- plugins/bungee/upload.sh
+++ plugins/bungee/upload.sh
@@ -3,12 +3,12 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR/target"
 
-plugin="$(ls -r Monumenta-Plugins_*.jar | grep -v sources | head -n 1)"
+plugin="$(ls -r MonumentaBungeecord-*.jar | grep -v sources | head -n 1)"
 if [[ -z "$plugin" ]]; then
 	exit 1
 fi
 
 echo "Plugin version: $plugin"
 
-scp -P 8822 $plugin rock@beta.playmonumenta.com:/home/rock/project_epic/server_config/plugins/
-ssh -p 8822 rock@beta.playmonumenta.com "cd /home/rock/project_epic/server_config/plugins && rm -f Monumenta-Plugins.jar ; ln -s $plugin Monumenta-Plugins.jar"
+scp -P 8822 $plugin rock@build.playmonumenta.com:/home/rock/project_epic/server_config/plugins/
+ssh -p 8822 rock@build.playmonumenta.com "cd /home/rock/project_epic/server_config/plugins && rm -f Monumenta-Bungee-Plugins.jar ; ln -s $plugin Monumenta-Bungee-Plugins.jar"
