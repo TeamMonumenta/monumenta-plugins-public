c51e12ef9dc3036f6d8d046bedcb88771a2dc6be
Byron Marohn <combustible@live.com>
2019-06-12 23:31:56 -0700

    Additional upload scripts
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ plugins/nms/upload-play2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR/target"
+
+plugin="$(ls -r MonumentaNMS_*.jar | grep -v sources | head -n 1)"
+if [[ -z "$plugin" ]]; then
+	exit 1
+fi
+
+echo "Plugin version: $plugin"
+
+scp -P 9922 $plugin build:/home/rock/project_epic/server_config/plugins/
+ssh -p 9922 build "cd /home/rock/project_epic/server_config/plugins && rm -f MonumentaNMS.jar ; ln -s $plugin MonumentaNMS.jar"
