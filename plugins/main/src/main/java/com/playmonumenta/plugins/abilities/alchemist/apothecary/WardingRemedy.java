de79158619daad98f5050105d711d735a9c6bbb8
Xinfu32 <frdrckq@gmail.com>
2019-08-28 02:05:05 -0400

    New Skill: Warding Remedy

---
+++ plugins/main/src/main/java/com/playmonumenta/plugins/abilities/alchemist/apothecary/WardingRemedy.java
@@ -0,0 +1,26 @@
+package com.playmonumenta.plugins.abilities.alchemist.apothecary;
+
+import java.util.Random;
+
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import com.playmonumenta.plugins.Plugin;
+import com.playmonumenta.plugins.abilities.Ability;
+
+/*
+ * WARDING REMEDY:
+ * You and allies in a 12 block radius gain 1 absorption health
+ * every 5 / 4 seconds, up to 6 absorption health. When an affected
+ * player has 4 or more absorption health from any source, gain an
+ * additional 10% / 20% damage on melee and ranged attacks.
+ */
+
+public class WardingRemedy extends Ability {
+
+	public WardingRemedy(Plugin plugin, World world, Random random, Player player) {
+		super(plugin, world, random, player);
+		mInfo.scoreboardId = "WardingRemedy";
+	}
+
+}
---
+++ plugins/main/src/main/java/com/playmonumenta/plugins/abilities/alchemist/apothecary/WardingRemedyNonApothecary.java
@@ -0,0 +1,124 @@
+package com.playmonumenta.plugins.abilities.alchemist.apothecary;
+
+import java.util.Random;
+
+import org.bukkit.World;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+import com.playmonumenta.plugins.Plugin;
+import com.playmonumenta.plugins.abilities.Ability;
+import com.playmonumenta.plugins.abilities.AbilityManager;
+import com.playmonumenta.plugins.utils.EntityUtils;
+import com.playmonumenta.plugins.utils.MetadataUtils;
+import com.playmonumenta.plugins.utils.PlayerUtils;
+
+/*
+ * Does the heavy lifting for WardingRemedy because we need to
+ * check the player-specific damage event and also prevent
+ * shield-stacking.
+ */
+
+public class WardingRemedyNonApothecary extends Ability {
+
+	private static final int REMEDY_RANGE = 12;
+	private static final int REMEDY_SHIELD = 1;
+	private static final int REMEDY_SHIELD_THRESHOLD = 4;
+	private static final int REMEDY_SHIELD_MAX = 6;
+	private static final int REMEDY_1_FREQUENCY = 5;				// Seconds
+	private static final int REMEDY_2_FREQUENCY = 4;				// Seconds
+	private static final double REMEDY_1_DAMAGE_MULTIPLIER = 1.1;
+	private static final double REMEDY_2_DAMAGE_MULTIPLIER = 1.2;
+
+	// Stored level so we don't have to run checks too often
+	private int mLevel = 0;
+	private int mSeconds = 0;
+	private AbsorptionManipulator mAbsorptionManipulator;
+
+	public WardingRemedyNonApothecary(Plugin plugin, World world, Random random, Player player) {
+		super(plugin, world, random, player);
+	}
+
+	@Override
+	public boolean canUse(Player player) {
+		return true;
+	}
+
+	@Override
+	public boolean LivingEntityDamagedByPlayerEvent(EntityDamageByEntityEvent event) {
+		if (event.getCause() == DamageCause.ENTITY_ATTACK
+			&& !MetadataUtils.happenedThisTick(mPlugin, mPlayer, EntityUtils.PLAYER_DEALT_CUSTOM_DAMAGE_METAKEY, 0)) {
+			mLevel = getWardingRemedyLevel();
+			if (mLevel > 0 && mAbsorptionManipulator.getAbsorption() > REMEDY_SHIELD_THRESHOLD) {
+				double multiplier = mLevel == 1 ? REMEDY_1_DAMAGE_MULTIPLIER : REMEDY_2_DAMAGE_MULTIPLIER;
+				event.setDamage(event.getDamage() * multiplier);
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean LivingEntityShotByPlayerEvent(Arrow arrow, LivingEntity damagee, EntityDamageByEntityEvent event) {
+		mLevel = getWardingRemedyLevel();
+		if (mLevel > 0 && mAbsorptionManipulator.getAbsorption() > REMEDY_SHIELD_THRESHOLD) {
+			double multiplier = mLevel == 1 ? REMEDY_1_DAMAGE_MULTIPLIER : REMEDY_2_DAMAGE_MULTIPLIER;
+			event.setDamage(event.getDamage() * multiplier);
+		}
+
+		return true;
+	}
+
+	@Override
+	public void PeriodicTrigger(boolean fourHertz, boolean twoHertz, boolean oneSecond, int ticks) {
+		if (oneSecond) {
+			mSeconds++;
+
+			if (mSeconds == REMEDY_2_FREQUENCY) {
+				// We can reuse this value for the next check if needed
+				mLevel = getWardingRemedyLevel();
+
+				if (mLevel == 2) {
+					mSeconds = 0;
+
+					if (mAbsorptionManipulator == null) {
+						mAbsorptionManipulator = new AbsorptionManipulator(mPlugin, mPlayer);
+					}
+
+					mAbsorptionManipulator.addAbsorption(REMEDY_SHIELD, REMEDY_SHIELD_MAX);
+				}
+			} else if (mSeconds >= REMEDY_1_FREQUENCY) {
+				mSeconds = 0;
+				if (mLevel > 0) {
+					if (mAbsorptionManipulator == null) {
+						mAbsorptionManipulator = new AbsorptionManipulator(mPlugin, mPlayer);
+					}
+
+					mAbsorptionManipulator.addAbsorption(REMEDY_SHIELD, REMEDY_SHIELD_MAX);
+				}
+			}
+		}
+	}
+
+	public int getWardingRemedyLevel() {
+		int level = 0;
+
+		for (Player player : PlayerUtils.getNearbyPlayers(mPlayer, REMEDY_RANGE, true)) {
+			Ability wr = AbilityManager.getManager().getPlayerAbility(player, WardingRemedy.class);
+			if (wr != null) {
+				int score = wr.getAbilityScore();
+				if (score == 2) {
+					return score;
+				} else if (score == 1) {
+					level = score;
+				}
+			}
+		}
+
+		return level;
+	}
+
+}
