7db28039c610d0bc3761359c21721264de8d5b64
Xinfu32 <frdrckq@gmail.com>
2019-08-28 02:03:26 -0400

    Helper class for more efficient absorption modification through reflection

---
+++ plugins/main/src/main/java/com/playmonumenta/plugins/abilities/alchemist/apothecary/AbsorptionManipulator.java
@@ -0,0 +1,67 @@
+package com.playmonumenta.plugins.abilities.alchemist.apothecary;
+
+import java.lang.NoSuchMethodException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import com.playmonumenta.plugins.Plugin;
+import com.playmonumenta.plugins.potion.PotionManager.PotionID;
+
+// This should probably be in a more general location, but for now only Apothecary uses it, so...
+
+public class AbsorptionManipulator {
+	private static final int MIN_ABSORPTION_DURATION = 20 * 30;
+
+	private Plugin plugin;
+	private Player player;
+	private Object handle;
+	private Method handleMethod;
+	private Method getAbsorptionMethod;
+	private Method setAbsorptionMethod;
+
+	public AbsorptionManipulator(Plugin plugin, Player player) {
+		this.plugin = plugin;
+		this.player = player;
+		try {
+			this.handleMethod = player.getClass().getMethod("getHandle");
+			this.handle = this.handleMethod.invoke(player);
+
+			this.getAbsorptionMethod = this.handle.getClass().getMethod("getAbsorptionHearts");
+			this.setAbsorptionMethod = this.handle.getClass().getMethod("setAbsorptionHearts", float.class);
+		} catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean isPlayerOnline() {
+		return player.isOnline();
+	}
+
+	// Doesn't work for subtracting absorption because newAbsorption makes sure it never drops (in case absorption is higher than maxAmount)
+	public void addAbsorption(double amount, double maxAmount) {
+		float absorption = getAbsorption();
+		float newAbsorption = (float) Math.max(absorption, Math.min(absorption + amount, maxAmount));
+		if (newAbsorption != absorption) {
+			try {
+				setAbsorptionMethod.invoke(handle, newAbsorption);
+			} catch (IllegalAccessException | InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public float getAbsorption() {
+		try {
+			return (Float) getAbsorptionMethod.invoke(handle);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+}
