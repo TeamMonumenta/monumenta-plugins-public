bbfa10424dc2f43250da51a18c74edc933d6388a
Byron Marohn <combustible@live.com>
2019-09-06 17:39:46 -0700

    Add a new NMS API for dealing custom damage
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ plugins/nmsapi/src/main/java/com/playmonumenta/nms/utils/NmsEntityUtils.java
@@ -0,0 +1,14 @@
+package com.playmonumenta.nms.utils;
+
+import org.bukkit.entity.Player;
+
+public class NmsEntityUtils {
+	public static void customDamageEntity(org.bukkit.entity.LivingEntity entity, double amount, Player damager) {
+
+	}
+
+	public static void customDamageEntity(org.bukkit.entity.LivingEntity entity, double amount, Player damager, String killedUsingMsg) {
+
+	}
+}
+
