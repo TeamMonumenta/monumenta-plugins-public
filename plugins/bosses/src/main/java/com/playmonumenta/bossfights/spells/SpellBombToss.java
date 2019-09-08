512caebb4368e419b087976743240fc174cdb5f0
Byron Marohn <combustible@live.com>
2019-09-08 11:04:20 -0700

    Improve boss_bombtoss ability
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugins/bosses/src/main/java/com/playmonumenta/bossfights/spells/SpellBombToss.java
+++ plugins/bosses/src/main/java/com/playmonumenta/bossfights/spells/SpellBombToss.java
@@ -3,10 +3,10 @@ package com.playmonumenta.bossfights.spells;
 import java.util.Collections;
 import java.util.List;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -16,10 +16,12 @@ import com.playmonumenta.bossfights.utils.Utils;
 
 public class SpellBombToss extends Spell {
 
+	private final Plugin mPlugin;
 	private final LivingEntity mBoss;
 	private final int mRange;
 
 	public SpellBombToss(Plugin plugin, LivingEntity boss, int range) {
+		mPlugin = plugin;
 		mBoss = boss;
 		mRange = range;
 	}
@@ -43,15 +45,19 @@ public class SpellBombToss extends Spell {
 	}
 
 	public void launch(Player target) {
-		Location SLoc = mBoss.getLocation();
-		SLoc.setY(SLoc.getY() + 1.7f);
-		SLoc.getWorld().playSound(SLoc, Sound.ENTITY_EVOKER_CAST_SPELL, 1, 1);
-		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "summon tnt " + SLoc.getX() + " " + SLoc.getY() + " " + SLoc.getZ() + " {Fuse:50}");
-		List<Entity> tnt = mBoss.getNearbyEntities(0.2, 2.5, 0.2);
-		Location pLoc = target.getLocation();
-		Location tLoc = tnt.get(0).getLocation();
-		Vector vect = new Vector(pLoc.getX() - tLoc.getX(), 0, pLoc.getZ() - tLoc.getZ());
-		vect.normalize().multiply((pLoc.distance(tLoc)) / 20).setY(0.7f);
-		tnt.get(0).setVelocity(vect);
+		Location sLoc = mBoss.getLocation();
+		sLoc.setY(sLoc.getY() + 1.7f);
+		sLoc.getWorld().playSound(sLoc, Sound.ENTITY_EVOKER_CAST_SPELL, 1, 1);
+		try {
+			Entity tnt = Utils.summonEntityAt(sLoc, EntityType.PRIMED_TNT, "{Fuse:50}");
+			Location pLoc = target.getLocation();
+			Location tLoc = tnt.getLocation();
+			Vector vect = new Vector(pLoc.getX() - tLoc.getX(), 0, pLoc.getZ() - tLoc.getZ());
+			vect.normalize().multiply((pLoc.distance(tLoc)) / 20).setY(0.7f);
+			tnt.setVelocity(vect);
+		} catch (Exception e) {
+			mPlugin.getLogger().warning("Failed to summon TNT for bomb toss: " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
