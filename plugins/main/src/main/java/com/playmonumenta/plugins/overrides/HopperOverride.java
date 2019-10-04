d8c8abf835991393a97e2b9a3bddf1d71e2a0cf6
Byron Marohn <combustible@live.com>
2019-10-03 20:43:43 -0700

    Hoppers can't be broken or exploded by non-creative players
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugins/main/src/main/java/com/playmonumenta/plugins/overrides/HopperOverride.java
+++ plugins/main/src/main/java/com/playmonumenta/plugins/overrides/HopperOverride.java
@@ -1,6 +1,7 @@
 package com.playmonumenta.plugins.overrides;
 
 import org.bukkit.GameMode;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
@@ -10,9 +11,20 @@ import com.playmonumenta.plugins.Plugin;
 public class HopperOverride extends BaseOverride {
 	@Override
 	public boolean blockPlaceInteraction(Plugin plugin, Player player, ItemStack item, BlockPlaceEvent event) {
-		if (player != null && player.getGameMode() != GameMode.CREATIVE) {
+		if (player != null && !player.getGameMode().equals(GameMode.CREATIVE)) {
 			return false;
 		}
 		return true;
+	}
+
+	public boolean blockBreakInteraction(Plugin plugin, Player player, Block block) {
+		if (player != null && !player.getGameMode().equals(GameMode.CREATIVE)) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean blockExplodeInteraction(Plugin plugin, Block block) {
+		return false;
 	}
 }
