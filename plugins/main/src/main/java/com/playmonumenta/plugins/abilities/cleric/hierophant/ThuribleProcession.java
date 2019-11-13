1d41aa66b05b5b0347eeb14d0fe763f04a3409b2
qooqoomagic <qoomagicxd@gmail.com>
2019-11-12 22:58:02 -0500

    Thurible Procession Rework

--- plugins/main/src/main/java/com/playmonumenta/plugins/abilities/cleric/hierophant/ThuribleProcession.java
+++ plugins/main/src/main/java/com/playmonumenta/plugins/abilities/cleric/hierophant/ThuribleProcession.java
@@ -3,25 +3,20 @@ package com.playmonumenta.plugins.abilities.cleric.hierophant;
 import java.util.List;
 import java.util.Random;
 
-import org.bukkit.Color;
-import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
-import org.bukkit.event.block.Action;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import com.playmonumenta.plugins.Plugin;
 import com.playmonumenta.plugins.abilities.Ability;
-import com.playmonumenta.plugins.abilities.AbilityTrigger;
 import com.playmonumenta.plugins.classes.Spells;
 import com.playmonumenta.plugins.potion.PotionManager.PotionID;
-import com.playmonumenta.plugins.utils.MetadataUtils;
+import com.playmonumenta.plugins.utils.EntityUtils;
 import com.playmonumenta.plugins.utils.PlayerUtils;
 
 /*
@@ -43,166 +38,86 @@ import com.playmonumenta.plugins.utils.PlayerUtils;
  */
 public class ThuribleProcession extends Ability {
 
-	private static final String CHECK_ONCE_THIS_TICK_METAKEY = "ThuribleTickRightClicked";
-
-	private static final Particle.DustOptions THURIBLE_COLOR = new Particle.DustOptions(Color.fromRGB(255, 255, 175), 1.0f);
-	private static final float DEFAULT_WALK_SPEED = 0.2f;
-	private static final float THURIBLE_1_WALK_SPEED = 0.8f;
-	private static final float THURIBLE_2_WALK_SPEED = 1.0f;
-	private static final int THURIBLE_THRESHOLD_0 = 20 * 0;
-	private static final int THURIBLE_THRESHOLD_1 = 20 * 2;
-	private static final int THURIBLE_THRESHOLD_2 = 20 * 4;
-	private static final int THURIBLE_THRESHOLD_3 = 20 * 6;
-	private static final int THURIBLE_1_THRESHOLD = 20 * 15;
-	private static final int THURIBLE_2_THRESHOLD = 20 * 10;
-	private static final int THURIBLE_1_BUFF_DURATION = 20 * 15;
-	private static final int THURIBLE_2_BUFF_DURATION = 20 * 20;
+	private static final int EFFECTS_DURATION = 20 * 8;
 	private static final int THURIBLE_1_RADIUS = 20;
 	private static final int THURIBLE_2_RADIUS = 30;
+	private static final int THURIBLE_COOLDOWN = 8;
+	private static final PotionEffectType[] EFFECTS = new PotionEffectType[] {PotionEffectType.FAST_DIGGING, PotionEffectType.SPEED, PotionEffectType.INCREASE_DAMAGE, PotionEffectType.DAMAGE_RESISTANCE};
 
-	private Location mLastLocation;
-	private int mRightClicks = 0;
+	private int seconds = 0;
+	private int buffs = 0;
 
 	public ThuribleProcession(Plugin plugin, World world, Random random, Player player) {
 		super(plugin, world, random, player);
 		mInfo.scoreboardId = "Thurible";
-		mInfo.trigger = AbilityTrigger.RIGHT_CLICK;
-		mInfo.cooldown = 20 * 5; // Just to prevent spamming
+		mInfo.cooldown = 20 * THURIBLE_COOLDOWN;
 		mInfo.linkedSpell = Spells.THURIBLE_PROCESSION;
 	}
 
 	@Override
-	public void cast(Action action) {
-		// Prevent two right clicks being registered from one action (e.g. blocking)
-		if (MetadataUtils.checkOnceThisTick(mPlugin, mPlayer, CHECK_ONCE_THIS_TICK_METAKEY)) {
-			mRightClicks++;
+	public boolean LivingEntityDamagedByPlayerEvent(EntityDamageByEntityEvent event) {
 
-			// This timer makes sure that the player actually blocked instead of some other right click interaction
-			new BukkitRunnable() {
-				@Override
-				public void run() {
-					if (!mPlayer.isHandRaised() && mRightClicks > 0) {
-						mRightClicks--;
-					}
-					this.cancel();
-				}
-			}.runTaskLater(mPlugin, 1);
-
-			new BukkitRunnable() {
-				@Override
-				public void run() {
-					if (mRightClicks > 0) {
-						mRightClicks--;
-					}
-					this.cancel();
-				}
-			}.runTaskLater(mPlugin, 5);
+		//If on cooldown, do not cast
+		if (!canCast()) {
+			return true;
 		}
 
-		if (mRightClicks >= 2) {
-			mRightClicks = 0;
-			float speed = getAbilityScore() == 1 ? THURIBLE_1_WALK_SPEED : THURIBLE_2_WALK_SPEED;
-			mPlayer.getWorld().playSound(mPlayer.getLocation(), Sound.ENTITY_EVOKER_PREPARE_SUMMON, 1, 1);
-			new BukkitRunnable() {
-				int t = 0;
-				double rot = 0;
-				int strands = 3;
-				double radius = 3;
-				@Override
-				public void run() {
-					if (t == 1) {
-						mPlayer.setWalkSpeed(speed);
-					}
-					runParticles(rot, strands, radius, t);
-					if (t % 20 == 0) {
-						radius = 3;
-					}
-					rot += 10;
-					radius -= 0.1428;
+		if (buffs > 0 && event.getCause() == DamageCause.ENTITY_ATTACK && EntityUtils.isHostileMob(event.getEntity())) {
 
-					int duration = getAbilityScore() == 1 ? THURIBLE_1_BUFF_DURATION : THURIBLE_2_BUFF_DURATION;
-					int radius = getAbilityScore() == 1 ? THURIBLE_1_RADIUS : THURIBLE_2_RADIUS;
-					int threshold = getAbilityScore() == 1 ? THURIBLE_1_THRESHOLD : THURIBLE_2_THRESHOLD;
+			updateBuffs();
 
-					// Apply periodic buffs
-					List<Player> players = PlayerUtils.getNearbyPlayers(mPlayer, radius, false);
-					for (Player pl : players) {
-						if (t > THURIBLE_THRESHOLD_0) {
-							mPlugin.mPotionManager.addPotion(pl, PotionID.ABILITY_OTHER, new PotionEffect(PotionEffectType.FAST_DIGGING, 40, 0, true, true));
-						}
-						if (t > THURIBLE_THRESHOLD_1) {
-							mPlugin.mPotionManager.addPotion(pl, PotionID.ABILITY_OTHER, new PotionEffect(PotionEffectType.SPEED, 40, 0, true, true));
-						}
-						if (t > THURIBLE_THRESHOLD_2) {
-							mPlugin.mPotionManager.addPotion(pl, PotionID.ABILITY_OTHER, new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 40, 0, true, true));
-						}
-						if (t > THURIBLE_THRESHOLD_3) {
-							mPlugin.mPotionManager.addPotion(pl, PotionID.ABILITY_OTHER, new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 40, 0, true, true));
-						}
-					}
+			int radius = getAbilityScore() == 1 ? THURIBLE_1_RADIUS : THURIBLE_2_RADIUS;
 
-					// Apply end buffs - we set walk speed slow one tick earlier
-					// so the player isn't launched forward
-					if (t == threshold - 1) {
-						mPlayer.setWalkSpeed(DEFAULT_WALK_SPEED);
-					}
-					if (t >= threshold) {
-						for (Player pl : PlayerUtils.getNearbyPlayers(mPlayer, radius, true)) {
-							mPlugin.mPotionManager.addPotion(pl, PotionID.ABILITY_OTHER, new PotionEffect(PotionEffectType.FAST_DIGGING, duration, 0, true, true));
-							mPlugin.mPotionManager.addPotion(pl, PotionID.ABILITY_OTHER, new PotionEffect(PotionEffectType.SPEED, duration, 0, true, true));
-							mPlugin.mPotionManager.addPotion(pl, PotionID.ABILITY_OTHER, new PotionEffect(PotionEffectType.INCREASE_DAMAGE, duration, 0, true, true));
-							mPlugin.mPotionManager.addPotion(pl, PotionID.ABILITY_OTHER, new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, duration, 0, true, true));
-						}
-						this.cancel();
-						putOnCooldown();
-						mPlayer.getWorld().playSound(mPlayer.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, 1, 1);
-						mWorld.spawnParticle(Particle.FIREWORKS_SPARK, mPlayer.getLocation(), 60, 0, 0, 0, 0.35);
-						mWorld.spawnParticle(Particle.SPELL_INSTANT, mPlayer.getLocation(), 60, 0.4, 0.4, 0.4, 1);
-						mWorld.spawnParticle(Particle.SPELL_INSTANT, mPlayer.getLocation(), 200, 5, 3, 5, 1);
-					}
-
-					// Cancel if player stops blocking
-					// Teleport player back to where they were 1 tick ago
-					// to prevent them from walking off a cliff
-					if (t > 0) {
-						if (!mPlayer.isHandRaised() && !mPlayer.isBlocking() || mPlayer.isDead()) {
-							this.cancel();
-							putOnCooldown();
-							mPlayer.setWalkSpeed(DEFAULT_WALK_SPEED);
-							mPlayer.teleport(mLastLocation);
-						}
-					}
-
-					mLastLocation = mPlayer.getLocation();
-					t++;
+			//Give everyone buffs from the array
+			List<Player> players = PlayerUtils.getNearbyPlayers(mPlayer, radius, true);
+			for (Player pl : players) {
+				for (int i = 0; i < buffs; i++) {
+					mPlugin.mPotionManager.addPotion(pl, PotionID.ABILITY_OTHER, new PotionEffect(EFFECTS[i], EFFECTS_DURATION, 0, true, true));
 				}
+			}
 
-			}.runTaskTimer(mPlugin, 0, 1);
+			mPlayer.getWorld().playSound(mPlayer.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, 1, 1);
+			mWorld.spawnParticle(Particle.FIREWORKS_SPARK, mPlayer.getLocation(), 60, 0, 0, 0, 0.35);
+			mWorld.spawnParticle(Particle.SPELL_INSTANT, mPlayer.getLocation(), 60, 0.4, 0.4, 0.4, 1);
+			mWorld.spawnParticle(Particle.SPELL_INSTANT, mPlayer.getLocation(), 200, 5, 3, 5, 1);
+
+			//Reset timer and have 8 second cooldown
+			seconds = 0;
+			putOnCooldown();
 		}
+		return true;
 	}
 
 	@Override
-	public boolean runCheck() {
-		ItemStack mHand = mPlayer.getInventory().getItemInMainHand();
-		ItemStack oHand = mPlayer.getInventory().getItemInOffHand();
-		return ((mHand != null && mHand.getType() == Material.SHIELD && oHand.getType() != Material.BOW) ||
-		        (oHand != null && oHand.getType() == Material.SHIELD && mHand.getType() != Material.BOW))
-		       && !mPlayer.isSneaking();
+	public void PeriodicTrigger(boolean fourHertz, boolean twoHertz, boolean oneSecond, int ticks) {
+		if (!canCast()) {
+			return;
+		}
+		if (oneSecond) {
+			seconds++;
+			updateBuffs();
+		}
+		if (fourHertz) {
+			for (int i = 0; i < buffs; i++) {
+				Particle.DustOptions color = new Particle.DustOptions(EFFECTS[i].getColor(), 1);
+				mWorld.spawnParticle(Particle.REDSTONE, mPlayer.getLocation(), 1, 0.4, 0.4, 0.4, color);
+			}
+		}
 	}
 
-	public void runParticles(double rot, int strands, double radius, int t) {
-		double rotAdd = 360 / strands;
-		Location ploc = mPlayer.getLocation();
-		for (int i = 0; i < strands; i++) {
-			double radian = Math.toRadians(rot + (rotAdd * i));
-			ploc.add(Math.cos(radian)*radius, 0, Math.sin(radian)*radius);
-			mWorld.spawnParticle(Particle.REDSTONE, ploc, 15, 0.1, 0.1, 0.1, THURIBLE_COLOR);
-			mWorld.spawnParticle(Particle.SPELL_INSTANT, ploc, 5, 0.1f, 0.1f, 0.1f, 0);
-			ploc.subtract(Math.cos(radian)*radius, 0, Math.sin(radian)*radius);
-		}
-		if (t % 20 == 0) {
-			mPlayer.getWorld().playSound(mPlayer.getLocation(), Sound.ENTITY_ILLUSIONER_PREPARE_BLINDNESS, 0.85f, 1.15f);
-			mWorld.spawnParticle(Particle.SPELL_INSTANT, mPlayer.getLocation().add(0, 1.15, 40), 15, 2, 0.4f, 2, 0);
+	//Recounts number of buffs
+	private void updateBuffs() {
+
+		//Convert time into number of buffs and cap to maximum effect index for that level
+		buffs = getAbilityScore() == 1 ? seconds / 5 : seconds / 3;
+
+		//If level 2, cap to 4 effects, if level 1, cap to 3 effects
+		if (buffs > 3) {
+			if (getAbilityScore() != 1) {
+				buffs = 4;
+			} else {
+				buffs = 3;
+			}
 		}
 	}
 
