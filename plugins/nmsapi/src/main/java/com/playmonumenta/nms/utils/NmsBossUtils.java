909976436eba5b5de7817ac38e395faa496c3c1e
Byron Marohn <combustible@live.com>
2019-12-22 18:34:04 -0800

    Remove nmsapi
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugins/nmsapi/src/main/java/com/playmonumenta/nms/utils/NmsBossUtils.java
+++
@@ -1,9 +0,0 @@
-package com.playmonumenta.nms.utils;
-
-import org.bukkit.entity.LivingEntity;
-
-public class NmsBossUtils {
-	public static void removeVexSpawnAIFromEvoker(LivingEntity boss) {
-
-	}
-}
