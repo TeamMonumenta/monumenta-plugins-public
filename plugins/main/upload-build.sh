45b87468d401b1e6f1f6fb689b4398265e85a754
Byron Marohn <combustible@live.com>
2019-12-22 19:08:45 -0800

    Rename MonumentaMain -> Monumenta
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugins/main/upload-build.sh
+++ plugins/main/upload-build.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR/target"
 
-plugin="$(ls -r MonumentaMain_*.jar | grep -v sources | head -n 1)"
+plugin="$(ls -r Monumenta_*.jar | grep -v sources | head -n 1)"
 if [[ -z "$plugin" ]]; then
 	exit 1
 fi
@@ -11,4 +11,4 @@ fi
 echo "Plugin version: $plugin"
 
 scp -P 9922 $plugin epic@build.playmonumenta.com:/home/epic/project_epic/server_config/plugins/
-ssh -p 9922 epic@build.playmonumenta.com "cd /home/epic/project_epic/server_config/plugins && rm -f MonumentaMain.jar ; ln -s $plugin MonumentaMain.jar"
+ssh -p 9922 epic@build.playmonumenta.com "cd /home/epic/project_epic/server_config/plugins && rm -f Monumenta.jar ; ln -s $plugin Monumenta.jar"
