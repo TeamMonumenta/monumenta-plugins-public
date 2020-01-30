8da4dcff27b779107e5adeb142afa7d20b806ba5
Byron Marohn <combustible@live.com>
2020-01-29 17:01:55 -0800

    Add CoreProtect dependency, currently unused
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/integrations/CoreProtectIntegration.java
@@ -0,0 +1,40 @@
+package com.playmonumenta.plugins.integrations;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
+
+public class CoreProtectIntegration {
+	private static CoreProtectAPI getCoreProtect() {
+		Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("CoreProtect");
+
+		// Check that CoreProtect is loaded
+		if (plugin == null || !(plugin instanceof CoreProtect)) {
+			return null;
+		}
+
+		// Check that the API is enabled
+		CoreProtectAPI CoreProtect = ((CoreProtect) plugin).getAPI();
+		if (CoreProtect.isEnabled() == false) {
+			return null;
+		}
+
+		// Check that a compatible version of the API is loaded
+		if (CoreProtect.APIVersion() < 6) {
+			return null;
+		}
+
+		return CoreProtect;
+	}
+
+	public void logContainerTransaction(Player player, Location location) {
+		CoreProtectAPI api = getCoreProtect();
+		if (api != null) {
+			api.logContainerTransaction(player.getName(), location);
+		}
+	}
+}
