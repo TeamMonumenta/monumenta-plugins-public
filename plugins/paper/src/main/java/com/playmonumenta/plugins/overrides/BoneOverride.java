0d579ce21faea48463fa7ae284e747302d4780ef
EnragedRabisu <kydawg7@hotmail.com>
2020-02-07 20:19:23 -0500

    Bone-based items can right click Entities other than Wolves (#87)
    Added a check to see if the clickedEntity is a Wolf and the item has metadata or lore.

--- plugins/paper/src/main/java/com/playmonumenta/plugins/overrides/BoneOverride.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/overrides/BoneOverride.java
@@ -3,6 +3,7 @@ package com.playmonumenta.plugins.overrides;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Wolf;
 import org.bukkit.inventory.ItemStack;
 
 import com.playmonumenta.plugins.Plugin;
@@ -19,6 +20,6 @@ public class BoneOverride extends BaseOverride {
 		}
 
 		// Don't allow non-creative players to feed bones with lore text to wolves
-		return (player.getGameMode() == GameMode.CREATIVE || item == null || !item.hasItemMeta() || !item.getItemMeta().hasLore());
+		return (player.getGameMode() == GameMode.CREATIVE || clickedEntity == null || !(clickedEntity instanceof Wolf && item != null && (item.hasItemMeta() || item.getItemMeta().hasLore())));
 	}
 }
