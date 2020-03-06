018ded3efc08ebfefe66e9fe4359d72e04cce576
CSAristotle <61561049+CSAristotle@users.noreply.github.com>
2020-03-06 15:08:23 -0600

    Enchanted Prayer Description Update
    Clarified damage is done in a 3-block radius.

--- plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/cleric/hierophant/EnchantedPrayer.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/cleric/hierophant/EnchantedPrayer.java
@@ -42,7 +42,7 @@ public class EnchantedPrayer extends Ability {
 		super(plugin, world, random, player, "Enchanted Prayer");
 		mInfo.scoreboardId = "EPrayer";
 		mInfo.mShorthandName = "EP";
-		mInfo.mDescriptions.add("Right-clicking in the air while shifted enchants the weapons of all players in a 15 block radius with holy magic. Their next melee attack deals an additional 7 damage while healing the player for 2 hp. Cooldown: 18s.");
+		mInfo.mDescriptions.add("Right-clicking in the air while shifted enchants the weapons of all players in a 15 block radius with holy magic. Their next melee attack deals an additional 7 damage in a 3-block radius while healing the player for 2 hp. Cooldown: 18s.");
 		mInfo.mDescriptions.add("Damage is increased to 12. Healing is increased to 4 hp.");
 		mInfo.linkedSpell = Spells.ENCHANTED_PRAYER;
 		mInfo.trigger = AbilityTrigger.RIGHT_CLICK;
