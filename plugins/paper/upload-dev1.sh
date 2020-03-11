584ec77615a0597d98e29e42618b7c1c4aea4100
Byron Marohn <combustible@live.com>
2020-03-11 15:59:51 -0700

    Add upload scripts for dev1 / dev2
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ plugins/paper/upload-dev1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR/target"
+
+plugin="$(ls -r Monumenta_*.jar | grep -v sources | head -n 1)"
+if [[ -z "$plugin" ]]; then
+	exit 1
+fi
+
+echo "Plugin version: $plugin"
+
+ssh -p 8822 epic@build.playmonumenta.com "cd /home/epic/dev1_shard_plugins && rm -f Monumenta*.jar"
+scp -P 8822 $plugin epic@build.playmonumenta.com:/home/epic/dev1_shard_plugins/
