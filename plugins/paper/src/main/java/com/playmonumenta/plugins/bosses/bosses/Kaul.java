70654279254aa6cdabc211f4ed15a220ee256e50
Fwap a Durp <16434132+FwapaDurp@users.noreply.github.com>
2020-04-03 03:26:19 +0100

    Kaul changes, especially block break (#182)
    * nerfed earth elemental damage 
    
    * block break is now more narrow and destroy most blocks, will also make stairs if kaul is below arena
    
    * removed random files that does literally nothing in root

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/Kaul.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/Kaul.java
@@ -246,7 +246,7 @@ public class Kaul extends BossAbilityGroup {
 		});
 
 		List<Spell> passiveSpells = Arrays.asList(
-			new SpellKaulBlockBreak(mBoss),
+			new SpellKaulBlockBreak(mBoss, Material.WATER),
 			new SpellBaseParticleAura(boss, 1, (LivingEntity mBoss) -> {
 				world.spawnParticle(Particle.FALLING_DUST, mBoss.getLocation().add(0, mBoss.getHeight() / 2, 0), 8, 0.35,
 				0.45, 0.35, Material.GREEN_CONCRETE.createBlockData());
@@ -262,7 +262,7 @@ public class Kaul extends BossAbilityGroup {
 		);
 
 		List<Spell> phase2PassiveSpells = Arrays.asList(
-			new SpellKaulBlockBreak(mBoss),
+			new SpellKaulBlockBreak(mBoss, Material.WATER),
 			new SpellBaseParticleAura(boss, 1, (LivingEntity mBoss) -> {
 				world.spawnParticle(Particle.FALLING_DUST, mBoss.getLocation().add(0, mBoss.getHeight() / 2, 0), 8, 0.35,
 				0.45, 0.35, Material.GREEN_CONCRETE.createBlockData());
@@ -278,7 +278,7 @@ public class Kaul extends BossAbilityGroup {
 		);
 
 		List<Spell> phase3PassiveSpells = Arrays.asList(
-			new SpellKaulBlockBreak(mBoss),
+			new SpellKaulBlockBreak(mBoss, Material.WATER),
 			new SpellBaseParticleAura(boss, 1, (LivingEntity mBoss) -> {
 				world.spawnParticle(Particle.FALLING_DUST, mBoss.getLocation().add(0, mBoss.getHeight() / 2, 0), 2, 0.35,
 				0.45, 0.35, Material.GREEN_CONCRETE.createBlockData());
@@ -300,7 +300,7 @@ public class Kaul extends BossAbilityGroup {
 		);
 
 		List<Spell> phase4PassiveSpells = Arrays.asList(
-			new SpellKaulBlockBreak(mBoss),
+			new SpellKaulBlockBreak(mBoss, Material.WATER),
 			new SpellBaseParticleAura(boss, 1, (LivingEntity mBoss) -> {
 				world.spawnParticle(Particle.FALLING_DUST, mBoss.getLocation().add(0, mBoss.getHeight() / 2, 0), 2, 0.35,
 				0.45, 0.35, Material.GREEN_CONCRETE.createBlockData());
--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/kaul/SpellKaulBlockBreak.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/kaul/SpellKaulBlockBreak.java
@@ -1,7 +1,6 @@
 package com.playmonumenta.plugins.bosses.spells.kaul;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
 
@@ -11,10 +10,10 @@ import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
+import org.bukkit.block.Container;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.EntityExplodeEvent;
-import org.bukkit.loot.Lootable;
 import org.bukkit.util.Vector;
 
 import com.playmonumenta.plugins.bosses.spells.Spell;
@@ -23,17 +22,7 @@ import com.playmonumenta.plugins.utils.VectorUtils;
 
 public class SpellKaulBlockBreak extends Spell {
 	private LivingEntity mBoss;
-	private List<Material> mNoBreak;
-
-	public SpellKaulBlockBreak(LivingEntity boss) {
-		mBoss = boss;
-		mNoBreak = new ArrayList<Material>();
-	}
-
-	public SpellKaulBlockBreak(LivingEntity launcher, Material... noBreak) {
-		mBoss = launcher;
-		mNoBreak = Arrays.asList(noBreak);
-	}
+	private int mINC = 0;
 
 	private final EnumSet<Material> mIgnoredMats = EnumSet.of(
 	            Material.AIR,
@@ -44,6 +33,13 @@ public class SpellKaulBlockBreak extends Spell {
 				Material.BARRIER,
 	            Material.SPAWNER
 	        );
+
+	public SpellKaulBlockBreak(LivingEntity launcher, Material... noBreak) {
+		mBoss = launcher;
+		for (Material mat : noBreak) {
+			mIgnoredMats.add(mat);
+		}
+	}
 
 	@Override
 	public void run() {
@@ -57,52 +53,69 @@ public class SpellKaulBlockBreak extends Spell {
 			this.cancel();
 			return;
 		}
-		Vector dir = LocationUtils.getDirectionTo(target.getLocation().add(0, 1, 0), mBoss.getLocation());
+		Vector dir = LocationUtils.getDirectionTo(target.getLocation().add(0, 0, 0), mBoss.getLocation());
 		Location tloc = mBoss.getLocation().setDirection(dir);
 
-		Vector vec;
-		float yaw = tloc.getYaw();
-		float yaw1 = yaw + 90;
-		vec = new Vector(Math.cos(0) * 1, 0, Math.sin(0) * 1);
-		vec = VectorUtils.rotateYAxis(vec, yaw1);
-		vec = VectorUtils.rotateXAxis(vec, -tloc.getPitch());
+		for (int i = 1; i <= 2; i++) {
+			Vector vec;
+			float yaw = tloc.getYaw();
+			float yaw1 = yaw + 90;
+			vec = new Vector(Math.cos(0) * i, 0, Math.sin(0) * i);
+			vec = VectorUtils.rotateYAxis(vec, yaw1);
+			vec = VectorUtils.rotateXAxis(vec, -tloc.getPitch());
 
-		Location l = tloc.clone().add(vec);
+			Location l = tloc.clone().add(vec);
 
-		/* Get a list of all blocks that impede the boss's movement */
-		List<Block> badBlockList = new ArrayList<Block>();
-		Location testloc = new Location(l.getWorld(), 0, 0, 0);
-		for (int x = -1; x <= 1; x++) {
-			testloc.setX(l.getX() + x);
-			for (int y = 1; y <= 3; y++) {
-				testloc.setY(l.getY() + y);
-				for (int z = -1; z <= 1; z++) {
-					testloc.setZ(l.getZ() + z);
-					Block block = testloc.getBlock();
-					Material material = block.getType();
-					if ((!mIgnoredMats.contains(material)) && !mNoBreak.contains(material) &&
-						(material.isSolid() || material.equals(Material.COBWEB)) &&
-						(!(block.getState() instanceof Lootable) || !((Lootable)block.getState()).hasLootTable())) {
-						badBlockList.add(testloc.getBlock());
+			/* Get a list of all blocks that impede the boss's movement */
+			List<Block> badBlockList = new ArrayList<Block>();
+			Location testloc = new Location(l.getWorld(), 0, 0, 0);
+
+			if (target.getLocation().getY() > 8.7) {
+				mINC++;
+				if (target.getLocation().getY() > 8.7 && mBoss.getLocation().distance(target.getLocation()) < 5 &&
+					mINC > 4) {
+					l = target.getLocation();
+					l.setY(target.getLocation().getY() - 3);
+					mINC = 0;
+				}
+			} else {
+				mINC = 0;
+			}
+
+			for (int x = -1; x <= 1; x++) {
+				testloc.setX(l.getX() + x);
+				for (int y = 1; y <= 4; y++) {
+					testloc.setY(l.getY() + y);
+					if (l.getY() < 8) {
+						testloc.setY(l.getY() + y + 1);
+					}
+					for (int z = -1; z <= 1; z++) {
+						testloc.setZ(l.getZ());
+						Block block = testloc.getBlock();
+						if (!mIgnoredMats.contains(block.getType())) {
+							badBlockList.add(block);
+						}
 					}
 				}
 			}
-		}
 
-		/* If more than one block, destroy all blocking blocks */
-		if (badBlockList.size() > 1) {
-			/* Call an event with these exploding blocks to give plugins a chance to modify it */
-			EntityExplodeEvent event = new EntityExplodeEvent(mBoss, l, badBlockList, 0f);
-			Bukkit.getServer().getPluginManager().callEvent(event);
-			if (event.isCancelled()) {
-				return;
-			}
-
-			/* Remove any remaining blocks, which might have been modified by the event */
-			for (Block block : badBlockList) {
-				block.setType(Material.AIR);
-			}
+			/* If there are any blocks, destroy all blocking blocks */
 			if (badBlockList.size() > 0) {
+				/* Call an event with these exploding blocks to give plugins a chance to modify it */
+				EntityExplodeEvent event = new EntityExplodeEvent(mBoss, l, badBlockList, 0f);
+				Bukkit.getServer().getPluginManager().callEvent(event);
+				if (event.isCancelled()) {
+					return;
+				}
+
+				/* Remove any remaining blocks, which might have been modified by the event */
+				for (Block block : badBlockList) {
+					if (block.getState() instanceof Container) {
+						block.breakNaturally();
+					} else {
+						block.setType(Material.AIR);
+					}
+				}
 				loc.getWorld().playSound(loc, Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, 0.3f, 0.9f);
 				loc.getWorld().spawnParticle(Particle.EXPLOSION_NORMAL, loc, 6, 1, 1, 1, 0.03);
 			}
