c35147e7fa59c8a101474b75a8632cd16da1b63a
Byron Marohn <combustible@live.com>
2020-07-16 22:33:10 +0000

    Allow /teleportbyscore to teleport entities
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugins/paper/src/main/java/com/playmonumenta/plugins/commands/TeleportByScore.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/commands/TeleportByScore.java
@@ -9,6 +9,8 @@ import com.playmonumenta.plugins.utils.ScoreboardUtils;
 
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
@@ -26,7 +28,7 @@ public class TeleportByScore extends GenericCommand {
 		CommandPermission perms = CommandPermission.fromString("monumenta.command.teleportbyscore");
 
 		LinkedHashMap<String, Argument> arguments = new LinkedHashMap<>();
-		arguments.put("player", new EntitySelectorArgument(EntitySelector.ONE_PLAYER));
+		arguments.put("entity", new EntitySelectorArgument(EntitySelector.ONE_ENTITY));
 		arguments.put("x objective", new StringArgument());
 		arguments.put("y objective", new StringArgument());
 		arguments.put("z objective", new StringArgument());
@@ -34,7 +36,7 @@ public class TeleportByScore extends GenericCommand {
 		                                  perms,
 		                                  arguments,
 		                                  (sender, args) -> {
-											  teleport(sender, (Player)args[0], (String)args[1], (String)args[2], (String)args[3], null, null, 1.0f);
+											  teleport(sender, (Entity)args[0], (String)args[1], (String)args[2], (String)args[3], null, null, 1.0f);
 		                                  }
 		);
 		arguments.put("yaw objective", new StringArgument());
@@ -43,7 +45,7 @@ public class TeleportByScore extends GenericCommand {
 		                                  perms,
 		                                  arguments,
 		                                  (sender, args) -> {
-											  teleport(sender, (Player)args[0], (String)args[1], (String)args[2], (String)args[3], (String)args[4], (String)args[5], 1.0f);
+											  teleport(sender, (Entity)args[0], (String)args[1], (String)args[2], (String)args[3], (String)args[4], (String)args[5], 1.0f);
 		                                  }
 		);
 		arguments.put("scale", new FloatArgument(1));
@@ -51,26 +53,26 @@ public class TeleportByScore extends GenericCommand {
 		                                  perms,
 		                                  arguments,
 		                                  (sender, args) -> {
-											  teleport(sender, (Player)args[0], (String)args[1], (String)args[2], (String)args[3], (String)args[4], (String)args[5], (Float)args[6]);
+											  teleport(sender, (Entity)args[0], (String)args[1], (String)args[2], (String)args[3], (String)args[4], (String)args[5], (Float)args[6]);
 		                                  }
 		);
 	}
 
 	@Nullable
-	private static Integer getValue(@Nonnull Player player, @Nullable String obj) {
+	private static Integer getValue(@Nonnull Entity entity, @Nullable String obj) {
 		if (obj == null || obj.equals("~")) {
 			return null;
 		}
 
-		return ScoreboardUtils.getScoreboardValue(player.getName(), obj).orElse(null);
+		return ScoreboardUtils.getScoreboardValue(entity.getName(), obj).orElse(null);
 	}
 
-	private static void teleport(@Nonnull CommandSender sender, @Nonnull Player player, @Nonnull String objX, @Nonnull String objY, @Nonnull String objZ, @Nullable String objYaw, @Nullable String objPitch, float scale) {
-		Integer x = getValue(player, objX);
-		Integer y = getValue(player, objY);
-		Integer z = getValue(player, objZ);
-		Integer yawNullable = getValue(player, objYaw);
-		Integer pitchNullable = getValue(player, objPitch);
+	private static void teleport(@Nonnull CommandSender sender, @Nonnull Entity entity, @Nonnull String objX, @Nonnull String objY, @Nonnull String objZ, @Nullable String objYaw, @Nullable String objPitch, float scale) {
+		Integer x = getValue(entity, objX);
+		Integer y = getValue(entity, objY);
+		Integer z = getValue(entity, objZ);
+		Integer yawNullable = getValue(entity, objYaw);
+		Integer pitchNullable = getValue(entity, objPitch);
 
 		if (x == null) {
 			error(sender, "Could not get value " + objX);
@@ -86,26 +88,30 @@ public class TeleportByScore extends GenericCommand {
 		float yaw;
 		float pitch;
 		if (yawNullable == null) {
-			yaw = player.getLocation().getYaw();
+			yaw = entity.getLocation().getYaw();
 		} else {
 			yaw = (float)yawNullable / scale;
 		}
 		if (pitchNullable == null) {
-			pitch = player.getLocation().getPitch();
+			pitch = entity.getLocation().getPitch();
 		} else {
 			pitch = (float)pitchNullable / scale;
 		}
 
-		Location loc = player.getLocation();
+		Location loc = entity.getLocation();
 		float offset = (scale == 1 ? 0.5f : 0.0f);
 		loc.setX((float)x / scale + offset);
 		loc.setY((float)y / scale + 0.1);
 		loc.setZ((float)z / scale + offset);
 		loc.setPitch((float)pitch);
 		loc.setYaw((float)yaw);
-		player.setSwimming(false);
-		player.setVelocity(new Vector(0, 0.1, 0));
-		player.teleport(loc);
+		if (entity instanceof Player) {
+			((Player)entity).setSwimming(false);
+		}
+		if (entity instanceof Mob) {
+			((Mob)entity).setVelocity(new Vector(0, 0.1, 0));
+		}
+		entity.teleport(loc);
 	}
 }
 
