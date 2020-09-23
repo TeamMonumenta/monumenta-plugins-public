8df3a5dfbe44ba63985f0e0feb2bfbe55dbab8bd
Byron Marohn <combustible@live.com>
2020-09-23 23:39:12 +0000

    Integrate with LibraryOfSouls plugin
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/integrations/LibraryOfSoulsIntegration.java
@@ -0,0 +1,52 @@
+package com.playmonumenta.plugins.integrations;
+
+import java.util.Set;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.plugin.Plugin;
+
+public class LibraryOfSoulsIntegration {
+	private static LibraryOfSoulsIntegrationConnector CONNECTOR = null;
+	private static boolean LOS_CHECKED = false;
+
+	private static void ensureInitialized() {
+		if (LOS_CHECKED)
+			return;
+
+		LOS_CHECKED = true;
+
+		Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("LibraryOfSouls");
+		if (plugin != null) {
+			CONNECTOR = new LibraryOfSoulsIntegrationConnector();
+		}
+	}
+
+	public static Entity summon(Location loc, String soulName) {
+		ensureInitialized();
+
+		if (CONNECTOR == null) {
+			return null;
+		}
+		return CONNECTOR.summon(loc, soulName);
+	}
+
+	public static Set<String> getSoulNames() {
+		ensureInitialized();
+
+		if (CONNECTOR == null) {
+			return null;
+		}
+		return CONNECTOR.getSoulNames();
+	}
+
+	public static Set<String> getSoulLocations() {
+		ensureInitialized();
+
+		if (CONNECTOR == null) {
+			return null;
+		}
+		return CONNECTOR.getSoulNames();
+	}
+}
