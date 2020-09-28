927b83c0645411094f47f1f2a036825d9e3925ac
Byron Marohn <combustible@live.com>
2020-09-28 20:48:36 +0000

    Stop teal keys from transferring
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugins/paper/src/main/java/com/playmonumenta/plugins/utils/InventoryUtils.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/utils/InventoryUtils.java
@@ -377,6 +377,7 @@ public class InventoryUtils {
 		        testForItemWithLore(item, "D5 Key") ||
 		        testForItemWithLore(item, "D6 Key") ||
 		        testForItemWithLore(item, "D11 Key") ||
+		        testForItemWithLore(item, "DT Key") ||
 		        testForItemWithLore(item, "DN Key");
 	}
 
