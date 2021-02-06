585d9c1e0004ab631cf93e329b3ebee178ecf8f5
Byron Marohn <combustible@live.com>
2021-02-06 15:44:51 -0800

    Several enhancements for shop manager (#490)
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- plugins/paper/src/main/java/com/playmonumenta/plugins/plots/ShopManager.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/plots/ShopManager.java
@@ -5,13 +5,17 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.function.Consumer;
 
+import javax.annotation.Nullable;
+
 import com.playmonumenta.plugins.Plugin;
 import com.playmonumenta.plugins.integrations.luckperms.LuckPermsIntegration;
 import com.playmonumenta.plugins.utils.ZoneUtils;
 import com.playmonumenta.plugins.utils.ZoneUtils.ZoneProperty;
 import com.playmonumenta.scriptedquests.quests.QuestNpc;
+import com.playmonumenta.scriptedquests.utils.ScoreboardUtils;
 
 import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -22,8 +26,11 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Container;
 import org.bukkit.block.Lockable;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Painting;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Shulker;
 import org.bukkit.event.Cancellable;
@@ -40,7 +47,10 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityPotionEffectEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.loot.LootTables;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.BoundingBox;
 
 import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPICommand;
@@ -55,8 +65,8 @@ public class ShopManager implements Listener {
 	private static final Material SHOP_EMPTY_MAT = Material.BRICKS;
 	private static final Material SHOP_PURCHASED_MAT = Material.STONE_BRICKS;
 	private static final int MAX_SHOP_WIDTH = 20;
-	private static final int SHOP_HEIGHT = 20;
-	private static final int SHOP_DEPTH = 10;
+	private static final int SHOP_HEIGHT = 13;
+	private static final int SHOP_DEPTH = 15;
 	private static final int GUILD_SHOP_WIDTH = 10;
 	private static final String NPC_NAME = "SHOP NPC";
 	private static final String GUILD_NPC_NAME = "GUILD SHOP NPC";
@@ -138,6 +148,15 @@ public class ShopManager implements Listener {
 
 	@EventHandler(priority = EventPriority.LOW)
 	public void blockPlaceEvent(BlockPlaceEvent event) {
+		/* Prevent players from placing bricks */
+		if (!event.isCancelled() && event.getBlockPlaced() != null
+			&& event.getBlockPlaced().getType().equals(Material.BRICKS)
+			&& !event.getPlayer().getGameMode().equals(GameMode.CREATIVE)
+			&& ZoneUtils.hasZoneProperty(event.getBlockPlaced().getLocation(), ZoneProperty.SHOPS_POSSIBLE)) {
+			event.setCancelled(true);
+			return;
+		}
+
 		if (!event.isCancelled() && !isAllowed(event.getPlayer(), event.getBlockPlaced())) {
 			event.setCancelled(true);
 		}
@@ -185,14 +204,16 @@ public class ShopManager implements Listener {
 		private final Location mMax;
 		private final Shulker mEntity;
 		private final String mOwnerName;
+		private final @Nullable String mOwnerGuildName;
 		private final UUID mOwnerUUID;
 		private final Material mOriginalEntityMat;
 
-		private Shop(Location min, Location max, Shulker entity, String ownerName, UUID ownerUUID, Material originalEntityMat) {
+		private Shop(Location min, Location max, Shulker entity, String ownerName, @Nullable String ownerGuildName, UUID ownerUUID, Material originalEntityMat) {
 			mMin = min;
 			mMax = max;
 			mEntity = entity;
 			mOwnerName = ownerName;
+			mOwnerGuildName = ownerGuildName;
 			mOwnerUUID = ownerUUID;
 			mOriginalEntityMat = originalEntityMat;
 		}
@@ -213,6 +234,7 @@ public class ShopManager implements Listener {
 			Integer y2 = null;
 			Integer z2 = null;
 			String ownerName = null;
+			String ownerGuildName = null;
 			UUID ownerUUID = null;
 			Material originalEntityMat = null;
 			try {
@@ -231,6 +253,8 @@ public class ShopManager implements Listener {
 						z2 = Integer.parseInt(tag.substring("shop_z2=".length()));
 					} else if (tag.startsWith("shop_ownerName=")) {
 						ownerName = tag.substring("shop_ownerName=".length());
+					} else if (tag.startsWith("shop_ownerGuildName=")) {
+						ownerGuildName = tag.substring("shop_ownerGuildName=".length());
 					} else if (tag.startsWith("shop_ownerUUID=")) {
 						ownerUUID = UUID.fromString(tag.substring("shop_ownerUUID=".length()));
 					} else if (tag.startsWith("shop_origMat=")) {
@@ -245,7 +269,7 @@ public class ShopManager implements Listener {
 				CommandAPI.fail("Shop entity is missing a required tag");
 			}
 
-			return new Shop(new Location(shopEntity.getWorld(), x1, y1, z1), new Location(shopEntity.getWorld(), x2, y2, z2), (Shulker) shopEntity, ownerName, ownerUUID, originalEntityMat);
+			return new Shop(new Location(shopEntity.getWorld(), x1, y1, z1), new Location(shopEntity.getWorld(), x2, y2, z2), (Shulker) shopEntity, ownerName, ownerGuildName, ownerUUID, originalEntityMat);
 		}
 
 		private void iterArea(Consumer<Location> func) {
@@ -288,7 +312,15 @@ public class ShopManager implements Listener {
 			.withPermission(CommandPermission.fromString("monumenta.shop"))
 			.withArguments(arguments)
 			.executes((sender, args) -> {
-				shopLock((Entity)args[0]);
+				shopLock((Entity)args[0], null);
+			})
+			.register();
+		arguments.put("player", new EntitySelectorArgument(EntitySelector.ONE_PLAYER));
+		new CommandAPICommand("monumentashop")
+			.withPermission(CommandPermission.fromString("monumenta.shop"))
+			.withArguments(arguments)
+			.executes((sender, args) -> {
+				shopLock((Entity)args[0], (Player)args[1]);
 			})
 			.register();
 
@@ -300,7 +332,15 @@ public class ShopManager implements Listener {
 			.withPermission(CommandPermission.fromString("monumenta.shop"))
 			.withArguments(arguments)
 			.executes((sender, args) -> {
-				shopUnlock((Entity)args[0]);
+				shopUnlock((Entity)args[0], null);
+			})
+			.register();
+		arguments.put("player", new EntitySelectorArgument(EntitySelector.ONE_PLAYER));
+		new CommandAPICommand("monumentashop")
+			.withPermission(CommandPermission.fromString("monumenta.shop"))
+			.withArguments(arguments)
+			.executes((sender, args) -> {
+				shopUnlock((Entity)args[0], (Player)args[1]);
 			})
 			.register();
 
@@ -312,7 +352,15 @@ public class ShopManager implements Listener {
 			.withPermission(CommandPermission.fromString("monumenta.shop"))
 			.withArguments(arguments)
 			.executes((sender, args) -> {
-				shopReset((Entity)args[0]);
+				shopReset((Entity)args[0], null);
+			})
+			.register();
+		arguments.put("player", new EntitySelectorArgument(EntitySelector.ONE_PLAYER));
+		new CommandAPICommand("monumentashop")
+			.withPermission(CommandPermission.fromString("monumenta.shop"))
+			.withArguments(arguments)
+			.executes((sender, args) -> {
+				shopReset((Entity)args[0], (Player)args[1]);
 			})
 			.register();
 	}
@@ -337,14 +385,14 @@ public class ShopManager implements Listener {
 		}
 
 		/* Find the dimensions of the platform */
-		int minX = -1;
-		int minZ = -1;
-		int maxX = -1;
-		int maxZ = -1;
+		int tmpminX = -1;
+		int tmpminZ = -1;
+		int tmpmaxX = -1;
+		int tmpmaxZ = -1;
 		Location testLoc = startLoc.clone();
 		for (int i = 0; i < MAX_SHOP_WIDTH; i++) {
 			if (testLoc.getBlock().getType().equals(SHOP_EMPTY_MAT)) {
-				minX = testLoc.getBlockX();
+				tmpminX = testLoc.getBlockX();
 			} else {
 				break;
 			}
@@ -353,7 +401,7 @@ public class ShopManager implements Listener {
 		testLoc = startLoc.clone();
 		for (int i = 0; i < MAX_SHOP_WIDTH; i++) {
 			if (testLoc.getBlock().getType().equals(SHOP_EMPTY_MAT)) {
-				maxX = testLoc.getBlockX();
+				tmpmaxX = testLoc.getBlockX();
 			} else {
 				break;
 			}
@@ -362,7 +410,7 @@ public class ShopManager implements Listener {
 		testLoc = startLoc.clone();
 		for (int i = 0; i < MAX_SHOP_WIDTH; i++) {
 			if (testLoc.getBlock().getType().equals(SHOP_EMPTY_MAT)) {
-				minZ = testLoc.getBlockZ();
+				tmpminZ = testLoc.getBlockZ();
 			} else {
 				break;
 			}
@@ -371,20 +419,27 @@ public class ShopManager implements Listener {
 		testLoc = startLoc.clone();
 		for (int i = 0; i < MAX_SHOP_WIDTH; i++) {
 			if (testLoc.getBlock().getType().equals(SHOP_EMPTY_MAT)) {
-				maxZ = testLoc.getBlockZ();
+				tmpmaxZ = testLoc.getBlockZ();
 			} else {
 				break;
 			}
 			testLoc.add(0, 0, 1);
 		}
+		final int minX = tmpminX;
+		final int minZ = tmpminZ;
+		final int maxX = tmpmaxX;
+		final int maxZ = tmpmaxZ;
+		final int platY = startLoc.getBlockY();
 
 		boolean isGuildShop = Math.abs(maxX - minX) >= GUILD_SHOP_WIDTH;
-		String guildName = null;
+		final String guildName;
 		if (isGuildShop) {
 			guildName = LuckPermsIntegration.getGuildName(LuckPermsIntegration.getGuild(player));
 			if (guildName == null) {
 				CommandAPI.fail("You must be in a guild to purchase a guild shop");
 			}
+		} else {
+			guildName = null;
 		}
 
 		/* Figure out which side the entity should go on */
@@ -410,24 +465,37 @@ public class ShopManager implements Listener {
 
 		Material originalEntityMat = entityLoc.getBlock().getType();
 		entityLoc.getBlock().setType(Material.AIR);
-		Shulker shopEntity = entityLoc.getWorld().spawn(entityLoc, Shulker.class, SpawnReason.CUSTOM);
-		shopEntity.setAI(false);
-		shopEntity.setPersistent(true);
-		if (isGuildShop) {
-			shopEntity.setCustomName(ChatColor.GREEN + guildName + "'s Guild Shop");
-		} else {
-			shopEntity.setCustomName(ChatColor.AQUA + player.getName() + "'s Shop");
-		}
-		Set<String> tags = shopEntity.getScoreboardTags();
-		tags.add("shop_x1=" + Integer.toString(minX));
-		tags.add("shop_y1=" + Integer.toString(startLoc.getBlockY()));
-		tags.add("shop_z1=" + Integer.toString(minZ));
-		tags.add("shop_x2=" + Integer.toString(maxX));
-		tags.add("shop_y2=" + Integer.toString(startLoc.getBlockY()));
-		tags.add("shop_z2=" + Integer.toString(maxZ));
-		tags.add("shop_ownerName=" + player.getName());
-		tags.add("shop_ownerUUID=" + player.getUniqueId());
-		tags.add("shop_origMat=" + originalEntityMat.toString());
+		Shulker shopEntity = entityLoc.getWorld().spawn(entityLoc, Shulker.class, SpawnReason.CUSTOM, (preloadEntity) -> {
+			preloadEntity.setAI(false);
+			preloadEntity.setGravity(false);
+			preloadEntity.setSilent(true);
+			preloadEntity.setColor(DyeColor.MAGENTA);
+			preloadEntity.setLootTable(LootTables.EMPTY.getLootTable());
+			preloadEntity.setPersistent(true);
+
+			if (isGuildShop) {
+				preloadEntity.setCustomName(ChatColor.GREEN + guildName + "'s Guild Shop");
+			} else {
+				preloadEntity.setCustomName(ChatColor.AQUA + player.getName() + "'s Shop");
+			}
+			Set<String> tags = preloadEntity.getScoreboardTags();
+			tags.add("shop_x1=" + Integer.toString(minX));
+			tags.add("shop_y1=" + Integer.toString(platY));
+			tags.add("shop_z1=" + Integer.toString(minZ));
+			tags.add("shop_x2=" + Integer.toString(maxX));
+			tags.add("shop_y2=" + Integer.toString(platY));
+			tags.add("shop_z2=" + Integer.toString(maxZ));
+			tags.add("shop_ownerName=" + player.getName());
+			tags.add("shop_ownerUUID=" + player.getUniqueId());
+			tags.add("shop_origMat=" + originalEntityMat.toString());
+			tags.add("shop_shulker");
+			if (isGuildShop) {
+				tags.add("guild_shop");
+				tags.add("shop_ownerGuildName=" + guildName);
+			} else {
+				tags.add("player_shop");
+			}
+		});
 
 		Shop shop = Shop.fromShopEntity(shopEntity);
 
@@ -441,7 +509,7 @@ public class ShopManager implements Listener {
 
 			/* Add a ceiling */
 			plat.add(0, SHOP_HEIGHT, 0);
-			plat.getBlock().setType(Material.BEDROCK);
+			plat.getBlock().setType(Material.BARRIER);
 
 			/* Add a celler floor */
 			plat.subtract(0, SHOP_HEIGHT + SHOP_DEPTH + 2, 0);
@@ -456,18 +524,19 @@ public class ShopManager implements Listener {
 		});
 	}
 
-	private static void shopLock(Entity shopEntity) throws WrapperCommandSyntaxException {
+	private static void shopLock(Entity shopEntity, Player player) throws WrapperCommandSyntaxException {
 		if (!ZoneUtils.hasZoneProperty(shopEntity, ZoneProperty.SHOPS_POSSIBLE)) {
 			CommandAPI.fail("This command can only be used within a Shops Possible zone");
 		}
 
 		Shop shop = Shop.fromShopEntity(shopEntity);
 
+		/* Make sure the player (if any) is allowed to change the lock */
+		checkAllowedToChangeLock(shop, player);
+
 		shop.iterArea((Location plat) -> {
 			plat.add(0, 1, 0);
 			plat.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, plat, 1, 0.1, 0.1, 0.1);
-
-			/* TODO ENTITIES */
 
 			/* Lock tile entities */
 			plat.subtract(0, 1 + SHOP_DEPTH, 0);
@@ -484,22 +553,34 @@ public class ShopManager implements Listener {
 			plat.setY(10);
 			plat.getBlock().setType(Material.RED_CONCRETE);
 		});
+
+		/* Lock regular entities */
+		for (Entity entity : shop.mMin.getWorld().getNearbyEntities(new BoundingBox(shop.mMin.getX(), shop.mMin.getY(), shop.mMin.getZ(),
+		                                                                            shop.mMax.getX(), shop.mMax.getY(), shop.mMax.getZ()))) {
+			if (entity instanceof ItemFrame) {
+				entity.setInvulnerable(true);
+			} else if (entity instanceof ArmorStand) {
+				entity.setInvulnerable(true);
+				((ArmorStand)entity).setDisabledSlots(EquipmentSlot.values());
+			}
+		}
 	}
 
-	private static void shopUnlock(Entity shopEntity) throws WrapperCommandSyntaxException {
+	private static void shopUnlock(Entity shopEntity, Player player) throws WrapperCommandSyntaxException {
 		if (!ZoneUtils.hasZoneProperty(shopEntity, ZoneProperty.SHOPS_POSSIBLE)) {
 			CommandAPI.fail("This command can only be used within a Shops Possible zone");
 		}
 
 		Shop shop = Shop.fromShopEntity(shopEntity);
 
+		/* Make sure the player (if any) is allowed to change the lock */
+		checkAllowedToChangeLock(shop, player);
+
 		shopEntity.getWorld().playSound(shopEntity.getLocation(), Sound.BLOCK_CHEST_LOCKED, SoundCategory.PLAYERS, 1.0f, 0.9f);
 
 		shop.iterArea((Location plat) -> {
 			plat.add(0, 1, 0);
 			plat.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, plat, 1, 0.1, 0.1, 0.1);
-
-			/* TODO ENTITIES */
 
 			/* Unlock tile entities */
 			plat.subtract(0, 1 + SHOP_DEPTH, 0);
@@ -516,22 +597,34 @@ public class ShopManager implements Listener {
 			plat.setY(10);
 			plat.getBlock().setType(Material.SPONGE);
 		});
+
+		/* Lock regular entities */
+		for (Entity entity : shop.mMin.getWorld().getNearbyEntities(new BoundingBox(shop.mMin.getX(), shop.mMin.getY(), shop.mMin.getZ(),
+		                                                                            shop.mMax.getX(), shop.mMax.getY(), shop.mMax.getZ()))) {
+			if (entity instanceof ItemFrame) {
+				entity.setInvulnerable(false);
+			} else if (entity instanceof ArmorStand) {
+				entity.setInvulnerable(false);
+				((ArmorStand)entity).removeDisabledSlots(EquipmentSlot.values());
+			}
+		}
 	}
 
-	private static void shopReset(Entity shopEntity) throws WrapperCommandSyntaxException {
+	private static void shopReset(Entity shopEntity, Player player) throws WrapperCommandSyntaxException {
 		if (!ZoneUtils.hasZoneProperty(shopEntity, ZoneProperty.SHOPS_POSSIBLE)) {
 			CommandAPI.fail("This command can only be used within a Shops Possible zone");
 		}
 
 		Shop shop = Shop.fromShopEntity(shopEntity);
 
+		/* Make sure the player (if any) is allowed to change the lock */
+		checkAllowedToChangeLock(shop, player);
+
 		shop.iterArea((Location plat) -> {
 			/* Replace platform area */
 			plat.getBlock().setType(SHOP_EMPTY_MAT);
 			plat.add(0, 1, 0);
 			plat.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, plat, 1, 0.1, 0.1, 0.1);
-
-			/* TODO ENTITIES */
 
 			/* Delete above platform */
 			for (int y = 0; y <= SHOP_HEIGHT; y++) {
@@ -553,10 +646,41 @@ public class ShopManager implements Listener {
 			plat.getBlock().setType(Material.RED_CONCRETE);
 		});
 
+		/* Remove entities */
+		for (Entity entity : shop.mMin.getWorld().getNearbyEntities(new BoundingBox(shop.mMin.getX(), shop.mMin.getY(), shop.mMin.getZ(),
+		                                                                            shop.mMax.getX(), shop.mMax.getY(), shop.mMax.getZ()))) {
+			if (entity instanceof ItemFrame || entity instanceof Painting || entity instanceof ArmorStand) {
+				entity.remove();
+			}
+		}
+
 		/* Remove the entity */
 		Location entityLoc = shop.mEntity.getLocation();
 		shop.mEntity.remove();
 		entityLoc.getBlock().setType(shop.mOriginalEntityMat);
+	}
+
+	/* Throws an exception if not allowed and sends a message to the player, otherwise returns */
+	private static void checkAllowedToChangeLock(Shop shop, Player player) throws WrapperCommandSyntaxException {
+		/* This command was run without a player (i.e. via a command block) - always allow it */
+		if (player == null || player.isOp() || player.getGameMode().equals(GameMode.CREATIVE)) {
+			return;
+		}
+
+		/* This command was initiated by a player interacting with the NPC - check permissions */
+		if (shop.isGuildShop()) {
+			String guildName = LuckPermsIntegration.getGuildName(LuckPermsIntegration.getGuild(player));
+			if (guildName == null || !guildName.equalsIgnoreCase(shop.mOwnerGuildName) || ScoreboardUtils.getScoreboardValue(player, "Founder") != 1) {
+				String msg = "You must be a *founder* of the guild '" + shop.mOwnerGuildName + "' to change settings for this shop";
+				player.sendMessage(ChatColor.RED + msg);
+				CommandAPI.fail(msg);
+			}
+		} else if (!player.getUniqueId().equals(shop.mOwnerUUID)) {
+			/* Not a guild shop, and also not the owning player */
+			String msg = "You must be the owner of this shop to change its settings";
+			player.sendMessage(ChatColor.RED + msg);
+			CommandAPI.fail(msg);
+		}
 	}
 
 	private static void clearContainer(Block block) {
