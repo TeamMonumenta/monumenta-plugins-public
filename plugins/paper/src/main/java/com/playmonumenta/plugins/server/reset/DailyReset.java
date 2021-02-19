0a569350cc657cd0fa7f2059f24499bea56c0eee
Cloud <44526554+Cloud7050@users.noreply.github.com>
2021-02-19 10:17:56 +0800

    Sync up daily resets with play's daily reboots (UTC-9) (#506)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/server/reset/DailyReset.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/server/reset/DailyReset.java
@@ -1,20 +1,21 @@
 package com.playmonumenta.plugins.server.reset;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
-
-import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
+import java.time.ZoneOffset;
 
 import com.playmonumenta.plugins.Plugin;
 import com.playmonumenta.plugins.server.properties.ServerProperties;
 import com.playmonumenta.plugins.utils.ScoreboardUtils;
 
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
 public class DailyReset {
 	private static final String DAILY_PLAYER_CHANGES_COMMAND = "execute as @S at @s run function monumenta:mechanisms/daily_player_changes";
-	private static final ZoneId TZ = ZoneId.of("America/New_York");
+	// Offset server time to UTC-9 to change when the new day arrives.
+	// Daily resets should match quest stuff & play's daily daily reboots at 9am UTC
+	private static final ZoneId TIMEZONE = ZoneOffset.of("-9");
 	private static BukkitRunnable mRunnable = null;
 
 	public static void startTimer(Plugin plugin) {
@@ -38,7 +39,8 @@ public class DailyReset {
 	}
 
 	private static int getDailyVersion() {
-		return (int) (ChronoUnit.DAYS.between(LocalDate.ofEpochDay(0), LocalDateTime.now(TZ)));
+		// In our specified timezone, how many days we perceive it is since our 1 Jan 1970.
+		return (int) LocalDate.now(TIMEZONE).toEpochDay();
 	}
 
 	public static void handle(Plugin plugin, Player player) {
