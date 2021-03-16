f618c34b4f7367ac0936cd7965e4949ff8e3666c
rayman520 <cpierre@student.42.fr>
2021-03-16 20:25:34 +0100

    Boat/Barrier Fix (#554)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/listeners/VehicleListener.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/listeners/VehicleListener.java
@@ -1,7 +1,10 @@
 package com.playmonumenta.plugins.listeners;
 
+import com.playmonumenta.plugins.utils.ItemUtils;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.AbstractHorse;
+import org.bukkit.entity.Boat;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Vehicle;
@@ -26,6 +29,14 @@ public class VehicleListener implements Listener {
 
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void vehicleCreateEvent(VehicleCreateEvent event) {
+
+		// prevent boats from being placed near barrier blocks
+		// anti barrier-passthrough glitch
+		if (antiBoatBarrierPasstrough(event)) {
+			event.setCancelled(true);
+			return;
+		}
+
 		mPlugin.mTrackingManager.addEntity(event.getVehicle());
 	}
 
@@ -71,5 +82,32 @@ public class VehicleListener implements Listener {
 			entity.teleport(loc);
 			event.setCancelled(true);
 		}
+	}
+
+	// prevent boats from being placed near barrier blocks
+	// anti barrier-passthrough glitch
+	private boolean antiBoatBarrierPasstrough(VehicleCreateEvent event) {
+
+		Vehicle vehicle = event.getVehicle();
+		if (vehicle instanceof Boat) {
+			// check a 5x5 box around the player.
+			// if one of those block is a no-passthrough block, cancel the spawn.
+			World world = vehicle.getLocation().getWorld();
+			int x = vehicle.getLocation().getBlockX() - 2;
+			int y = vehicle.getLocation().getBlockY() - 2;
+			int z = vehicle.getLocation().getBlockZ() - 2;
+
+			for (int xi = 0; xi < 5; xi++) {
+				for (int yi = 0; yi < 5; yi++) {
+					for (int zi = 0; zi < 5; zi++) {
+						if (ItemUtils.noPassthrough.contains(world.getBlockAt(x + xi, y + yi, z + zi).getType())) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+
+		return false;
 	}
 }
