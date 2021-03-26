242ae8796effd5ef031041eacf50da21b35a284d
dmdinnc <38630939+dmdinnc@users.noreply.github.com>
2021-03-26 16:33:58 -0400

    pebgui: fix summon style to direct (#581)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/overrides/WrittenBookOverride.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/overrides/WrittenBookOverride.java
@@ -6,13 +6,15 @@ import org.bukkit.event.block.Action;
 import org.bukkit.inventory.ItemStack;
 
 import com.playmonumenta.plugins.Plugin;
+import com.playmonumenta.plugins.guis.SinglePageGUIManager;
+import com.playmonumenta.plugins.guis.singlepageguis.PebGui;
 
 public class WrittenBookOverride extends BaseOverride {
 	@Override
 	public boolean rightClickItemInteraction(Plugin plugin, Player player, Action action, ItemStack item, Block block) {
 		if (item.getItemMeta().getDisplayName().contains("Personal Enchanted Book")) {
 			if (player.hasPermission("monumenta.peb")) {
-				player.performCommand("openpeb " + player.getDisplayName());
+				SinglePageGUIManager.openGUI(player, new PebGui(player, null));
 				return false;
 			}
 		}
