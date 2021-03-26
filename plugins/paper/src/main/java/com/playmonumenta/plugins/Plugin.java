1ae1b4657645b356efb4ac98713e8823fe882279
Byron Marohn <Combustible@live.com>
2021-03-26 04:34:42 +0000

    Revert "Complete Graves Rework (#569)"
    This reverts commit 888b945fd231d202f4f782f275ba584558097898.

--- plugins/paper/src/main/java/com/playmonumenta/plugins/Plugin.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/Plugin.java
@@ -30,7 +30,6 @@ import com.playmonumenta.plugins.commands.GetDepthPoints;
 import com.playmonumenta.plugins.commands.GetScoreCommand;
 import com.playmonumenta.plugins.commands.GildifyHeldItem;
 import com.playmonumenta.plugins.commands.GiveSoulbound;
-import com.playmonumenta.plugins.commands.Grave;
 import com.playmonumenta.plugins.commands.HopeifyHeldItem;
 import com.playmonumenta.plugins.commands.InfuseHeldItem;
 import com.playmonumenta.plugins.commands.Launch;
@@ -83,7 +82,6 @@ import com.playmonumenta.plugins.listeners.DeathItemListener;
 import com.playmonumenta.plugins.listeners.DelvesListener;
 import com.playmonumenta.plugins.listeners.EntityListener;
 import com.playmonumenta.plugins.listeners.ExceptionListener;
-import com.playmonumenta.plugins.listeners.GraveListener;
 import com.playmonumenta.plugins.listeners.JunkItemListener;
 import com.playmonumenta.plugins.listeners.MobListener;
 import com.playmonumenta.plugins.listeners.PlayerListener;
@@ -188,7 +186,6 @@ public class Plugin extends JavaPlugin {
 		SetViewDistance.register();
 		UnsignBook.register();
 		GetScoreCommand.register(this);
-		Grave.register();
 
 		try {
 			mHttpManager = new HttpManager(this);
@@ -289,7 +286,6 @@ public class Plugin extends JavaPlugin {
 		manager.registerEvents(new LootChestsInInventory(), this);
 		manager.registerEvents(new ArrowListener(this), this);
 		manager.registerEvents(new SinglePageGUIManager(this), this);
-		manager.registerEvents(new GraveListener(this), this);
 
 		//  Move the logic out of Plugin and into it's own class that derives off Runnable, a Timer class of some type.
 		getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
