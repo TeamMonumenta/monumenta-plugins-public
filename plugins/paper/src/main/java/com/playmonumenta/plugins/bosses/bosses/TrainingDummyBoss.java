e9c461f144af4834bccce591de44c622a24656ff
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2021-06-01 13:11:33 -0700

    Rounded training dummy damage message to 5 digits (#688)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/TrainingDummyBoss.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/TrainingDummyBoss.java
@@ -1,5 +1,6 @@
 package com.playmonumenta.plugins.bosses.bosses;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 import org.bukkit.ChatColor;
@@ -17,6 +18,7 @@ import com.playmonumenta.plugins.bosses.spells.SpellRunAction;
 public class TrainingDummyBoss extends BossAbilityGroup {
 	public static final String identityTag = "boss_training_dummy";
 	public static final int detectionRange = 25;
+	private static final DecimalFormat cutoffDigits = new DecimalFormat("#.#####"); // number of #s determines maximum digits shown
 
 	public static BossAbilityGroup deserialize(Plugin plugin, LivingEntity boss) throws Exception {
 		return new TrainingDummyBoss(plugin, boss);
@@ -44,9 +46,14 @@ public class TrainingDummyBoss extends BossAbilityGroup {
 			return;
 		}
 
+		String damageString = cutoffDigits.format(damage);
+		if (!damageString.contains(".")) {
+			damageString += ".0"; // DecimalFormat would take 1.0 to "1", but the ".0" is desired
+		}
+
 		if (damager instanceof Player) {
 			Player player = (Player) damager;
-			player.sendMessage(ChatColor.GOLD + "Damage: " + ChatColor.RED + damage);
+			player.sendMessage(ChatColor.GOLD + "Damage: " + ChatColor.RED + damageString);
 		}
 
 		if (damager instanceof Projectile) {
@@ -54,7 +61,7 @@ public class TrainingDummyBoss extends BossAbilityGroup {
 
 			if (projectile.getShooter() instanceof Player) {
 				Player player = (Player) projectile.getShooter();
-				player.sendMessage(ChatColor.GOLD + "Damage: " + ChatColor.RED + damage);
+				player.sendMessage(ChatColor.GOLD + "Damage: " + ChatColor.RED + damageString);
 			}
 		}
 	}
