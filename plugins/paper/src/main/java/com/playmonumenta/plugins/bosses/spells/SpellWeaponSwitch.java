c39bbf9d5803c050b33ec4722549836775ca4fa8
Timothy Southwick <NickNackGus@gmail.com>
2021-07-08 13:22:11 -0400

    SpellWeaponSwitch now supports crossbows (#773)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/SpellWeaponSwitch.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/SpellWeaponSwitch.java
@@ -1,11 +1,16 @@
 package com.playmonumenta.plugins.bosses.spells;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Mob;
 import org.bukkit.inventory.ItemStack;
 
 public class SpellWeaponSwitch extends Spell {
+	private static final Set<Material> RANGED_MATERIALS = EnumSet.of(Material.BOW, Material.CROSSBOW, Material.TRIDENT);
+
 	private final Mob mLauncher;
 
 	public SpellWeaponSwitch(Mob launcher) {
@@ -30,22 +35,19 @@ public class SpellWeaponSwitch extends Spell {
 		if (mLauncher.getLocation().distance(target.getLocation()) > 6) {
 			// Switch to ranged weapon if not already equipped
 
-			if (!(curItem.getType().equals(Material.BOW) || curItem.getType().equals(Material.TRIDENT))
-				&& (offItem.getType().equals(Material.BOW) || offItem.getType().equals(Material.TRIDENT))) {
+			if (!RANGED_MATERIALS.contains(curItem.getType())
+				&& RANGED_MATERIALS.contains(offItem.getType())) {
 				// Need to switch hands - offhand is ranged
 				mLauncher.getEquipment().setItemInMainHand(offItem);
 				mLauncher.getEquipment().setItemInOffHand(curItem);
 			}
 		} else {
 			// Switch to non-ranged weapon if not already equipped
-
-			if (curItem.getType().equals(Material.BOW) || curItem.getType().equals(Material.TRIDENT)) {
-				// Need to switch - using a ranged weapon currently
-				if (!(offItem.getType().equals(Material.BOW) || offItem.getType().equals(Material.TRIDENT))) {
-					// Need to switch hands - offhand is not ranged and mainhand is
-					mLauncher.getEquipment().setItemInMainHand(offItem);
-					mLauncher.getEquipment().setItemInOffHand(curItem);
-				}
+			if (RANGED_MATERIALS.contains(curItem.getType())
+				&& !RANGED_MATERIALS.contains(offItem.getType())) {
+				// Need to switch hands - offhand is not ranged and mainhand is
+				mLauncher.getEquipment().setItemInMainHand(offItem);
+				mLauncher.getEquipment().setItemInOffHand(curItem);
 			}
 		}
 	}
