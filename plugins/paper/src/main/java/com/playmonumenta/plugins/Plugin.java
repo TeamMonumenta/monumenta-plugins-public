b9a54b3b9d906be064d996d8d3bd3c73b3534606
dmdinnc <38630939+dmdinnc@users.noreply.github.com>
2021-07-09 00:26:41 -0400

    gui: un-remove command registration (#785)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/Plugin.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/Plugin.java
@@ -57,6 +57,7 @@ import com.playmonumenta.plugins.commands.UnlockHeldItem;
 import com.playmonumenta.plugins.commands.ToggleSwap;
 import com.playmonumenta.plugins.commands.UnsignBook;
 import com.playmonumenta.plugins.commands.UpdateHeldItem;
+import com.playmonumenta.plugins.custominventories.CustomInventoryCommands;
 import com.playmonumenta.plugins.effects.EffectManager;
 import com.playmonumenta.plugins.enchantments.EnchantmentManager;
 import com.playmonumenta.plugins.guis.SinglePageGUIManager;
@@ -211,6 +212,7 @@ public class Plugin extends JavaPlugin {
 		LockedHeldItem.register();
 		UnlockHeldItem.register();
 		ToggleSwap.register(this);
+		CustomInventoryCommands.register(this);
 
 		try {
 			mHttpManager = new HttpManager(this);
