7580289e1dd0db3429298165b5b80b1d329a0b17
Luke Silvestri <40128099+silvestriluke@users.noreply.github.com>
2021-07-28 21:43:37 -0400

    Lime delve infusion number tweak (#831)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/custominventories/DelveInfusionCustomInventory.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/custominventories/DelveInfusionCustomInventory.java
@@ -116,7 +116,7 @@ public class DelveInfusionCustomInventory extends CustomInventory {
 		ItemStack limeItem = new ItemStack(Material.LIME_WOOL);
 		ItemMeta limeMeta = limeItem.getItemMeta();
 		limeMeta.displayName(Component.text("Empowered", TextColor.fromCSSHexString("#55FF55")).decoration(TextDecoration.BOLD, true).decoration(TextDecoration.ITALIC, false));
-		splitLoreLine(limeMeta, "When you gain XP, you have a 1% chance per XP point per level to repair all currently equipped items by 1% of their max durability.", MAX_LORE_LENGHT, ChatColor.GRAY);
+		splitLoreLine(limeMeta, "When you gain XP, you have a 0.25% chance per XP point per level to repair all currently equipped items by 1% of their max durability.", MAX_LORE_LENGHT, ChatColor.GRAY);
 		limeItem.setItemMeta(limeMeta);
 		mDelvePannelList.add(limeItem);
 
@@ -271,7 +271,7 @@ public class DelveInfusionCustomInventory extends CustomInventory {
 			ItemStack pannel = new ItemStack(Material.LIME_WOOL, 1);
 			ItemMeta meta = pannel.getItemMeta();
 			meta.displayName(Component.text("Empowered level " + (i + 1), TextColor.fromCSSHexString("#55FF55")).decoration(TextDecoration.BOLD, true).decoration(TextDecoration.ITALIC, false));
-			splitLoreLine(meta, "When you gain XP, you have a " + (i + 1) + "% chance per XP point to repair all currently equipped items by 1% of their max durability.", MAX_LORE_LENGHT, ChatColor.GRAY);
+			splitLoreLine(meta, "When you gain XP, you have a " + 0.25 * (i + 1) + "% chance per XP point to repair all currently equipped items by 1% of their max durability.", MAX_LORE_LENGHT, ChatColor.GRAY);
 			pannel.setItemMeta(meta);
 			limeItems.add(pannel);
 		}
