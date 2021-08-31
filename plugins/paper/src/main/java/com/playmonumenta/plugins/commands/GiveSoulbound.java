68a3be58355323d6d466317370ed44e1815d3d6f
Byron Marohn <Combustible@live.com>
2021-08-31 03:18:01 +0000

    Add an optional loot table argument to /givesoulbound
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- plugins/paper/src/main/java/com/playmonumenta/plugins/commands/GiveSoulbound.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/commands/GiveSoulbound.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import com.playmonumenta.plugins.utils.FastUtils;
 import com.playmonumenta.plugins.utils.InventoryUtils;
 import com.playmonumenta.plugins.utils.ItemUtils;
 
@@ -11,15 +12,16 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
-import net.kyori.adventure.text.Component;
+import org.bukkit.loot.LootContext;
+import org.bukkit.loot.LootTable;
 
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.CommandPermission;
-import dev.jorel.commandapi.arguments.Argument;
 import dev.jorel.commandapi.arguments.EntitySelectorArgument;
 import dev.jorel.commandapi.arguments.EntitySelectorArgument.EntitySelector;
 import dev.jorel.commandapi.arguments.ItemStackArgument;
+import dev.jorel.commandapi.arguments.LootTableArgument;
+import net.kyori.adventure.text.Component;
 
 /*
  * NOTICE!
@@ -33,15 +35,31 @@ public class GiveSoulbound extends GenericCommand {
 	public static void register() {
 		CommandPermission perms = CommandPermission.fromString("monumenta.command.givesoulbound");
 
-		List<Argument> arguments = new ArrayList<>();
-		arguments.add(new EntitySelectorArgument("players", EntitySelector.MANY_PLAYERS));
-		arguments.add(new ItemStackArgument("item"));
 		new CommandAPICommand("givesoulbound")
 			.withPermission(perms)
-			.withArguments(arguments)
+			.withArguments(new EntitySelectorArgument("players", EntitySelector.MANY_PLAYERS))
+			.withArguments(new ItemStackArgument("item"))
 			.executes((sender, args) -> {
 				for (Player player : (Collection<Player>)args[0]) {
 					give(player, (ItemStack)args[1]);
+				}
+			})
+			.register();
+
+		new CommandAPICommand("givesoulbound")
+			.withPermission(perms)
+			.withArguments(new EntitySelectorArgument("players", EntitySelector.MANY_PLAYERS))
+			.withArguments(new LootTableArgument("item_loot_table"))
+			.executes((sender, args) -> {
+				for (Player player : (Collection<Player>)args[0]) {
+					LootTable table = (LootTable)args[1];
+					if (table != null) {
+						Collection<ItemStack> loot = table.populateLoot(FastUtils.RANDOM, new LootContext.Builder(player.getLocation()).build());
+						if (!loot.isEmpty()) {
+							/* Give the player the first item in the collection */
+							give(player, loot.iterator().next());
+						}
+					}
 				}
 			})
 			.register();
