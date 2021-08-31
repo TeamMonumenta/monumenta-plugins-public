20cbcc7bd556bc11b107cb826d4ff97cd2a1cba1
Byron Marohn <Combustible@live.com>
2021-08-31 23:40:35 +0000

    Add another guard against withering players to magenta tesseract
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- plugins/paper/src/main/java/com/playmonumenta/plugins/enchantments/other/PestilenceTesseract.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/enchantments/other/PestilenceTesseract.java
@@ -6,14 +6,6 @@ import java.util.Collection;
 import java.util.EnumSet;
 import java.util.Map;
 
-import org.bukkit.Location;
-import org.bukkit.Particle;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
-import org.bukkit.scheduler.BukkitRunnable;
-
 import com.playmonumenta.plugins.Constants;
 import com.playmonumenta.plugins.Plugin;
 import com.playmonumenta.plugins.enchantments.BaseSpawnableItemEnchantment;
@@ -21,6 +13,15 @@ import com.playmonumenta.plugins.enchantments.EnchantmentManager.ItemSlot;
 import com.playmonumenta.plugins.utils.EntityUtils;
 import com.playmonumenta.plugins.utils.ParticleUtils;
 import com.playmonumenta.plugins.utils.ParticleUtils.SpawnParticleAction;
+
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PestilenceTesseract implements BaseSpawnableItemEnchantment {
 	private static final String PROPERTY_NAME = "PestilenceTesseract";
@@ -56,8 +57,10 @@ public class PestilenceTesseract implements BaseSpawnableItemEnchantment {
 				ParticleUtils.explodingRingEffect(plugin, item.getLocation().add(0, 0.5, 0), EFFECT_RADIUS, PARTICLE_RING_HEIGHT, TICK_PERIOD, PARTICLES);
 
 				for (LivingEntity mob : EntityUtils.getNearbyMobs(item.getLocation(), EFFECT_RADIUS)) {
-					mob.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 600, 2, false, true));
-					EntityUtils.applySlow(plugin, 600, 0.2, mob);
+					if (!(mob instanceof Player)) {
+						mob.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 600, 2, false, true));
+						EntityUtils.applySlow(plugin, 600, 0.2, mob);
+					}
 				}
 
 				mTicks++;
