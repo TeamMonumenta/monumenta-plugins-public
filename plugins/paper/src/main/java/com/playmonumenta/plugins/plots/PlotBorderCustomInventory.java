237feec34d29acbc91cf7af3e1ad4cc06b1d2fb3
Byron Marohn <Combustible@live.com>
2021-12-12 05:47:50 +0000

    Patreon score bump to 10
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- plugins/paper/src/main/java/com/playmonumenta/plugins/plots/PlotBorderCustomInventory.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/plots/PlotBorderCustomInventory.java
@@ -89,7 +89,7 @@ public class PlotBorderCustomInventory extends CustomInventory {
 	static {
 		BORDER_ITEMS.add(new TeleportEntry(0, 47, "Base Choices", "Click to view the plot borders with no requirements.", Material.GRASS_BLOCK, null, 0, "page 1"));
 		BORDER_ITEMS.add(new TeleportEntry(0, 49, "Unlockable Choices", "Click to view plot borders locked behind completion of content.", Material.IRON_INGOT, null, 0, "page 2"));
-		BORDER_ITEMS.add(new TeleportEntry(0, 51, "Patreon Choices", "Click to view options only available to Patrons.", Material.GOLD_INGOT, "Patreon", 5, "page 3"));
+		BORDER_ITEMS.add(new TeleportEntry(0, 51, "Patreon Choices", "Click to view options only available to Patrons.", Material.GOLD_INGOT, "Patreon", 10, "page 3"));
 
 
 		BORDER_ITEMS.add(new TeleportEntry(1, 20, "Narsen Village", "A small town of Narsen citizens, bearing some resemblance to the old plots world.", Material.LIGHT_BLUE_CONCRETE, null, 0, "narsen_village"));
