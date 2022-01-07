ceb88688a0daa27c517b31275ad9fc0159aef9f4
Peter Güttinger <njol@gmx.ch>
2022-01-07 02:51:16 +0100

    Fixed items not stacking due to the grave sort change (#1115)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/listeners/DeathItemListener.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/listeners/DeathItemListener.java
@@ -1,12 +1,11 @@
 package com.playmonumenta.plugins.listeners;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Item;
@@ -15,13 +14,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 import com.playmonumenta.plugins.Plugin;
+import com.playmonumenta.plugins.graves.GraveItem;
+import com.playmonumenta.plugins.graves.GraveManager;
 import com.playmonumenta.plugins.utils.FastUtils;
 import com.playmonumenta.plugins.utils.InventoryUtils;
 import com.playmonumenta.plugins.utils.ItemUtils;
@@ -33,143 +31,46 @@ import de.tr7zw.nbtapi.NBTEntity;
 public class DeathItemListener implements Listener {
 	private static final String DEATH_SORT_TAG = "DeathSortTag";
 
-	//Snapshot of the player's inventory at death
-	private final Map<UUID, List<ItemStack>> mBeforeDeathItems = new HashMap<>();
-	private final Map<UUID, BukkitRunnable> mTimers30Sec = new HashMap<>();
-	private final Map<UUID, BukkitRunnable> mTimers15Min = new HashMap<>();
 	private final Plugin mPlugin;
 
 	public DeathItemListener(Plugin plugin) {
 		mPlugin = plugin;
 	}
 
-	//Records player's items into a map that gives slot of a hashmap
-	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-	public void recordPlayerDeathEvent(PlayerDeathEvent event) {
-		if (
-			//Player contains PEB tag
-			event.getEntity().getScoreboardTags().contains(DEATH_SORT_TAG)
-				//In case a problem happens and the peb option can be turned off
-				&& event.getEntity().hasPermission("monumenta.deathitemsort")
-				//Player is in an area where they drop their items
-				&& !event.getKeepInventory()) {
-
-			@Nullable ItemStack[] contents = event.getEntity().getInventory().getContents();
-			List<@Nullable ItemStack> beforeDeathItems = Arrays.asList(contents);
-
-			//Do not put in the death map of a player with no items
-			for (ItemStack item : beforeDeathItems) {
-				if (item != null && !item.getType().isAir() && item.hasItemMeta() && !item.getType().equals(Material.COMPASS)) {
-					//Found at least one non-air non-compass item with meta
-					//Save the player's death inventory
-					//Update inventory includes the 15 minute timer that starts to run on death (synced up to hoped items despawn timer)
-					mBeforeDeathItems.put(event.getEntity().getUniqueId(), beforeDeathItems);
-					cancelTimers(event.getEntity().getUniqueId());
-					//Clears the saving of the players inventory after 15 minutes
-					runClearMapDelay(event.getEntity().getUniqueId(), 20 * 60 * 15, mTimers15Min);
-					break;
-				}
-			}
-		}
-	}
-
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void checkItemEvent(EntityPickupItemEvent event) {
-		if (!(event.getEntity() instanceof Player) || !event.getEntity().getScoreboardTags().contains(DEATH_SORT_TAG)) {
+		if (!(event.getEntity() instanceof Player player) || !event.getEntity().getScoreboardTags().contains(DEATH_SORT_TAG)) {
 			return; // not a player, or they don't have death sort enabled
 		}
 
 		//Get the item picked up
-		Player player = (Player) event.getEntity();
 		PlayerInventory inventory = player.getInventory();
-		ItemStack item = event.getItem().getItemStack();
+		Item entity = event.getItem();
+		GraveItem graveItem = GraveManager.getGraveItem(entity);
 
-		//Get the player's inventory before they died
-		List<ItemStack> beforeDeathItems = mBeforeDeathItems.get(player.getUniqueId());
-		if (beforeDeathItems == null) {
-			return; //No data
-		}
-
-		//Combine item with the same item if it can be stacked into it (do nothing)
-		for (int slot = 0; slot < beforeDeathItems.size(); slot++) {
-			ItemStack invItem = inventory.getItem(slot);
-			if (invItem != null && !invItem.getType().isAir() && invItem.getAmount() < invItem.getMaxStackSize() && invItem.isSimilar(item)) {
-				//Item picked up will stack with one already in inventory - don't need to do anything
-				return;
-			}
-		}
-
-		//Attempt to place this in an empty inventory slot where it previously was
-		for (int slot = 0; slot < beforeDeathItems.size(); slot++) {
-			ItemStack originalItem = beforeDeathItems.get(slot);
-			ItemStack invItem = inventory.getItem(slot);
-			if (originalItem != null && (invItem == null || invItem.getType().isAir()) && originalItem.isSimilar(item)) {
-				//Put the item back in this slot, unless it was an armor slot and the item was curse of binding
-				if (slot < 36 || !item.containsEnchantment(Enchantment.BINDING_CURSE)) {
-					if (ItemUtils.isArmor(item) && slot >= 36 && slot <= 39) {
-						player.getWorld().playSound(player.getLocation(), ItemUtils.getArmorEquipSound(item.getType()), 0.75f, 1);
-					}
-
-					//Simulate item pickup
-					inventory.setItem(slot, event.getItem().getItemStack());
-					InventoryUtils.scheduleDelayedEquipmentSlotCheck(mPlugin, player, slot);
-					simulatePickup(event.getItem(), player);
-					event.setCancelled(true);
-
-					//Delete the inventory slots data after 30 seconds once the player successfully picks up one item from their death pile
-					runClearMapDelay(player.getUniqueId(), 20 * 30, mTimers30Sec);
-
-					return; //Nothing left to do, this item is handled
+		// Try putting the item back in its slot if it is an item from a grave
+		if (graveItem != null && graveItem.mSlot != null) {
+			if (insertIntoSlot(player, entity, graveItem.mSlot)) {
+				event.setCancelled(true);
+				return; // Nothing left to do, this item is handled
+			} else {
+				// Combine item with the same item if it can be stacked into it (do nothing)
+				ItemStack invItem = inventory.getItem(graveItem.mSlot);
+				if (invItem != null && !invItem.getType().isAir() && invItem.getAmount() < invItem.getMaxStackSize() && invItem.isSimilar(entity.getItemStack())) {
+					// Item picked up will stack with one already in inventory - don't need to do anything
+					return;
 				}
 			}
 		}
 
-		//Attempt to place this in an empty inventory slot that was empty before death
-		for (int slot = 0; slot < 36; slot++) {
-			ItemStack originalItem = beforeDeathItems.get(slot);
-			ItemStack invItem = inventory.getItem(slot);
-			if ((originalItem == null || originalItem.getType().isAir()) && (invItem == null || invItem.getType().isAir())) {
-				//Found an empty slot that was empty before death
-
-				//Simulate item pickup
-				inventory.setItem(slot, event.getItem().getItemStack());
-				InventoryUtils.scheduleDelayedEquipmentSlotCheck(mPlugin, player, slot);
-				simulatePickup(event.getItem(), player);
-				event.setCancelled(true);
-
-				return; //Nothing left to do, this item is handled
-			}
+		// Attempt to place this in an empty inventory slot that was empty before recent deaths
+		if (pickupIntoEmptySlot(player, entity, true)) {
+			event.setCancelled(true);
+			return; // Nothing left to do, this item is handled
 		}
 
 		//Failed to put it on an existing stack, or where it was before, or in an empty slot before death.
 		//Just let normal item pickup do its thing
-	}
-
-	//Clears the saving of the players inventory after some delay
-	private void runClearMapDelay(UUID id, int delay, Map<UUID, BukkitRunnable> trackingMap) {
-		if (!trackingMap.containsKey(id)) {
-			BukkitRunnable runnable = new BukkitRunnable() {
-				@Override
-				public void run() {
-					mBeforeDeathItems.remove(id);
-					cancelTimers(id);
-				}
-			};
-			runnable.runTaskLater(mPlugin, delay);
-			trackingMap.put(id, runnable);
-		}
-	}
-
-	//Cancels and removes the map wipe timers
-	private void cancelTimers(UUID id) {
-		BukkitRunnable timer = mTimers15Min.remove(id);
-		if (timer != null) {
-			timer.cancel();
-		}
-		timer = mTimers30Sec.remove(id);
-		if (timer != null) {
-			timer.cancel();
-		}
 	}
 
 	//Makes the item seem like it was picked up by the player animation wise, and then deletes it
@@ -188,69 +89,20 @@ public class DeathItemListener implements Listener {
 		player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ITEM_PICKUP, 0.25f, (float) FastUtils.randomDoubleInRange(1, 2));
 	}
 
-	public int pickupItem(Player player, Item entity) {
+	public int pickupItem(Player player, Item entity, GraveItem graveItem) {
 		PlayerInventory inventory = player.getInventory();
 		ItemStack item = entity.getItemStack();
-		if (player.getScoreboardTags().contains(DEATH_SORT_TAG) && mBeforeDeathItems.containsKey(player.getUniqueId())) {
-			List<ItemStack> beforeDeathItems = mBeforeDeathItems.get(player.getUniqueId());
 
-			//Combine item with the same item if it can be stacked into it
-			for (int slot = 0; slot < beforeDeathItems.size(); slot++) {
-				ItemStack invItem = inventory.getItem(slot);
-				if (invItem != null && !invItem.getType().isAir() && invItem.getAmount() < invItem.getMaxStackSize() && invItem.isSimilar(item)) {
-					if (invItem.getAmount() + item.getAmount() <= invItem.getMaxStackSize()) {
-						// Full amount can be combined
-						invItem.add(item.getAmount());
-						simulatePickup(entity, player);
-						return 0;
-					} else {
-						// Partial amount can be combined
-						item.setAmount(invItem.getAmount() + item.getAmount() - invItem.getMaxStackSize());
-						invItem.setAmount(invItem.getMaxStackSize());
-						entity.setItemStack(item);
-					}
-				}
-			}
+		//Attempt to place this in an empty inventory slot where it previously was
+		if (player.getScoreboardTags().contains(DEATH_SORT_TAG)
+			    && graveItem.mSlot != null
+			    && insertIntoSlot(player, entity, graveItem.mSlot)) {
+			return 0; //Nothing left to do, this item is handled
+		}
 
-			//Attempt to place this in an empty inventory slot where it previously was
-			for (int slot = 0; slot < beforeDeathItems.size(); slot++) {
-				ItemStack originalItem = beforeDeathItems.get(slot);
-				ItemStack invItem = inventory.getItem(slot);
-				if (originalItem != null && (invItem == null || invItem.getType().isAir()) && originalItem.isSimilar(item)) {
-					//Put the item back in this slot, unless it was an armor slot and the item was curse of binding
-					if (slot < 36 || (!item.containsEnchantment(Enchantment.BINDING_CURSE) && !ItemUtils.isShatteredWearable(item))) {
-						if (ItemUtils.isArmor(item) && slot >= 36 && slot <= 39) {
-							player.getWorld().playSound(player.getLocation(), ItemUtils.getArmorEquipSound(item.getType()), 0.75f, 1);
-						}
-
-						//Simulate item pickup
-						inventory.setItem(slot, item);
-						InventoryUtils.scheduleDelayedEquipmentSlotCheck(mPlugin, player, slot);
-						simulatePickup(entity, player);
-
-						//Delete the inventory slots data after 30 seconds once the player successfully picks up one item from their death pile
-						runClearMapDelay(player.getUniqueId(), 20 * 30, mTimers30Sec);
-
-						return 0; //Nothing left to do, this item is handled
-					}
-				}
-			}
-
-			//Attempt to place this in an empty inventory slot that was empty before death
-			for (int slot = 0; slot < 36; slot++) {
-				ItemStack originalItem = beforeDeathItems.get(slot);
-				ItemStack invItem = inventory.getItem(slot);
-				if ((originalItem == null || originalItem.getType().isAir()) && (invItem == null || invItem.getType().isAir())) {
-					//Found an empty slot that was empty before death
-
-					//Simulate item pickup
-					inventory.setItem(slot, item);
-					InventoryUtils.scheduleDelayedEquipmentSlotCheck(mPlugin, player, slot);
-					simulatePickup(entity, player);
-
-					return 0; //Nothing left to do, this item is handled
-				}
-			}
+		//Attempt to place this in an empty inventory slot that was empty before death
+		if (pickupIntoEmptySlot(player, entity, false)) {
+			return 0; //Nothing left to do, this item is handled
 		}
 
 		//Attempt to place this in any empty slot
@@ -264,4 +116,100 @@ public class DeathItemListener implements Listener {
 		// Couldn't place the item, return how many are left
 		return item.getAmount();
 	}
+
+	/**
+	 * Attempts to put the given grave item into the given slot if the slot is empty. Will not attempt to fill an existing stack in the slot.
+	 *
+	 * @return Whether teh item was put into the slot
+	 */
+	private boolean insertIntoSlot(Player player, Item entity, int slot) {
+		PlayerInventory inventory = player.getInventory();
+		ItemStack invItem = inventory.getItem(slot);
+		if (invItem != null && !invItem.getType().isAir()) {
+			return false;
+		}
+		ItemStack item = entity.getItemStack();
+		//Put the item back in this slot, unless it was an armor slot and the item was curse of binding
+		if (slot < 36 || (!item.containsEnchantment(Enchantment.BINDING_CURSE) && !ItemUtils.isShatteredWearable(item))) {
+			if (ItemUtils.isArmor(item) && slot >= 36 && slot <= 39) {
+				player.getWorld().playSound(player.getLocation(), ItemUtils.getArmorEquipSound(item.getType()), 0.75f, 1);
+			}
+
+			//Simulate item pickup
+			inventory.setItem(slot, item);
+			InventoryUtils.scheduleDelayedEquipmentSlotCheck(mPlugin, player, slot);
+			simulatePickup(entity, player);
+
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Attempts to pick up the given item inta an empty inventory slot that has been empty before any recent deaths if death sort is enabled.
+	 * Or more accurately, into a slot not currently "reserved" for items in recent graves.
+	 * Will attempt to stack into existing slots first if there are recent graves or if doNothingIfNoRecentGraves is false.
+	 *
+	 * @return Whether the item was picked up completely
+	 */
+	private boolean pickupIntoEmptySlot(Player player, Item entity, boolean doNothingIfNoRecentGraves) {
+		PlayerInventory inventory = player.getInventory();
+
+		Set<Integer> reservedItemSlots = null;
+		if (player.getScoreboardTags().contains(DEATH_SORT_TAG)) {
+			GraveManager graveManager = GraveManager.getInstance(player);
+			if (graveManager != null) {
+				Instant considerGravesAfter = Instant.now().minus(30, ChronoUnit.MINUTES);
+				reservedItemSlots = graveManager.getGraves().stream()
+					.filter(grave -> grave.getDeathTime().isAfter(considerGravesAfter))
+					.flatMap(grave -> grave.getItems().stream())
+					.map(i -> i.mSlot)
+					.filter(Objects::nonNull)
+					.collect(Collectors.toSet());
+			}
+		}
+
+		if (doNothingIfNoRecentGraves && (reservedItemSlots == null || reservedItemSlots.isEmpty())) {
+			return false;
+		}
+
+		// Combine item with the same item if it can be stacked into it
+		ItemStack item = entity.getItemStack();
+		for (int slot = 0; slot < 36; slot++) {
+			ItemStack invItem = inventory.getItem(slot);
+			if (invItem != null && !invItem.getType().isAir() && invItem.getAmount() < invItem.getMaxStackSize() && invItem.isSimilar(item)) {
+				if (invItem.getAmount() + item.getAmount() <= invItem.getMaxStackSize()) {
+					// Full amount can be combined
+					invItem.add(item.getAmount());
+					simulatePickup(entity, player);
+					return true;
+				} else {
+					// Partial amount can be combined
+					item.setAmount(invItem.getAmount() + item.getAmount() - invItem.getMaxStackSize());
+					invItem.setAmount(invItem.getMaxStackSize());
+					entity.setItemStack(item);
+				}
+			}
+		}
+
+		// Attempt to place this in an empty inventory slot that was empty before recent deaths
+		if (reservedItemSlots != null && !reservedItemSlots.isEmpty()) {
+			for (int slot = 0; slot < 36; slot++) {
+				ItemStack invItem = inventory.getItem(slot);
+				if (!reservedItemSlots.contains(slot) && (invItem == null || invItem.getType().isAir())) {
+					//Found an empty slot that was empty before death
+
+					//Simulate item pickup
+					inventory.setItem(slot, entity.getItemStack());
+					InventoryUtils.scheduleDelayedEquipmentSlotCheck(mPlugin, player, slot);
+					simulatePickup(entity, player);
+
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
 }
