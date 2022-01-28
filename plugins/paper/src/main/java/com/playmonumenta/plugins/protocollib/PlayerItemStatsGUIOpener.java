54f9e54b7dd04a2cd74f2aaab1a65918ad409cb5
Peter Güttinger <njol@gmx.ch>
2022-01-28 23:04:11 +0100

    Fixed /vf not working (#1184)
    * Improved stat gui listener

--- plugins/paper/src/main/java/com/playmonumenta/plugins/protocollib/PlayerItemStatsGUIOpener.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/protocollib/PlayerItemStatsGUIOpener.java
@@ -4,33 +4,25 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
 import com.playmonumenta.plugins.itemstats.PlayerItemStatsGUI;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-
-import java.util.logging.Level;
 
 public class PlayerItemStatsGUIOpener extends PacketAdapter {
 
 	private final Plugin mPlugin;
-	private final long mMainThreadId;
 
-	public PlayerItemStatsGUIOpener(Plugin plugin, long mainThreadId) {
+	public PlayerItemStatsGUIOpener(Plugin plugin) {
 		super(plugin, PacketType.Play.Client.RECIPE_SETTINGS);
 		mPlugin = plugin;
-		mMainThreadId = mainThreadId;
 	}
 
 	@Override
 	public void onPacketReceiving(PacketEvent event) {
-		if (mMainThreadId != Thread.currentThread().getId()) {
-			mPlugin.getLogger().log(Level.WARNING, "Thread mismatch on synchronous PacketListener PlayerItemStatsGUIOpener");
-		} else {
-			Player player = event.getPlayer();
+		Player player = event.getPlayer();
+		Bukkit.getScheduler().runTask(mPlugin, () -> {
 			new PlayerItemStatsGUI(player).openInventory(player, mPlugin);
-
-			// TODO: this doesn't work, need to cancel the opening somehow; canceling event doesn't work, and neither does putting stuff in onPacketSending()
-			event.getPacket().getBooleans().write(0, false).write(1, false);
-		}
+		});
 	}
 
 }
--- plugins/paper/src/main/java/com/playmonumenta/plugins/protocollib/ProtocolLibIntegration.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/protocollib/ProtocolLibIntegration.java
@@ -1,6 +1,5 @@
 package com.playmonumenta.plugins.protocollib;
 
-import com.comphenix.protocol.AsynchronousManager;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.playmonumenta.plugins.Plugin;
@@ -14,21 +13,19 @@ public class ProtocolLibIntegration {
 		Logger logger = plugin.getLogger();
 		logger.info("Enabling ProtocolLib integration");
 
-		ProtocolManager asyncManager = ProtocolLibrary.getProtocolManager();
-		AsynchronousManager syncManager = asyncManager.getAsynchronousManager();
-		long mainThreadId = Thread.currentThread().getId();
+		ProtocolManager syncManager = ProtocolLibrary.getProtocolManager();
 
-		// Must register in synchronous fashion for anything that calls Paper methods; these should check that the threads match before doing anything or bad things will happen
-		syncManager.registerAsyncHandler(new PlayerItemStatsGUIOpener(plugin, mainThreadId)).syncStart();
+		syncManager.addPacketListener(new PlayerItemStatsGUIOpener(plugin));
 
-		// Register in asynchronous fashion for things that only use ProtocolLib
 		if (ServerProperties.getReplaceSpawnerEntities()) {
 			logger.info("Enabling replacement of spawner entities");
-			asyncManager.addPacketListener(new SpawnerEntityReplacer(plugin));
+			syncManager.addPacketListener(new SpawnerEntityReplacer(plugin));
 		} else {
 			logger.info("Will not replace spawner entities on this shard");
 		}
-		asyncManager.addPacketListener(new GlowingReplacer(plugin));
+		syncManager.addPacketListener(new GlowingReplacer(plugin));
+		syncManager.addPacketListener(new VirtualFirmamentReplacer(plugin));
+		syncManager.addPacketListener(new FirmamentLagFix(plugin));
 
 	}
 
