2d186601c408d267e0106acc26ada32080e2f83b
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2022-01-29 13:16:20 -0800

    fixed panacea not doing damage (#1213)
    * fixed panacea not doing damage
    
    Signed-off-by: NobodyPi <officialnobodypi@gmail.com>
    
    * fix pmd error
    
    Signed-off-by: NobodyPi <officialnobodypi@gmail.com>

--- plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/alchemist/apothecary/Panacea.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/alchemist/apothecary/Panacea.java
@@ -7,7 +7,6 @@ import com.playmonumenta.plugins.abilities.AbilityTrigger;
 import com.playmonumenta.plugins.abilities.alchemist.AlchemistPotions;
 import com.playmonumenta.plugins.classes.ClassAbility;
 import com.playmonumenta.plugins.events.DamageEvent;
-import com.playmonumenta.plugins.events.DamageEvent.DamageType;
 import com.playmonumenta.plugins.utils.AbsorptionUtils;
 import com.playmonumenta.plugins.utils.DamageUtils;
 import com.playmonumenta.plugins.utils.EntityUtils;
@@ -37,7 +36,6 @@ import java.util.Iterator;
 import java.util.List;
 
 
-
 public class Panacea extends Ability {
 
 	private static final double PANACEA_1_DAMAGE_FRACTION = 0.8;
@@ -56,9 +54,10 @@ public class Panacea extends Ability {
 	private static final Particle.DustOptions APOTHECARY_DARK_COLOR = new Particle.DustOptions(Color.fromRGB(83, 0, 135), 1.0f);
 	private static final int COOLDOWN = 24 * 20;
 
-	private double mDamage;
+	private double mDamageFraction;
 	private final int mShield;
 	private int mSlowTicks;
+	private @Nullable AlchemistPotions mAlchemistPotions;
 
 	public Panacea(Plugin plugin, @Nullable Player player) {
 		super(plugin, player, "Panacea");
@@ -69,15 +68,12 @@ public class Panacea extends Ability {
 		mInfo.mCooldown = COOLDOWN;
 		mInfo.mLinkedSpell = ClassAbility.PANACEA;
 		mInfo.mTrigger = AbilityTrigger.LEFT_CLICK;
-		mDamage = 0;
-		Bukkit.getScheduler().runTask(Plugin.getInstance(), () -> {
-			AlchemistPotions alchemistPotions = AbilityManager.getManager().getPlayerAbilityIgnoringSilence(player, AlchemistPotions.class);
-			if (alchemistPotions != null) {
-				mDamage = alchemistPotions.getDamage() * (getAbilityScore() == 1 ? PANACEA_1_DAMAGE_FRACTION : PANACEA_2_DAMAGE_FRACTION);
-			}
-		});
+		mDamageFraction = getAbilityScore() == 1 ? PANACEA_1_DAMAGE_FRACTION : PANACEA_2_DAMAGE_FRACTION;
 		mSlowTicks = getAbilityScore() == 1 ? PANACEA_1_SLOW_TICKS : PANACEA_2_SLOW_TICKS;
 		mShield = getAbilityScore() == 1 ? PANACEA_1_SHIELD : PANACEA_2_SHIELD;
+		Bukkit.getScheduler().runTask(Plugin.getInstance(), () -> {
+			mAlchemistPotions = AbilityManager.getManager().getPlayerAbilityIgnoringSilence(player, AlchemistPotions.class);
+		});
 		mDisplayItem = new ItemStack(Material.PURPLE_CONCRETE_POWDER, 1);
 	}
 
@@ -97,114 +93,109 @@ public class Panacea extends Ability {
 
 		FixedMetadataValue playerItemStats = mPlugin.mItemStatManager.getPlayerItemStatsMetadata(mPlayer);
 
-		new BukkitRunnable() {
-			final Location mLoc = mPlayer.getEyeLocation();
-			final BoundingBox mBox = BoundingBox.of(mLoc, PANACEA_RADIUS, PANACEA_RADIUS, PANACEA_RADIUS);
-			Vector mIncrement = mLoc.getDirection().multiply(PANACEA_MOVE_SPEED);
+		if (mAlchemistPotions != null) {
+			double damage = mAlchemistPotions.getDamage() * mDamageFraction;
 
-			// Convoluted range parameter makes sure we grab all possible entities to be hit without recalculating manually
-			List<LivingEntity> mMobs = EntityUtils.getNearbyMobs(mLoc, PANACEA_MOVE_SPEED * PANACEA_MAX_DURATION + 2, mPlayer);
-			List<Player> mPlayers = PlayerUtils.otherPlayersInRange(mPlayer, PANACEA_MOVE_SPEED * PANACEA_MAX_DURATION + 2, true);
+			new BukkitRunnable() {
+				final Location mLoc = mPlayer.getEyeLocation();
+				final BoundingBox mBox = BoundingBox.of(mLoc, PANACEA_RADIUS, PANACEA_RADIUS, PANACEA_RADIUS);
+				Vector mIncrement = mLoc.getDirection().multiply(PANACEA_MOVE_SPEED);
 
-			int mTicks = 0;
-			int mReverseTick = 0;
-			double mDegree = 0;
-			boolean mReverse = false;
+				// Convoluted range parameter makes sure we grab all possible entities to be hit without recalculating manually
+				List<LivingEntity> mMobs = EntityUtils.getNearbyMobs(mLoc, PANACEA_MOVE_SPEED * PANACEA_MAX_DURATION + 2, mPlayer);
+				List<Player> mPlayers = PlayerUtils.otherPlayersInRange(mPlayer, PANACEA_MOVE_SPEED * PANACEA_MAX_DURATION + 2, true);
 
-			@Override
-			public void run() {
-				mBox.shift(mIncrement);
-				mLoc.add(mIncrement);
-				Iterator<LivingEntity> mobIter = mMobs.iterator();
-				while (mobIter.hasNext()) {
-					LivingEntity mob = mobIter.next();
-					if (mBox.overlaps(mob.getBoundingBox())) {
-						/*
-						 * Based on the ticks since reverse and the mob's noDamageTicks (11 to 20
-						 * is iframes range), we can somewhat deduce if the noDamageTicks were
-						 * triggered by the forward Amalgam hit and reset if necessary.
-						 */
-						if (mReverse && mReverseTick - mTicks + 10 <= mob.getNoDamageTicks()) {
-							mob.setNoDamageTicks(0);
+				int mTicks = 0;
+				int mReverseTick = 0;
+				double mDegree = 0;
+				boolean mReverse = false;
+
+				@Override
+				public void run() {
+					mBox.shift(mIncrement);
+					mLoc.add(mIncrement);
+					Iterator<LivingEntity> mobIter = mMobs.iterator();
+					while (mobIter.hasNext()) {
+						LivingEntity mob = mobIter.next();
+						if (mBox.overlaps(mob.getBoundingBox())) {
+							DamageEvent damageEvent = new DamageEvent(mob, mPlayer, mPlayer, DamageEvent.DamageType.MAGIC, mInfo.mLinkedSpell, damage);
+							damageEvent.setDelayed(true);
+							damageEvent.setPlayerItemStat(playerItemStats);
+							DamageUtils.damage(damageEvent, true, true, null);
+
+							EntityUtils.applySlow(mPlugin, mSlowTicks, 1, mob);
+							mobIter.remove();
+						}
+					}
+					Iterator<Player> playerIter = mPlayers.iterator();
+					while (playerIter.hasNext()) {
+						Player player = playerIter.next();
+						if (mBox.overlaps(player.getBoundingBox())) {
+							if (player != mPlayer) {
+								AbsorptionUtils.addAbsorption(player, mShield, PANACEA_MAX_SHIELD, PANACEA_ABSORPTION_DURATION);
+							}
+							playerIter.remove();
+						}
+					}
+
+					mDegree += 12;
+					Vector vec;
+					for (int i = 0; i < 2; i++) {
+						double radian1 = Math.toRadians(mDegree + (i * 180));
+						vec = new Vector(FastUtils.cos(radian1) * 0.325, 0, FastUtils.sin(radian1) * 0.325);
+						vec = VectorUtils.rotateXAxis(vec, mLoc.getPitch() - 90);
+						vec = VectorUtils.rotateYAxis(vec, mLoc.getYaw());
+
+						Location l = mLoc.clone().add(vec);
+						world.spawnParticle(Particle.REDSTONE, l, 5, 0.1, 0.1, 0.1, APOTHECARY_LIGHT_COLOR);
+						world.spawnParticle(Particle.REDSTONE, l, 5, 0.1, 0.1, 0.1, APOTHECARY_DARK_COLOR);
+					}
+					world.spawnParticle(Particle.SPELL_INSTANT, mLoc, 5, 0.35, 0.35, 0.35, 1);
+					world.spawnParticle(Particle.SPELL_WITCH, mLoc, 5, 0.35, 0.35, 0.35, 1);
+
+					if (!mReverse && (!mLoc.isChunkLoaded() || LocationUtils.collidesWithSolid(mLoc, mLoc.getBlock()) || mTicks >= PANACEA_MAX_DURATION)) {
+						mMobs = EntityUtils.getNearbyMobs(mLoc, (0.3 + PANACEA_MOVE_SPEED) * PANACEA_MAX_DURATION + 2, mPlayer);
+						mPlayers = PlayerUtils.otherPlayersInRange(mPlayer, (0.3 + PANACEA_MOVE_SPEED) * PANACEA_MAX_DURATION + 2, true);
+						mReverse = true;
+						mReverseTick = mTicks;
+					}
+
+					if (mReverse) {
+						if (mTicks <= 0) {
+							world.playSound(mLoc, Sound.BLOCK_ENCHANTMENT_TABLE_USE, 1.2f, 2.4f);
+							world.spawnParticle(Particle.SPELL_INSTANT, mPlayer.getLocation().add(0, 1, 0), 8, 0.25, 0.5, 0.25, 0.5);
+							world.spawnParticle(Particle.SPELL, mPlayer.getLocation().add(0, 1, 0), 8, 0.35, 0.5, 0.35);
+							world.spawnParticle(Particle.REDSTONE, mPlayer.getLocation().add(0, 1, 0), 25, 0.35, 0.5, 0.35, APOTHECARY_LIGHT_COLOR);
+							this.cancel();
 						}
 
-						DamageEvent damageEvent = new DamageEvent(mob, mPlayer, mPlayer, DamageType.MAGIC, mInfo.mLinkedSpell, mDamage);
-						damageEvent.setDelayed(true);
-						damageEvent.setPlayerItemStat(playerItemStats);
-						DamageUtils.damage(damageEvent, false, true, null);
+						// The mIncrement is calculated by the distance to the player divided by the number of increments left
+						mIncrement = mPlayer.getEyeLocation().toVector().subtract(mLoc.toVector()).multiply(1.0 / mTicks);
 
-						EntityUtils.applySlow(mPlugin, mSlowTicks, 1, mob);
-						mobIter.remove();
-					}
-				}
-				Iterator<Player> playerIter = mPlayers.iterator();
-				while (playerIter.hasNext()) {
-					Player player = playerIter.next();
-					if (mBox.overlaps(player.getBoundingBox())) {
-						if (player != mPlayer) {
-							AbsorptionUtils.addAbsorption(player, mShield, PANACEA_MAX_SHIELD, PANACEA_ABSORPTION_DURATION);
+						// To make the particles function without rewriting the particle code, manually calculate and set pitch and yaw
+						double x = mIncrement.getX();
+						double y = mIncrement.getY();
+						double z = mIncrement.getZ();
+						// As long as Z is nonzero, we won't get division by 0
+						if (z == 0) {
+							z = 0.0001;
 						}
-						playerIter.remove();
+						float pitch = (float) Math.toDegrees(-Math.atan(y / Math.sqrt(Math.pow(x, 2) + Math.pow(z, 2))));
+						float yaw = (float) Math.toDegrees(Math.atan(-x / z));
+						if (z < 0) {
+							yaw += 180;
+						}
+						mLoc.setPitch(pitch);
+						mLoc.setYaw(yaw);
+
+						mTicks--;
+					} else {
+						mTicks++;
 					}
 				}
 
-				mDegree += 12;
-				Vector vec;
-				for (int i = 0; i < 2; i++) {
-					double radian1 = Math.toRadians(mDegree + (i * 180));
-					vec = new Vector(FastUtils.cos(radian1) * 0.325, 0, FastUtils.sin(radian1) * 0.325);
-					vec = VectorUtils.rotateXAxis(vec, mLoc.getPitch() - 90);
-					vec = VectorUtils.rotateYAxis(vec, mLoc.getYaw());
-
-					Location l = mLoc.clone().add(vec);
-					world.spawnParticle(Particle.REDSTONE, l, 5, 0.1, 0.1, 0.1, APOTHECARY_LIGHT_COLOR);
-					world.spawnParticle(Particle.REDSTONE, l, 5, 0.1, 0.1, 0.1, APOTHECARY_DARK_COLOR);
-				}
-				world.spawnParticle(Particle.SPELL_INSTANT, mLoc, 5, 0.35, 0.35, 0.35, 1);
-				world.spawnParticle(Particle.SPELL_WITCH, mLoc, 5, 0.35, 0.35, 0.35, 1);
-
-				if (!mReverse && (!mLoc.isChunkLoaded() || LocationUtils.collidesWithSolid(mLoc, mLoc.getBlock()) || mTicks >= PANACEA_MAX_DURATION)) {
-					mMobs = EntityUtils.getNearbyMobs(mLoc, (0.3 + PANACEA_MOVE_SPEED) * PANACEA_MAX_DURATION + 2, mPlayer);
-					mPlayers = PlayerUtils.otherPlayersInRange(mPlayer, (0.3 + PANACEA_MOVE_SPEED) * PANACEA_MAX_DURATION + 2, true);
-					mReverse = true;
-					mReverseTick = mTicks;
-				}
-
-				if (mReverse) {
-					if (mTicks <= 0) {
-						world.playSound(mLoc, Sound.BLOCK_ENCHANTMENT_TABLE_USE, 1.2f, 2.4f);
-						world.spawnParticle(Particle.SPELL_INSTANT, mPlayer.getLocation().add(0, 1, 0), 8, 0.25, 0.5, 0.25, 0.5);
-						world.spawnParticle(Particle.SPELL, mPlayer.getLocation().add(0, 1, 0), 8, 0.35, 0.5, 0.35);
-						world.spawnParticle(Particle.REDSTONE, mPlayer.getLocation().add(0, 1, 0), 25, 0.35, 0.5, 0.35, APOTHECARY_LIGHT_COLOR);
-						this.cancel();
-					}
-
-					// The mIncrement is calculated by the distance to the player divided by the number of increments left
-					mIncrement = mPlayer.getEyeLocation().toVector().subtract(mLoc.toVector()).multiply(1.0 / mTicks);
-
-					// To make the particles function without rewriting the particle code, manually calculate and set pitch and yaw
-					double x = mIncrement.getX();
-					double y = mIncrement.getY();
-					double z = mIncrement.getZ();
-					// As long as Z is nonzero, we won't get division by 0
-					if (z == 0) {
-						z = 0.0001;
-					}
-					float pitch = (float) Math.toDegrees(-Math.atan(y / Math.sqrt(Math.pow(x, 2) + Math.pow(z, 2))));
-					float yaw = (float) Math.toDegrees(Math.atan(-x / z));
-					if (z < 0) {
-						yaw += 180;
-					}
-					mLoc.setPitch(pitch);
-					mLoc.setYaw(yaw);
-
-					mTicks--;
-				} else {
-					mTicks++;
-				}
-			}
-
-		}.runTaskTimer(mPlugin, 0, 1);
+			}.runTaskTimer(mPlugin, 0, 1);
+		}
 	}
 
 	@Override
