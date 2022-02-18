608b52192f4406be59bc7cb72890082c19ea74bb
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2022-02-17 17:22:51 -0800

    fix hunting companion not working in rod, metalmancy against some depths mobs, /k not working vs rod mobs (#1340)
    Signed-off-by: NobodyPi <officialnobodypi@gmail.com>

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/PlayerDamageOnlyBoss.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/PlayerDamageOnlyBoss.java
@@ -1,17 +1,19 @@
 package com.playmonumenta.plugins.bosses.bosses;
 
-import java.util.Collections;
-
 import com.playmonumenta.plugins.bosses.SpellManager;
-
 import com.playmonumenta.plugins.events.DamageEvent;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import java.util.Collections;
+
 public class PlayerDamageOnlyBoss extends BossAbilityGroup {
 	public static final String identityTag = "boss_player_dmg_only";
 	public static final int detectionRange = 50;
+
+	//This tag is on mobs from summon abilities (Hunting Companion, Metalmancy, etc.)
+	public static final String bypassTag = "bypass_player_damage_only";
 
 	public static BossAbilityGroup deserialize(Plugin plugin, LivingEntity boss) throws Exception {
 		return new PlayerDamageOnlyBoss(plugin, boss);
@@ -24,7 +26,7 @@ public class PlayerDamageOnlyBoss extends BossAbilityGroup {
 
 	@Override
 	public void onHurt(DamageEvent event) {
-		if (!(event.getSource() instanceof Player)) {
+		if (event.getSource() != null && !(event.getSource() instanceof Player) && !event.getSource().getScoreboardTags().contains(bypassTag)) {
 			event.setCancelled(true);
 		}
 	}
