edcda0fd6568ea090b9b81a3e979c8180baf612f
Timothy Southwick <NickNackGus@gmail.com>
2022-02-18 03:43:01 -0500

    /getscore uses ScoreHolderArgument now (#1361)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/commands/GetScoreCommand.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/commands/GetScoreCommand.java
@@ -6,7 +6,8 @@ import dev.jorel.commandapi.CommandPermission;
 import dev.jorel.commandapi.arguments.EntitySelectorArgument;
 import dev.jorel.commandapi.arguments.EntitySelectorArgument.EntitySelector;
 import dev.jorel.commandapi.arguments.ObjectiveArgument;
-import dev.jorel.commandapi.arguments.TextArgument;
+import dev.jorel.commandapi.arguments.ScoreHolderArgument;
+import dev.jorel.commandapi.arguments.ScoreHolderArgument.ScoreHolderType;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
@@ -25,40 +26,23 @@ public class GetScoreCommand {
 	public static void register(Plugin plugin) {
 		new CommandAPICommand("getscore")
 		.withPermission(CommandPermission.fromString("monumenta.command.getscore"))
-		.withArguments(new TextArgument("name"))
+		.withArguments(new ScoreHolderArgument("name", ScoreHolderType.MULTIPLE))
 		.withArguments(new ObjectiveArgument("objective"))
 		.executes((sender, args) -> {
-			run(sender, (String)args[0], (String)args[1]);
-		})
-		.register();
-		new CommandAPICommand("getscore")
-		.withPermission(CommandPermission.fromString("monumenta.command.getscore"))
-		.withArguments(new EntitySelectorArgument("targets", EntitySelector.MANY_ENTITIES))
-		.withArguments(new ObjectiveArgument("objective"))
-		.executes((sender, args) -> {
-			for (Entity entity : (Collection<Entity>)args[0]) {
-				if (entity instanceof Player) {
-					run(sender, entity.getName(), (String)args[1]);
-				} else {
-					run(sender, entity.getUniqueId().toString(), (String)args[1]);
-				}
+			String objective = (String)args[1];
+			for (String scoreHolder : (Collection<String>)args[0]) {
+				run(sender, scoreHolder, objective);
 			}
 		})
 		.register();
 	}
 
 	private static void run(CommandSender sender, String name, String objectiveName) {
-
-		Optional<Integer> scoreValue = Optional.empty();
-		Objective objective = Bukkit.getScoreboardManager().getMainScoreboard().getObjective(objectiveName);
-		if (objective != null) {
-			Score score = objective.getScore(name);
-			if (score != null && score.isScoreSet()) {
-				scoreValue = ScoreboardUtils.getScoreboardValue(name, objectiveName);
-				sender.sendMessage(ChatColor.AQUA + "Score for " + ChatColor.GOLD + name + ChatColor.AQUA + " in " + ChatColor.GOLD + objectiveName + ChatColor.AQUA + ": " + ChatColor.GOLD + scoreValue.get());
-			} else {
-				sender.sendMessage(ChatColor.AQUA + "Score not set for " + ChatColor.GOLD + name + ChatColor.AQUA + " in " + ChatColor.GOLD + objectiveName);
-			}
+		Optional<Integer> scoreValue = ScoreboardUtils.getScoreboardValue(name, objectiveName);
+		if (scoreValue.isPresent()) {
+			sender.sendMessage(ChatColor.AQUA + "Score for " + ChatColor.GOLD + name + ChatColor.AQUA + " in " + ChatColor.GOLD + objectiveName + ChatColor.AQUA + ": " + ChatColor.GOLD + scoreValue.get());
+		} else {
+			sender.sendMessage(ChatColor.AQUA + "Score for " + ChatColor.GOLD + name + ChatColor.AQUA + " in " + ChatColor.GOLD + objectiveName + ChatColor.AQUA + ": " + ChatColor.GOLD + "not set");
 		}
 	}
 
