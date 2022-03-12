e67a0f74c1d8009c44d40fc2f82edec529ebff32
Fabio Bocci <74119740+FabioBocci@users.noreply.github.com>
2022-03-12 06:49:17 +0100

    fix wrong capitalization in infernal (#1464)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/delves/Infernal.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/delves/Infernal.java
@@ -87,14 +87,14 @@ public class Infernal extends DelveModifier {
 		List<String> flameNovaBoss = new ArrayList<>();
 		flameNovaBoss.add(NovaBoss.identityTag);
 		flameNovaBoss.add(NovaBoss.identityTag + "[damage=17,duration=70,detection=20,effects=[(fire,80)],spellname=\"" + NOVA_SPELL_NAME + "\"]");
-		flameNovaBoss.add(NovaBoss.identityTag + "[soundcharge=BLOCK_FIRE_AMBIENT,soundcast=[(ENTITY_WITHER_SHOOT,1.5,0.65)]");
-		flameNovaBoss.add(NovaBoss.identityTag + "[particleair=[(lava,2,4.5,4.5,4.5,0.05)],particleload=[(flame,1,0.25,0.25,0.25,0.1)],particleexplode=[(flame,1,0.1,0.1,0.1,0.3),(smoke_normal,2,0.25,0.25,0.25,0.1)]]");
+		flameNovaBoss.add(NovaBoss.identityTag + "[soundcharge=BLOCK_FIRE_AMBIENT,soundcast=[(ENTITY_WITHER_SHOOT,1.5,0.65)]]");
+		flameNovaBoss.add(NovaBoss.identityTag + "[particleair=[(LAVA,2,4.5,4.5,4.5,0.05)],particleload=[(FLAME,1,0.25,0.25,0.25,0.1)],particleexplode=[(FLAME,1,0.1,0.1,0.1,0.3),(SMOKE_NORMAL,2,0.25,0.25,0.25,0.1)]]");
 		ABILITY_POOL_R2.add(flameNovaBoss);
 		flameNovaBoss = new ArrayList<>();
 		flameNovaBoss.add(NovaBoss.identityTag);
 		flameNovaBoss.add(NovaBoss.identityTag + "[damage=9,duration=70,detection=20,effects=[(fire,60)],spellname=\"" + NOVA_SPELL_NAME + "\"]");
-		flameNovaBoss.add(NovaBoss.identityTag + "[soundcharge=BLOCK_FIRE_AMBIENT,soundcast=[(ENTITY_WITHER_SHOOT,1.5,0.65)]");
-		flameNovaBoss.add(NovaBoss.identityTag + "[particleair=[(lava,2,4.5,4.5,4.5,0.05)],particleload=[(flame,1,0.25,0.25,0.25,0.1)],particleexplode=[(flame,1,0.1,0.1,0.1,0.3),(smoke_normal,2,0.25,0.25,0.25,0.1)]]");
+		flameNovaBoss.add(NovaBoss.identityTag + "[soundcharge=BLOCK_FIRE_AMBIENT,soundcast=[(ENTITY_WITHER_SHOOT,1.5,0.65)]]");
+		flameNovaBoss.add(NovaBoss.identityTag + "[particleair=[(LAVA,2,4.5,4.5,4.5,0.05)],particleload=[(FLAME,1,0.25,0.25,0.25,0.1)],particleexplode=[(FLAME,1,0.1,0.1,0.1,0.3),(SMOKE_NORMAL,2,0.25,0.25,0.25,0.1)]]");
 		ABILITY_POOL_R1.add(flameNovaBoss);
 
 
