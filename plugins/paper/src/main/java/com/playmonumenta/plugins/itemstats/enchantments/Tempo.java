a12e7eeb02dca23d1bddda84f230112a4026c429
Luke Silvestri <40128099+silvestriluke@users.noreply.github.com>
2022-03-17 20:07:59 -0400

    Update Tempo.java (#1494)
    * Update Tempo.java
    
    * Updated Tempo description in /ps GUI
    
    Co-authored-by: Peter Güttinger <njol@gmx.ch>

--- plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/enchantments/Tempo.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/enchantments/Tempo.java
@@ -14,8 +14,10 @@ import org.bukkit.entity.Player;
 
 public class Tempo implements Enchantment {
 
-	private static final double AGIL_BONUS_PER_LEVEL = 0.2;
+	private static final double AGIL_BONUS_PER_LEVEL_1 = 0.1;
+	private static final double AGIL_BONUS_PER_LEVEL_2 = 0.2;
 	private static final int PAST_HIT_DURATION_TIME = 20 * 4;
+	private static final int PAST_HIT_DURATION_TIME_HALF = 20 * 2;
 	private static final String TEMPO_EFFECT_NAME = "TempoEffect";
 
 	@Override
@@ -40,9 +42,14 @@ public class Tempo implements Enchantment {
 	public static double applyTempo(DamageEvent event, Plugin plugin, Player player) {
 		NavigableSet<Effect> tempo = plugin.mEffectManager.getEffects(player, TEMPO_EFFECT_NAME);
 		if (tempo == null) {
-			return plugin.mItemStatManager.getEnchantmentLevel(player, EnchantmentType.TEMPO) * AGIL_BONUS_PER_LEVEL;
+			return plugin.mItemStatManager.getEnchantmentLevel(player, EnchantmentType.TEMPO) * AGIL_BONUS_PER_LEVEL_2;
 		} else {
-			return 0;
+			Effect temp = tempo.last();
+			if (temp.getDuration() <= PAST_HIT_DURATION_TIME_HALF) {
+				return plugin.mItemStatManager.getEnchantmentLevel(player, EnchantmentType.TEMPO) * AGIL_BONUS_PER_LEVEL_1;
+			} else {
+				return 0;
+			}
 		}
 	}
 
