c3bba3554b097e6b9cd02ad80aeaa238f6959687
NobodyPi <officialnobodypi@gmail.com>
2022-04-06 11:56:19 -0700

    Weapon Mastery
    Signed-off-by: NobodyPi <officialnobodypi@gmail.com>

--- plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/warrior/WeaponMastery.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/warrior/WeaponMastery.java
@@ -2,8 +2,10 @@ package com.playmonumenta.plugins.abilities.warrior;
 
 import com.playmonumenta.plugins.Plugin;
 import com.playmonumenta.plugins.abilities.Ability;
+import com.playmonumenta.plugins.effects.PercentAttackSpeed;
 import com.playmonumenta.plugins.events.DamageEvent;
 import com.playmonumenta.plugins.events.DamageEvent.DamageType;
+import com.playmonumenta.plugins.utils.EntityUtils;
 import com.playmonumenta.plugins.utils.ItemUtils;
 import javax.annotation.Nullable;
 import org.bukkit.Material;
@@ -21,7 +23,12 @@ public class WeaponMastery extends Ability {
 	private static final double AXE_1_DAMAGE = 0.05;
 	private static final double AXE_2_DAMAGE = 0.1;
 	private static final double SWORD_2_DAMAGE = 0.1;
+	private static final double SWORD_ENHANCED_DAMAGE = 0.1;
 	private static final double WEAPON_MASTERY_SWORD_DAMAGE_RESISTANCE = 0.1;
+	private static final double SWORD_WEAKEN = 0.1;
+	private static final int SWORD_WEAKEN_DURATION = 4 * 20;
+	private static final double AXE_ATTACK_SPEED = 0.15;
+	private static final String ATTACK_SPEED_EFFECT = "WeaponMasteryAttackSpeedEffect";
 
 	private final double mDamageBonusAxeFlat;
 	private final double mDamageBonusSwordFlat;
@@ -38,7 +45,7 @@ public class WeaponMastery extends Ability {
 		mDamageBonusAxeFlat = isLevelOne() ? AXE_1_DAMAGE_FLAT : AXE_2_DAMAGE_FLAT;
 		mDamageBonusSwordFlat = isLevelOne() ? 0 : SWORD_2_DAMAGE_FLAT;
 		mDamageBonusAxe = isLevelOne() ? AXE_1_DAMAGE : AXE_2_DAMAGE;
-		mDamageBonusSword = isLevelOne() ? 0 : SWORD_2_DAMAGE;
+		mDamageBonusSword = (isLevelOne() ? 0 : SWORD_2_DAMAGE) + (isEnhanced() ? SWORD_ENHANCED_DAMAGE : 0);
 	}
 
 	@Override
@@ -49,9 +56,19 @@ public class WeaponMastery extends Ability {
 				event.setDamage((event.getDamage() + mDamageBonusAxeFlat) * (1 + mDamageBonusAxe));
 			} else if (ItemUtils.isSword(mainHand)) {
 				event.setDamage((event.getDamage() + mDamageBonusSwordFlat) * (1 + mDamageBonusSword));
+				if (isEnhanced()) {
+					EntityUtils.applyWeaken(mPlugin, SWORD_WEAKEN_DURATION, SWORD_WEAKEN, enemy);
+				}
 			}
 		}
 		return false; // only changes event damage
+	}
+
+	@Override
+	public void periodicTrigger(boolean twoHertz, boolean oneSecond, int ticks) {
+		if (isEnhanced() && mPlayer != null && ItemUtils.isAxe(mPlayer.getInventory().getItemInMainHand())) {
+			mPlugin.mEffectManager.addEffect(mPlayer, ATTACK_SPEED_EFFECT, new PercentAttackSpeed(6, AXE_ATTACK_SPEED, ATTACK_SPEED_EFFECT));
+		}
 	}
 
 	@Override
