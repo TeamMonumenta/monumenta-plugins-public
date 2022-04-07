6e69b9d92685258ae4780ac35442b13f133f500c
Fwap a Durp <derpderp0000@gmail.com>
2022-04-07 19:13:16 +0100

    rip pizza kaul (#1601)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/kaul/SpellRaiseJungle.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/kaul/SpellRaiseJungle.java
@@ -106,7 +106,6 @@ public class SpellRaiseJungle extends Spell {
 					}
 					Location spawn = sLoc.clone().subtract(0, 1.75, 0); // should end up 1.5 blocks below the arena floor
 					LivingEntity ele = (LivingEntity)LibraryOfSoulsIntegration.summon(spawn, "EarthElemental");
-					ele.setCustomName("Mushroom Elemental");
 					Location scLoc = sLoc.clone();
 					if (ele != null && !mSummoned.contains(ele.getUniqueId())) {
 						mSummoned.add(ele.getUniqueId());
