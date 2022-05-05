4842a098ea0e43bed83bba8c7fd24a35bfbfa944
Fabio Bocci <74119740+FabioBocci@users.noreply.github.com>
2022-05-06 01:19:35 +0200

    Artillery Enhancement (#1701)
    * Artillery Enhancement
    
    * removed comments
    
    * add description

--- plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/alchemist/AlchemicalArtillery.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/alchemist/AlchemicalArtillery.java
@@ -5,21 +5,32 @@ import com.playmonumenta.plugins.abilities.Ability;
 import com.playmonumenta.plugins.abilities.AbilityManager;
 import com.playmonumenta.plugins.abilities.alchemist.harbinger.Taboo;
 import com.playmonumenta.plugins.classes.ClassAbility;
+import com.playmonumenta.plugins.events.DamageEvent;
+import com.playmonumenta.plugins.particle.PartialParticle;
+import com.playmonumenta.plugins.utils.DamageUtils;
+import com.playmonumenta.plugins.utils.EntityUtils;
 import com.playmonumenta.plugins.utils.ItemStatUtils;
 import com.playmonumenta.plugins.utils.ItemStatUtils.AttributeType;
 import com.playmonumenta.plugins.utils.ItemStatUtils.Operation;
 import com.playmonumenta.plugins.utils.ItemStatUtils.Slot;
 import com.playmonumenta.plugins.utils.ItemUtils;
+import com.playmonumenta.plugins.utils.MovementUtils;
 import com.playmonumenta.plugins.utils.ScoreboardUtils;
+import java.util.Collection;
+import java.util.List;
 import javax.annotation.Nullable;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.AbstractArrow;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.ThrownPotion;
 import org.bukkit.entity.Trident;
+import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -31,6 +42,12 @@ public class AlchemicalArtillery extends Ability {
 
 	private static final double BOW_DAMAGE_MULTIPLIER = 0.25;
 
+	private static final int ENHANCEMENT_EXPLOSION_DELAY = 20;
+	private static final int ENHANCEMENT_EXPLOSION_RADIUS = 3;
+	private static final float ENHANCEMENT_EXPLOSION_KNOCK_UP = 1.5f;
+	private static final double ENHANCEMENT_EXPLOSION_POT_PERCENT_DAMAGE = 0.15;
+
+
 	private boolean mActive;
 	private @Nullable AlchemistPotions mAlchemistPotions;
 	private boolean mHasTaboo;
@@ -41,6 +58,7 @@ public class AlchemicalArtillery extends Ability {
 		mInfo.mShorthandName = "AA";
 		mInfo.mDescriptions.add("Swap hands while holding a bow, crossbow, or trident to toggle shooting Alchemist's Potions instead of projectiles. Shooting a potion consumes the potion and applies the damage and any effects that potion would normally apply.");
 		mInfo.mDescriptions.add("Potions shot with this ability have 25% of your projectile damage added to their base damage.");
+		mInfo.mDescriptions.add("Wherever your Artillery lands, an explosion will generate after 1s dealing 15% of the original damage and knocking up enemies.");
 		mInfo.mLinkedSpell = ClassAbility.ALCHEMICAL_ARTILLERY;
 		mDisplayItem = new ItemStack(Material.CROSSBOW, 1);
 
@@ -105,6 +123,25 @@ public class AlchemicalArtillery extends Ability {
 			mPlayer.sendActionBar(ChatColor.YELLOW + "Alchemical Artillery has been " + active + "!");
 			mPlayer.playSound(mPlayer.getLocation(), Sound.BLOCK_BREWING_STAND_BREW, 0.9f, 1.2f);
 		}
+	}
+
+	@Override
+	public boolean playerSplashPotionEvent(Collection<LivingEntity> affectedEntities, ThrownPotion potion, PotionSplashEvent event) {
+		if (isEnhanced() && potion.hasMetadata(ARTILLERY_POTION_TAG)) {
+			Bukkit.getScheduler().runTaskLater(mPlugin, () -> {
+				Location loc = potion.getLocation();
+
+				new PartialParticle(Particle.EXPLOSION_HUGE, loc, 15, ENHANCEMENT_EXPLOSION_RADIUS, ENHANCEMENT_EXPLOSION_RADIUS / 2.0, ENHANCEMENT_EXPLOSION_RADIUS, 0.1).spawnAsPlayerActive(mPlayer);
+				List<LivingEntity> mobs = EntityUtils.getNearbyMobs(loc, ENHANCEMENT_EXPLOSION_RADIUS);
+				double damage = mAlchemistPotions.getDamage() * ENHANCEMENT_EXPLOSION_POT_PERCENT_DAMAGE;
+				for (LivingEntity mob : mobs) {
+					DamageUtils.damage(mPlayer, mob, DamageEvent.DamageType.MAGIC, damage, mInfo.mLinkedSpell, true, false);
+					MovementUtils.knockAway(loc, mob, ENHANCEMENT_EXPLOSION_KNOCK_UP);
+				}
+
+			}, ENHANCEMENT_EXPLOSION_DELAY);
+		}
+		return true;
 	}
 
 	public boolean isActive() {
