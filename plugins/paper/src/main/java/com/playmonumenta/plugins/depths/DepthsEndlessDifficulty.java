de9fdcb59d80bc4f11b71028db9302ba22210f84
Fabio Bocci <74119740+FabioBocci@users.noreply.github.com>
2022-06-03 13:20:26 +0200

    fixed DepthsEndlessDifficulty not giving point (#1818)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/depths/DepthsEndlessDifficulty.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/depths/DepthsEndlessDifficulty.java
@@ -2,6 +2,7 @@ package com.playmonumenta.plugins.depths;
 
 import com.playmonumenta.plugins.delves.DelvesModifier;
 import com.playmonumenta.plugins.delves.DelvesUtils;
+import com.playmonumenta.plugins.server.properties.ServerProperties;
 import java.util.List;
 import java.util.Map;
 import org.bukkit.Bukkit;
@@ -30,26 +31,27 @@ public class DepthsEndlessDifficulty {
 			return;
 		}
 
+		String shard = ServerProperties.getShardName();
 		//Get delve info for that player
 		if (delvePointsForParty != null && delvePointsForParty.size() > 0) {
 
 			for (DelvesModifier m : DelvesModifier.values()) {
-				DelvesUtils.setDelvePoint(null, playerToUse, "depths", m, delvePointsForParty.getOrDefault(m, 0));
+				DelvesUtils.setDelvePoint(null, playerToUse, shard, m, delvePointsForParty.getOrDefault(m, 0));
 			}
 		}
 
 		//Assign twisted and entropy if they are on the last floor
 		if (twisted) {
-			DelvesUtils.setDelvePoint(null, playerToUse, "depths", DelvesModifier.ENTROPY, 5);
-			DelvesUtils.setDelvePoint(null, playerToUse, "depths", DelvesModifier.TWISTED, 1);
+			DelvesUtils.setDelvePoint(null, playerToUse, shard, DelvesModifier.ENTROPY, 5);
+			DelvesUtils.setDelvePoint(null, playerToUse, shard, DelvesModifier.TWISTED, 1);
 		} else {
 			//Assign random points to that player on top of what they currently have
-			DelvesUtils.assignRandomDelvePoints(playerToUse, "depths", pointsToAssign);
+			DelvesUtils.assignRandomDelvePoints(playerToUse, shard, pointsToAssign);
 		}
 
 		//Store player's modifiers in the party index
 		for (DelvesModifier m : DelvesModifier.values()) {
-			delvePointsForParty.put(m, DelvesUtils.getDelveModLevel(playerToUse, "depths", m));
+			delvePointsForParty.put(m, DelvesUtils.getDelveModLevel(playerToUse, shard, m));
 		}
 
 		//Assign the scores to all other active players
@@ -63,7 +65,7 @@ public class DepthsEndlessDifficulty {
 				p.sendMessage(DepthsUtils.DEPTHS_MESSAGE_PREFIX + "Assigning your party " + pointsToAssign + " delve points randomly. Sneak left click while holding your Depths Trinket to view all delve modifiers.");
 			}
 
-			DelvesUtils.copyDelvePoint(null, p, playerToUse, "depths");
+			DelvesUtils.copyDelvePoint(null, playerToUse, p, shard);
 		}
 
 	}
