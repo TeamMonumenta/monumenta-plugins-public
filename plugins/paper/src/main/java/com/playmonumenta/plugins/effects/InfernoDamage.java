77ac4f70767e9d4c3ae442527dccc39862808c33
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2022-06-16 08:44:11 -0700

    Make Inferno deal full damage to fire immune mobs (#1861)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/effects/InfernoDamage.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/effects/InfernoDamage.java
@@ -3,7 +3,6 @@ package com.playmonumenta.plugins.effects;
 import com.playmonumenta.plugins.events.DamageEvent.DamageType;
 import com.playmonumenta.plugins.particle.PartialParticle;
 import com.playmonumenta.plugins.utils.DamageUtils;
-import com.playmonumenta.plugins.utils.EntityUtils;
 import org.bukkit.Particle;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -29,9 +28,6 @@ public class InfernoDamage extends Effect {
 	public void entityTickEffect(Entity entity, boolean fourHertz, boolean twoHertz, boolean oneHertz) {
 		if (oneHertz && entity instanceof LivingEntity le) {
 			double damage = mLevel;
-			if (le.getFireTicks() <= 0 || EntityUtils.isFireResistant(le)) {
-				damage *= 0.5;
-			}
 			DamageUtils.damage(mPlayer, le, DamageType.AILMENT, damage, null, true, false);
 			new PartialParticle(Particle.FLAME, le.getLocation().add(0, 1, 0), 11, 0.4, 0.4, 0.4, 0.05).spawnAsEnemyBuff();
 		}
