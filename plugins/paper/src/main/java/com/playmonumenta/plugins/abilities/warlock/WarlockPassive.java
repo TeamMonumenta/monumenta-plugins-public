81dde50168e7215d35f7864eecdc0ab139e5792a
Asinius <44119581+Asinius@users.noreply.github.com>
2022-08-12 01:03:48 -0400

    Changed warlock passive to percentage based resistance rather than vanilla (#1996)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/warlock/WarlockPassive.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/warlock/WarlockPassive.java
@@ -2,6 +2,7 @@ package com.playmonumenta.plugins.abilities.warlock;
 
 import com.playmonumenta.plugins.Plugin;
 import com.playmonumenta.plugins.abilities.Ability;
+import com.playmonumenta.plugins.effects.PercentDamageReceived;
 import com.playmonumenta.plugins.potion.PotionManager.PotionID;
 import com.playmonumenta.plugins.utils.EntityUtils;
 import com.playmonumenta.plugins.utils.ItemUtils;
@@ -16,6 +17,8 @@ import org.bukkit.potion.PotionEffectType;
 public class WarlockPassive extends Ability {
 
 	private static final int PASSIVE_DURATION = 6 * 20;
+	private static final String WARLOCK_PASSIVE_EFFECT_NAME = "CullingPercentDamageResistEffect";
+	private static final double WARLOCK_PASSIVE_DAMAGE_REDUCTION_PERCENT = -0.1;
 
 	public WarlockPassive(Plugin plugin, @Nullable Player player) {
 		super(plugin, player, null);
@@ -30,8 +33,7 @@ public class WarlockPassive extends Ability {
 	public void entityDeathEvent(EntityDeathEvent event, boolean shouldGenDrops) {
 		if (EntityUtils.isHostileMob(event.getEntity())
 				&& ItemUtils.isHoe(mPlayer.getInventory().getItemInMainHand())) {
-			mPlugin.mPotionManager.addPotion(mPlayer, PotionID.ABILITY_SELF,
-					new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, PASSIVE_DURATION, 0, false, true));
+			mPlugin.mEffectManager.addEffect(mPlayer, WARLOCK_PASSIVE_EFFECT_NAME, new PercentDamageReceived(PASSIVE_DURATION, WARLOCK_PASSIVE_DAMAGE_REDUCTION_PERCENT));
 		}
 	}
 }
