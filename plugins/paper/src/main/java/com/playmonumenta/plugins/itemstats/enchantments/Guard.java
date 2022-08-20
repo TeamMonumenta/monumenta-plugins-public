d1c5f01ae2a30f4ab8e5dbcaf3364e9d0bb1ae75
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2022-08-19 22:30:51 -0700

    Guard situational enchant (#1965)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/enchantments/Guard.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/enchantments/Guard.java
@@ -1,12 +1,23 @@
 package com.playmonumenta.plugins.itemstats.enchantments;
 
 import com.playmonumenta.plugins.Plugin;
+import com.playmonumenta.plugins.effects.Effect;
+import com.playmonumenta.plugins.effects.OnHitTimerEffect;
 import com.playmonumenta.plugins.events.DamageEvent;
 import com.playmonumenta.plugins.itemstats.Enchantment;
 import com.playmonumenta.plugins.utils.ItemStatUtils.EnchantmentType;
+import java.util.NavigableSet;
+import javax.annotation.Nullable;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 public class Guard implements Enchantment {
+	private static final double ARMOR_BONUS_PER_LEVEL = 0.2;
+	private static final int PAST_HIT_DURATION_TIME_MAINHAND = 5 * 20;
+	private static final int PAST_HIT_DURATION_TIME_OFFHAND = (int) (2.5 * 20);
+	private static final String GUARD_EFFECT_NAME = "GuardEffect";
 
 	@Override
 	public String getName() {
@@ -18,8 +29,22 @@ public class Guard implements Enchantment {
 		return EnchantmentType.GUARD;
 	}
 
+	@Override
+	public void onHurt(Plugin plugin, Player player, double value, DamageEvent event, @Nullable Entity damager, @Nullable LivingEntity source) {
+		if (!event.isBlockedByShield()) {
+			return;
+		}
+
+		plugin.mEffectManager.addEffect(player, GUARD_EFFECT_NAME, new OnHitTimerEffect(player.getInventory().getItemInMainHand().getType() == Material.SHIELD ? PAST_HIT_DURATION_TIME_MAINHAND : PAST_HIT_DURATION_TIME_OFFHAND));
+	}
+
 	public static double applyGuard(DamageEvent event, Plugin plugin, Player player) {
-		return 0;
+		NavigableSet<Effect> guard = plugin.mEffectManager.getEffects(player, GUARD_EFFECT_NAME);
+		if (guard != null) {
+			return plugin.mItemStatManager.getEnchantmentLevel(player, EnchantmentType.GUARD) * ARMOR_BONUS_PER_LEVEL;
+		} else {
+			return 0;
+		}
 	}
 
 }
