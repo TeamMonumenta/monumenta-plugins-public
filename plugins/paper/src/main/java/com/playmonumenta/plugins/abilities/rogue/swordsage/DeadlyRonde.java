cc3403c91b42485d3d997d413a8671f53f617dd3
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2022-09-09 10:25:15 -0700

    deadly ronde change + new charm effects (#2057)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/rogue/swordsage/DeadlyRonde.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/rogue/swordsage/DeadlyRonde.java
@@ -13,6 +13,7 @@ import com.playmonumenta.plugins.network.ClientModHandler;
 import com.playmonumenta.plugins.particle.PartialParticle;
 import com.playmonumenta.plugins.utils.DamageUtils;
 import com.playmonumenta.plugins.utils.EntityUtils;
+import com.playmonumenta.plugins.utils.FastUtils;
 import com.playmonumenta.plugins.utils.InventoryUtils;
 import com.playmonumenta.plugins.utils.MessagingUtils;
 import com.playmonumenta.plugins.utils.MovementUtils;
@@ -29,34 +30,32 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
-/*
- * Deadly Ronde: After using a skill, your next sword
- * attack deals 4 / 6 extra damage, also adding half of
- * that bonus to sweeping attacks. At lvl 2, the sweep
- * attack takes the full bonus and all attacks also
- * staggers the single mob you melee hit, afflicting
- * it with Slowness II for 4 s.
- */
-
 public class DeadlyRonde extends Ability implements AbilityWithChargesOrStacks {
 
-	private static final int RONDE_1_DAMAGE = 3;
-	private static final int RONDE_2_DAMAGE = 5;
+	private static final int RONDE_1_DAMAGE = 5;
+	private static final int RONDE_2_DAMAGE = 7;
 	private static final int RONDE_1_MAX_STACKS = 2;
 	private static final int RONDE_2_MAX_STACKS = 3;
 	private static final double RONDE_SPEED_BONUS = 0.2;
 	private static final int RONDE_DECAY_TIMER = 5 * 20;
 	private static final double RONDE_RADIUS = 4.5;
-	private static final double RONDE_DOT_COSINE = 0.33;
+	private static final double RONDE_ANGLE = 35;
 	private static final float RONDE_KNOCKBACK_SPEED = 0.14f;
 
 	public static final String CHARM_DAMAGE = "Deadly Ronde Damage";
 	public static final String CHARM_RADIUS = "Deadly Ronde Radius";
+	public static final String CHARM_ANGLE = "Deadly Ronde Angle";
+	public static final String CHARM_KNOCKBACK = "Deadly Ronde Knockback";
+	public static final String CHARM_STACKS = "Deadly Ronde Max Stacks";
 
 	private static final Particle.DustOptions SWORDSAGE_COLOR = new Particle.DustOptions(Color.fromRGB(150, 0, 0), 1.0f);
 
 	private @Nullable BukkitRunnable mActiveRunnable = null;
 	private int mRondeStacks = 0;
+
+	private final double mDamage;
+	private final float mKnockback;
+	private final int mMaxStacks;
 
 	public DeadlyRonde(Plugin plugin, @Nullable Player player) {
 		super(plugin, player, "Deadly Ronde");
@@ -64,7 +63,7 @@ public class DeadlyRonde extends Ability implements AbilityWithChargesOrStacks {
 		mInfo.mScoreboardId = "DeadlyRonde";
 		mInfo.mShorthandName = "DR";
 		mInfo.mDescriptions.add(
-			String.format("After casting a skill, gain a stack of Deadly Ronde for %s seconds, stacking up to %s times. While Deadly Ronde is active, you gain %s%% Speed, and your next melee attack consumes a stack to fire a flurry of blades, that fire in a cone with a radius of %s blocks and deal %s melee damage to all enemies they hit.",
+			String.format("After casting a skill, gain a stack of Deadly Ronde for %s seconds, stacking up to %s times. While Deadly Ronde is active, you gain %s%% Speed, and your next melee attack consumes a stack to fire a flurry of blades, that fire in a thin cone with a radius of %s blocks and deal %s melee damage to all enemies they hit.",
 				RONDE_DECAY_TIMER / 20,
 				RONDE_1_MAX_STACKS,
 				(int)(RONDE_SPEED_BONUS * 100),
@@ -72,10 +71,14 @@ public class DeadlyRonde extends Ability implements AbilityWithChargesOrStacks {
 				RONDE_1_DAMAGE
 				));
 		mInfo.mDescriptions.add(
-			String.format("Damage increased to %s, and you can now store up to %s charges.",
+			String.format("Damage increased to %s, and you can now store up to %s stacks.",
 				RONDE_2_DAMAGE,
 				RONDE_2_MAX_STACKS));
 		mDisplayItem = new ItemStack(Material.BLAZE_ROD, 1);
+
+		mDamage = CharmManager.calculateFlatAndPercentValue(mPlayer, CHARM_DAMAGE, isLevelOne() ? RONDE_1_DAMAGE : RONDE_2_DAMAGE);
+		mKnockback = (float) CharmManager.calculateFlatAndPercentValue(mPlayer, CHARM_KNOCKBACK, RONDE_KNOCKBACK_SPEED);
+		mMaxStacks = (int) ((isLevelOne() ? RONDE_1_MAX_STACKS : RONDE_2_MAX_STACKS) + CharmManager.getLevel(mPlayer, CHARM_STACKS));
 	}
 
 	@Override
@@ -111,8 +114,7 @@ public class DeadlyRonde extends Ability implements AbilityWithChargesOrStacks {
 		};
 		mActiveRunnable.runTaskLater(mPlugin, RONDE_DECAY_TIMER);
 
-		int maxStacks = getMaxCharges();
-		if (mRondeStacks < maxStacks) {
+		if (mRondeStacks < mMaxStacks) {
 			mPlayer.playSound(mPlayer.getLocation(), Sound.ENTITY_PUFFER_FISH_BLOW_OUT, 1, 1f);
 			mPlayer.playSound(mPlayer.getLocation(), Sound.ENTITY_SNOW_GOLEM_DEATH, 0.7f, 1.5f);
 			mRondeStacks++;
@@ -130,21 +132,24 @@ public class DeadlyRonde extends Ability implements AbilityWithChargesOrStacks {
 			    && event.getType() == DamageType.MELEE
 			    && InventoryUtils.rogueTriggerCheck(mPlugin, mPlayer)) {
 			Vector playerDirVector = mPlayer.getEyeLocation().getDirection().setY(0).normalize();
-			for (LivingEntity mob : EntityUtils.getNearbyMobs(mPlayer.getLocation(), CharmManager.getRadius(mPlayer, CHARM_RADIUS, RONDE_RADIUS))) {
+			double cos = FastUtils.cosDeg(CharmManager.calculateFlatAndPercentValue(mPlayer, CHARM_ANGLE, RONDE_ANGLE));
+			double radius = CharmManager.getRadius(mPlayer, CHARM_RADIUS, RONDE_RADIUS);
+			for (LivingEntity mob : EntityUtils.getNearbyMobs(mPlayer.getLocation(), radius)) {
 				Vector toMobVector = mob.getLocation().toVector().subtract(mPlayer.getLocation().toVector()).setY(0).normalize();
-				if (playerDirVector.dot(toMobVector) > RONDE_DOT_COSINE) {
-					double damage = CharmManager.calculateFlatAndPercentValue(mPlayer, CHARM_DAMAGE, isLevelOne() ? RONDE_1_DAMAGE : RONDE_2_DAMAGE);
-					DamageUtils.damage(mPlayer, mob, DamageType.MELEE_SKILL, damage, mInfo.mLinkedSpell, true);
-					MovementUtils.knockAway(mPlayer, mob, RONDE_KNOCKBACK_SPEED, true);
+				if (playerDirVector.dot(toMobVector) > cos) {
+					DamageUtils.damage(mPlayer, mob, DamageType.MELEE_SKILL, mDamage, mInfo.mLinkedSpell, true);
+					MovementUtils.knockAway(mPlayer, mob, mKnockback, true);
 				}
 			}
 
 			Location particleLoc = mPlayer.getEyeLocation().add(mPlayer.getEyeLocation().getDirection().multiply(3));
 			World world = mPlayer.getWorld();
-			new PartialParticle(Particle.SWEEP_ATTACK, particleLoc, 10, 1.5, 0.5, 1.5).spawnAsPlayerActive(mPlayer);
-			new PartialParticle(Particle.CRIT, particleLoc, 50, 1.5, 0.5, 1.5, 0.2).spawnAsPlayerActive(mPlayer);
-			new PartialParticle(Particle.CLOUD, particleLoc, 20, 1.5, 0.5, 1.5, 0.3).spawnAsPlayerActive(mPlayer);
-			new PartialParticle(Particle.REDSTONE, particleLoc, 45, 1.5, 0.5, 1.5, SWORDSAGE_COLOR).spawnAsPlayerActive(mPlayer);
+			double multiplier = radius / RONDE_RADIUS;
+			double delta = 1.5 * multiplier;
+			new PartialParticle(Particle.SWEEP_ATTACK, particleLoc, (int) (10 * multiplier), delta, 0.5, delta).spawnAsPlayerActive(mPlayer);
+			new PartialParticle(Particle.CRIT, particleLoc, (int) (50 * multiplier), delta, 0.5, delta, 0.2).spawnAsPlayerActive(mPlayer);
+			new PartialParticle(Particle.CLOUD, particleLoc, (int) (20 * multiplier), delta, 0.5, delta, 0.3).spawnAsPlayerActive(mPlayer);
+			new PartialParticle(Particle.REDSTONE, particleLoc, (int) (45 * multiplier), delta, 0.5, delta, SWORDSAGE_COLOR).spawnAsPlayerActive(mPlayer);
 
 			world.playSound(particleLoc, Sound.ITEM_TRIDENT_THROW, 1, 1.25f);
 			world.playSound(particleLoc, Sound.ENTITY_PLAYER_ATTACK_CRIT, 0.8f, 0.75f);
@@ -183,7 +188,7 @@ public class DeadlyRonde extends Ability implements AbilityWithChargesOrStacks {
 
 	@Override
 	public int getMaxCharges() {
-		return isLevelOne() ? RONDE_1_MAX_STACKS : RONDE_2_MAX_STACKS;
+		return mMaxStacks;
 	}
 
 }
