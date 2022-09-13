c0499a453a58698b10cc075973edee952c9d2ba0
Timothy Southwick <NickNackGus@gmail.com>
2022-09-13 18:09:31 -0400

    Removed remaining pre-1.18 code (#2084)

--- plugins/adapter_api/build.gradle.kts
+++ plugins/adapter_api/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    compileOnly("com.destroystokyo.paper:paper-api:1.15.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
 }
 
 description = "adapter_api"
