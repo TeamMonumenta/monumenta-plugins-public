d232f1b7423d34bb7d41d372c4a1e610ac25b8de
Asinius <44119581+Asinius@users.noreply.github.com>
2022-10-20 17:30:12 -0400

    Fixing echoes (again) (#2315)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/WhispersBoss.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/WhispersBoss.java
@@ -3,9 +3,7 @@ package com.playmonumenta.plugins.bosses.bosses;
 import com.playmonumenta.plugins.bosses.SpellManager;
 import com.playmonumenta.plugins.events.DamageEvent;
 import com.playmonumenta.plugins.events.DamageEvent.DamageType;
-import com.playmonumenta.plugins.utils.DamageUtils;
 import com.playmonumenta.plugins.utils.EntityUtils;
-import com.playmonumenta.plugins.utils.PlayerUtils;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -17,8 +15,6 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class WhispersBoss extends BossAbilityGroup {
@@ -50,21 +46,12 @@ public class WhispersBoss extends BossAbilityGroup {
 	public void onDamage(DamageEvent event, LivingEntity damagee) {
 		if (damagee instanceof Player player && !EntityUtils.hasAttributesContaining(player, Attribute.GENERIC_MAX_HEALTH, "Whispers" + mBoss.getUniqueId())
 			&& !event.getType().equals(DamageType.TRUE) && !event.isBlocked()) {
-			double maxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
-			double trueDamage = mParam.PERCENT_DAMAGE / 100.0 * maxHealth;
-			PotionEffect resistance = player.getPotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
-			double resistanceLevel = (resistance == null ? 0 : resistance.getAmplifier() + 1);
-			double resistanceMod = (resistanceLevel >= 5 ? 1.0 : 1.0 / (1.0 - 0.2 * resistanceLevel));
-
 			new BukkitRunnable() {
 				@Override
 				public void run() {
 					if (!player.isDead() && !mBoss.isDead() && !event.isCancelled()) {
 						mStolenPlayers.add(player);
 						player.playSound(player.getLocation(), Sound.BLOCK_BUBBLE_COLUMN_WHIRLPOOL_INSIDE, 5, 1f);
-						if (player.getAbsorptionAmount() == 0) {
-							DamageUtils.damage(null, damagee, new DamageEvent.Metadata(DamageType.TRUE, null, null, "Whispers"), trueDamage * resistanceMod, true, false, false);
-						}
 						EntityUtils.addAttribute(player, Attribute.GENERIC_MAX_HEALTH, new AttributeModifier("Whispers" + mBoss.getUniqueId(), -mParam.PERCENT_DAMAGE / 100.0, AttributeModifier.Operation.MULTIPLY_SCALAR_1));
 						BukkitRunnable cleanse = new BukkitRunnable() {
 							@Override
@@ -86,7 +73,7 @@ public class WhispersBoss extends BossAbilityGroup {
 	public void death(EntityDeathEvent event) {
 		for (int i = 0; i < mStolenPlayers.size(); i++) {
 			Player stolenPlayer = mStolenPlayers.get(i);
-			if (EntityUtils.hasAttributesContaining(stolenPlayer, Attribute.GENERIC_MAX_HEALTH, "Whispers")) {
+			if (!stolenPlayer.isDead() && EntityUtils.hasAttributesContaining(stolenPlayer, Attribute.GENERIC_MAX_HEALTH, "Whispers")) {
 				mCleanse.get(i).cancel();
 				double amtHeal = (mParam.PERCENT_DAMAGE / 100.0) / (1 - mParam.PERCENT_DAMAGE / 100.0) * stolenPlayer.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
 				EntityUtils.removeAttribute(stolenPlayer, Attribute.GENERIC_MAX_HEALTH, "Whispers" + mBoss.getUniqueId());
