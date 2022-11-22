2c24277d49237d4200f83a424af015dbb4932c9f
Peter Güttinger <njol@gmx.ch>
2022-11-22 15:12:38 +0100

    Fixed CME in Trivium (#2592)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/enchantments/Trivium.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/enchantments/Trivium.java
@@ -14,13 +14,14 @@ import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitTask;
 
 public class Trivium implements Enchantment {
 
 	private final double DAMAGE_PER_LEVEL = 0.1;
 
 	private HashMap<Player, HashMap<ClassAbility, List<DamageEvent>>> mDamageInTick = new HashMap<>();
-	private boolean mRunDamageNextTick = false;
+	private BukkitTask mRunDamageTask = null;
 
 	@Override
 	public String getName() {
@@ -44,10 +45,11 @@ public class Trivium implements Enchantment {
 		if (event.getType() == DamageEvent.DamageType.MAGIC && event.getAbility() != null) {
 			mDamageInTick.computeIfAbsent(player, key -> new HashMap<>()).computeIfAbsent(event.getAbility(), key -> new ArrayList<>()).add(event);
 
-			if (!mRunDamageNextTick) {
-				mRunDamageNextTick = true;
-				Bukkit.getScheduler().runTask(plugin, () -> {
-					mDamageInTick.forEach((p, map) -> {
+			if (mRunDamageTask == null || !Bukkit.getScheduler().isQueued(mRunDamageTask.getTaskId())) {
+				mRunDamageTask = Bukkit.getScheduler().runTask(plugin, () -> {
+					HashMap<Player, HashMap<ClassAbility, List<DamageEvent>>> damageInTick = mDamageInTick;
+					mDamageInTick = new HashMap<>();
+					damageInTick.forEach((p, map) -> {
 						map.forEach((ability, eventList) -> {
 							if (eventList.size() >= 3) {
 								// If this tick had more than 3 of the same damage event of the same ability...
@@ -72,8 +74,7 @@ public class Trivium implements Enchantment {
 							}
 						});
 					});
-					mDamageInTick.clear();
-					mRunDamageNextTick = false;
+					mRunDamageTask = null;
 				});
 			}
 		}
