d621a49e7b9211fbccefd8ae4b6e1ec3291b6d64
Peter Güttinger <njol@gmx.ch>
2022-12-12 07:41:24 +0100

    Re-enabled the drop key in creative mode (#2761)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/protocollib/DropKeyPacketListener.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/protocollib/DropKeyPacketListener.java
@@ -7,6 +7,7 @@ import com.comphenix.protocol.events.PacketEvent;
 import com.playmonumenta.plugins.Plugin;
 import com.playmonumenta.plugins.abilities.AbilityTrigger;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 
 /**
@@ -25,9 +26,13 @@ public class DropKeyPacketListener extends PacketAdapter {
 		PacketPlayInBlockDigHandle.EnumPlayerDigTypeHandle digType = packet.getDigType();
 		if (PacketPlayInBlockDigHandle.EnumPlayerDigTypeHandle.DROP_ITEM.equals(digType)
 			    || PacketPlayInBlockDigHandle.EnumPlayerDigTypeHandle.DROP_ALL_ITEMS.equals(digType)) {
-			event.setCancelled(true);
 			Player player = event.getPlayer();
-			// drop key causes a left click that needs to be ignored
+			if (player.getGameMode() == GameMode.CREATIVE) {
+				// Allow dropping in creative mode. Accessing the player's game mode should be reasonably thread-safe.
+				return;
+			}
+			event.setCancelled(true);
+			// Drop key causes a left click that needs to be ignored
 			Plugin.getInstance().mAbilityManager.preDropKey(player);
 			Bukkit.getScheduler().runTask(plugin, () -> {
 				Plugin.getInstance().mAbilityManager.checkTrigger(player, AbilityTrigger.Key.DROP);
