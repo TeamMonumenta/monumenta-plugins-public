e69adab3b3f80c80fd0819873b03d0f89826750d
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2023-01-25 00:37:03 -0800

    fix issue with switching away from weapon when dealing melee skill damage (#2987)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/attributes/AttackDamageAdd.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/attributes/AttackDamageAdd.java
@@ -4,15 +4,9 @@ import com.playmonumenta.plugins.Plugin;
 import com.playmonumenta.plugins.events.DamageEvent;
 import com.playmonumenta.plugins.events.DamageEvent.DamageType;
 import com.playmonumenta.plugins.itemstats.Attribute;
-import com.playmonumenta.plugins.utils.ItemStatUtils;
 import com.playmonumenta.plugins.utils.ItemStatUtils.AttributeType;
-import com.playmonumenta.plugins.utils.ItemStatUtils.Operation;
-import com.playmonumenta.plugins.utils.ItemStatUtils.Slot;
-import de.tr7zw.nbtapi.NBTCompoundList;
-import de.tr7zw.nbtapi.NBTItem;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
 public class AttackDamageAdd implements Attribute {
 
@@ -35,14 +29,6 @@ public class AttackDamageAdd implements Attribute {
 	public void onDamage(Plugin plugin, Player player, double value, DamageEvent event, LivingEntity enemy) {
 		if (event.getType() == DamageType.MELEE) {
 			event.setDamage(event.getDamage() + value);
-		} else if (event.getType() == DamageType.MELEE_SKILL) {
-			ItemStack item = player.getInventory().getItemInMainHand();
-			if (item.getType().isAir()) {
-				return;
-			}
-			NBTItem nbt = new NBTItem(item);
-			NBTCompoundList compound = ItemStatUtils.getAttributes(nbt);
-			event.setDamage(event.getDamage() + (value - ItemStatUtils.getAttributeAmount(compound, AttributeType.ATTACK_DAMAGE_ADD, Operation.ADD, Slot.MAINHAND)));
 		}
 	}
 }
