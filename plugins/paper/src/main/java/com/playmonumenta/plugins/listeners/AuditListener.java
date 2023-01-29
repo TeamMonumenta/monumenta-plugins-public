0a26443a715e3c836d5c9b786040cc558d61fe6c
dmdinnc <38630939+dmdinnc@users.noreply.github.com>
2023-01-29 12:29:47 -0500

    audit: add trade chat exception (#3019)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/listeners/AuditListener.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/listeners/AuditListener.java
@@ -69,6 +69,7 @@ public class AuditListener implements Listener {
 		exactOptionalArguments("lfg"),
 		exactOptionalArguments("m"),
 		exactOptionalArguments("mh"),
+		exactOptionalArguments("tr"),
 		exactOptionalArguments("join"),
 		exactOptionalArguments("leave"),
 		exactOptionalArguments("ignore"),
