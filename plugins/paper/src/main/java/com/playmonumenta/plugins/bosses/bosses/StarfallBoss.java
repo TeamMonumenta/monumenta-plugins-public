6341da5df2a8f12d738a79908ca4450a20d1399b
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2023-02-15 12:55:18 -0800

    add random spread option in starfall boss (#3067)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/StarfallBoss.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/StarfallBoss.java
@@ -11,8 +11,11 @@ import com.playmonumenta.plugins.events.DamageEvent;
 import com.playmonumenta.plugins.utils.BossUtils;
 import com.playmonumenta.plugins.utils.EntityUtils;
 import com.playmonumenta.plugins.utils.FastUtils;
+import com.playmonumenta.plugins.utils.LocationUtils;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.BiFunction;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.plugin.Plugin;
@@ -38,10 +41,20 @@ public class StarfallBoss extends BossAbilityGroup {
 		public int METEOR_SPEED = 2;
 		public double HEIGHT = 16;
 
+		@BossParam(help = "The radius of the explosion")
 		public double SPHERE_RADIUS = 4;
 
 		public EntityTargets TARGETS = EntityTargets.GENERIC_ONE_PLAYER_TARGET;
 		public EntityTargets TARGETS_EXPLOSION = EntityTargets.GENERIC_PLAYER_TARGET.clone().setRange(5);
+
+		@BossParam(help = "Set to true to target entities; set to false to spawn randomly in the range")
+		public boolean DOES_TARGETING = true;
+
+		@BossParam(help = "The range when set to spawn randomly (i.e. DOES_TARGETING = FALSE). Does nothing otherwise")
+		public double RANGE = 12;
+
+		@BossParam(help = "The number of starfalls to spawn when set to spawn randomly (i.e. DOES_TARGETING = FALSE). Does nothing otherwise")
+		public int COUNT = 3;
 
 		public ParticlesList PARTICLE_CIRCLE = ParticlesList.fromString("[(FLAME,1,0,0,0,0.1)]");
 
@@ -67,85 +80,16 @@ public class StarfallBoss extends BossAbilityGroup {
 			new Spell() {
 				@Override
 				public void run() {
-					List<? extends LivingEntity> targets = p.TARGETS.getTargetsList(mBoss);
-
-					for (LivingEntity target : targets) {
-						new BukkitRunnable() {
-							int mTicks = 0;
-							Location mLocation = target.getLocation();
-
-
-							@Override
-							public void run() {
-								if (EntityUtils.shouldCancelSpells(mBoss)) {
-									cancel();
-									return;
-								}
-								if (mTicks >= p.LOCKING_DURATION) {
-									new BukkitRunnable() {
-										final double mStep = p.METEOR_SPEED;
-										double mCurrentHeight = p.HEIGHT;
-
-										@Override
-										public void run() {
-											if (EntityUtils.shouldCancelSpells(mBoss)) {
-												cancel();
-												return;
-											}
-
-											mCurrentHeight -= mStep;
-											Location meteorCenter = mLocation.clone().add(0, mCurrentHeight, 0);
-
-											if (mCurrentHeight <= 0) {
-												p.PARTICLE_EXPLOSION.spawn(boss, meteorCenter, p.TARGETS_EXPLOSION.getRange() / 2, p.TARGETS_EXPLOSION.getRange() / 2, p.TARGETS_EXPLOSION.getRange() / 2, 0.1);
-												p.SOUND_EXPLOSION.play(meteorCenter);
-												for (LivingEntity target : p.TARGETS_EXPLOSION.getTargetsListByLocation(mBoss, meteorCenter)) {
-
-													if (p.DAMAGE > 0) {
-														// Must be looking up to block
-														BossUtils.blockableDamage(mBoss, target, p.DAMAGE_TYPE, p.DAMAGE, p.SPELL_NAME, meteorCenter);
-													}
-
-													if (p.DAMAGE_PERCENTAGE > 0.0) {
-														BossUtils.bossDamagePercent(mBoss, target, p.DAMAGE_PERCENTAGE, meteorCenter, p.SPELL_NAME);
-													}
-													p.EFFECTS.apply(target, mBoss);
-												}
-
-												cancel();
-											}
-
-											p.SOUND_METEOR.play(meteorCenter, 3.0f, (float) (mCurrentHeight / p.HEIGHT) * 1.5f);
-											p.PARTICLE_METEOR.spawn(boss, meteorCenter);
-
-										}
-									}.runTaskTimer(mPlugin, 0, 1);
-
-									cancel();
-									return;
-								}
-
-								if (mTicks <= p.TRACKING) {
-									mLocation = target.getLocation();
-								}
-
-								if (mTicks % 4 == 0) {
-									double size = (p.LOCKING_DURATION - mTicks) / 20.0 * p.SPHERE_RADIUS;
-									for (int degree = 0; degree <= 360; degree += 5) {
-										double radiant = Math.toRadians(degree);
-										Location l = mLocation.clone().add(FastUtils.cos(radiant) * size, 0.3, FastUtils.sin(radiant) * size);
-										p.PARTICLE_CIRCLE.spawn(boss, l);
-									}
-								}
-
-								p.SOUND_LOCKING.play(mLocation, 1, 1.5f * (((float) mTicks + 1) / (float) p.LOCKING_DURATION));
-
-
-								mTicks += 2;
-							}
-						}.runTaskTimer(mPlugin, 0, 2);
+					if (p.DOES_TARGETING) {
+						List<? extends LivingEntity> targets = p.TARGETS.getTargetsList(mBoss);
+						targets.forEach(e -> spawnStarfall(p, mBoss, e.getLocation(), (ticks, oldLoc) -> ticks <= p.TRACKING ? e.getLocation() : oldLoc));
+					} else {
+						List<Location> locs = new ArrayList<>();
+						for (int i = 1; i <= p.COUNT; i++) {
+							locs.add(LocationUtils.randomLocationInCircle(mBoss.getLocation(), p.RANGE));
+						}
+						locs.forEach(l -> spawnStarfall(p, mBoss, l, (ticks, oldLoc) -> l));
 					}
-
 				}
 
 				@Override
@@ -155,11 +99,84 @@ public class StarfallBoss extends BossAbilityGroup {
 
 				@Override
 				public boolean canRun() {
-					return !p.TARGETS.getTargetsList(mBoss).isEmpty();
+					return !p.TARGETS.getTargetsList(mBoss).isEmpty() || !p.DOES_TARGETING;
 				}
 			}
 		));
 
 		super.constructBoss(spellManager, Collections.emptyList(), (int) (p.TARGETS.getRange() * 2), null, p.DELAY);
+	}
+
+	private void spawnStarfall(Parameters p, LivingEntity boss, Location loc, BiFunction<Integer, Location, Location> locationGetter) {
+		new BukkitRunnable() {
+			int mTicks = 0;
+			Location mLocation = loc;
+
+			@Override
+			public void run() {
+				if (EntityUtils.shouldCancelSpells(mBoss)) {
+					cancel();
+					return;
+				}
+				if (mTicks >= p.LOCKING_DURATION) {
+					new BukkitRunnable() {
+						final double mStep = p.METEOR_SPEED;
+						double mCurrentHeight = p.HEIGHT;
+
+						@Override
+						public void run() {
+							if (EntityUtils.shouldCancelSpells(mBoss)) {
+								cancel();
+								return;
+							}
+
+							mCurrentHeight -= mStep;
+							Location meteorCenter = mLocation.clone().add(0, mCurrentHeight, 0);
+
+							if (mCurrentHeight <= 0) {
+								p.PARTICLE_EXPLOSION.spawn(boss, meteorCenter, p.TARGETS_EXPLOSION.getRange() / 2, p.TARGETS_EXPLOSION.getRange() / 2, p.TARGETS_EXPLOSION.getRange() / 2, 0.1);
+								p.SOUND_EXPLOSION.play(meteorCenter);
+								for (LivingEntity target : p.TARGETS_EXPLOSION.getTargetsListByLocation(mBoss, meteorCenter)) {
+
+									if (p.DAMAGE > 0) {
+										// Must be looking up to block
+										BossUtils.blockableDamage(mBoss, target, p.DAMAGE_TYPE, p.DAMAGE, p.SPELL_NAME, meteorCenter);
+									}
+
+									if (p.DAMAGE_PERCENTAGE > 0.0) {
+										BossUtils.bossDamagePercent(mBoss, target, p.DAMAGE_PERCENTAGE, meteorCenter, p.SPELL_NAME);
+									}
+									p.EFFECTS.apply(target, mBoss);
+								}
+
+								cancel();
+							}
+
+							p.SOUND_METEOR.play(meteorCenter, 3.0f, (float) (mCurrentHeight / p.HEIGHT) * 1.5f);
+							p.PARTICLE_METEOR.spawn(boss, meteorCenter);
+
+						}
+					}.runTaskTimer(mPlugin, 0, 1);
+
+					cancel();
+					return;
+				}
+
+				mLocation = locationGetter.apply(mTicks, mLocation);
+
+				if (mTicks % 4 == 0) {
+					double size = (p.LOCKING_DURATION - mTicks) / 20.0 * p.SPHERE_RADIUS;
+					for (int degree = 0; degree <= 360; degree += 5) {
+						double radiant = Math.toRadians(degree);
+						Location l = mLocation.clone().add(FastUtils.cos(radiant) * size, 0.3, FastUtils.sin(radiant) * size);
+						p.PARTICLE_CIRCLE.spawn(boss, l);
+					}
+				}
+
+				p.SOUND_LOCKING.play(mLocation, 1, 1.5f * (((float) mTicks + 1) / (float) p.LOCKING_DURATION));
+
+				mTicks += 2;
+			}
+		}.runTaskTimer(mPlugin, 0, 2);
 	}
 }
