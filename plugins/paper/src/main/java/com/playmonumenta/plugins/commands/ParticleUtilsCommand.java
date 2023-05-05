dbc654c70c19fc1c646859d9fc8a8c79aa7ad8a8
FlamingoBike <36803440+FlamingoBike@users.noreply.github.com>
2023-05-05 03:11:46 +0200

    put arguments in fields, add possibility to spawn particles fully (#3300)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/commands/ParticleUtilsCommand.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/commands/ParticleUtilsCommand.java
@@ -1,5 +1,6 @@
 package com.playmonumenta.plugins.commands;
 
+import com.mojang.brigadier.LiteralMessage;
 import com.playmonumenta.plugins.Plugin;
 import com.playmonumenta.plugins.particle.AbstractPartialParticle;
 import com.playmonumenta.plugins.particle.PPCircle;
@@ -9,6 +10,8 @@ import com.playmonumenta.plugins.utils.CommandUtils;
 import com.playmonumenta.plugins.utils.FastUtils;
 import com.playmonumenta.plugins.utils.MMLog;
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.StringTooltip;
+import dev.jorel.commandapi.arguments.Argument;
 import dev.jorel.commandapi.arguments.ArgumentSuggestions;
 import dev.jorel.commandapi.arguments.BooleanArgument;
 import dev.jorel.commandapi.arguments.DoubleArgument;
@@ -36,6 +39,16 @@ public class ParticleUtilsCommand {
 	public static String COMMAND = "particleutils";
 	public static Particle.DustOptions DEFAULT_DUST_OPTIONS = new Particle.DustOptions(Color.RED, 1);
 	public static BlockData DEFAULT_BLOCK_DATA = Material.REDSTONE_BLOCK.createBlockData();
+	private static final Argument<String> particleArgument = new StringArgument("particle").includeSuggestions(
+		ArgumentSuggestions.strings(Arrays.stream(Particle.values()).map(Enum::name).collect(Collectors.toList()))
+	);
+	private static final Argument<Double> distanceFalloffArgument = new DoubleArgument("distance falloff").includeSuggestions(
+		ArgumentSuggestions.stringsWithTooltips(StringTooltip.ofMessage("0", new LiteralMessage("Spawn the particles fully, ignoring PEB.")))
+	);
+	private static final Argument<String> extraDataArgument = new TextArgument("extra data").includeSuggestions(
+		ArgumentSuggestions.strings("null", "\"255,0,0,2.5\"", "REDSTONE_BLOCK")
+	);
+
 
 	public static void register() {
 		// Line
@@ -46,13 +59,9 @@ public class ParticleUtilsCommand {
 				new LocationArgument("start"),
 				new LocationArgument("end"),
 				new DoubleArgument("count per meter"),
-				new StringArgument("particle").includeSuggestions(
-					ArgumentSuggestions.strings(Arrays.stream(Particle.values()).map(Enum::name).collect(Collectors.toList()))
-				),
-				new DoubleArgument("distance falloff"),
-				new TextArgument("extra data").includeSuggestions(
-					ArgumentSuggestions.strings("null", "\"255,0,0,2.5\"", "REDSTONE_BLOCK")
-				)
+				particleArgument,
+				distanceFalloffArgument,
+				extraDataArgument
 			)
 			.executes((sender, args) -> {
 				Location start = (Location) args[0];
@@ -60,10 +69,9 @@ public class ParticleUtilsCommand {
 				double countPerMeter = (double) args[2];
 				Particle particle = Particle.valueOf((String) args[3]);
 				double distanceFalloff = (double) args[4];
-
-				CommandSender callee = CommandUtils.getCallee(sender);
-
 				String extraData = (String) args[5];
+
+				CommandSender callee = getFinalCallee(sender, distanceFalloff);
 				doLine(callee, start, end, countPerMeter, particle, distanceFalloff, parseExtraData(extraData, particle));
 			})
 			.register();
@@ -77,13 +85,9 @@ public class ParticleUtilsCommand {
 				new DoubleArgument("dx"),
 				new DoubleArgument("dz"),
 				new DoubleArgument("count per meter"),
-				new StringArgument("particle").includeSuggestions(
-					ArgumentSuggestions.strings(Arrays.stream(Particle.values()).map(Enum::name).collect(Collectors.toList()))
-				),
-				new DoubleArgument("distance falloff"),
-				new TextArgument("extra data").includeSuggestions(
-					ArgumentSuggestions.strings("null", "\"255,0,0,2.5\"", "REDSTONE_BLOCK")
-				)
+				particleArgument,
+				distanceFalloffArgument,
+				extraDataArgument
 			)
 			.executes((sender, args) -> {
 				Location start = (Location) args[0];
@@ -92,10 +96,9 @@ public class ParticleUtilsCommand {
 				double countPerMeter = (double) args[3];
 				Particle particle = Particle.valueOf((String) args[4]);
 				double distanceFalloff = (double) args[5];
-
-				CommandSender callee = CommandUtils.getCallee(sender);
-
 				String extraData = (String) args[6];
+
+				CommandSender callee = getFinalCallee(sender, distanceFalloff);
 				doRectangle(callee, start, dx, dz, countPerMeter, particle, distanceFalloff, parseExtraData(extraData, particle));
 			})
 			.register();
@@ -109,13 +112,9 @@ public class ParticleUtilsCommand {
 				new LocationArgument("center"),
 				new DoubleArgument("radius"),
 				new DoubleArgument("count per meter"),
-				new StringArgument("particle").includeSuggestions(
-					ArgumentSuggestions.strings(Arrays.stream(Particle.values()).map(Enum::name).collect(Collectors.toList()))
-				),
-				new DoubleArgument("distance falloff"),
-				new TextArgument("extra data").includeSuggestions(
-					ArgumentSuggestions.strings("null", "\"255,0,0,2.5\"", "REDSTONE_BLOCK")
-				)
+				particleArgument,
+				distanceFalloffArgument,
+				extraDataArgument
 			)
 			.executes((sender, args) -> {
 				boolean ringMode = (boolean) args[0];
@@ -124,10 +123,9 @@ public class ParticleUtilsCommand {
 				double countPerMeter = (double) args[3];
 				Particle particle = Particle.valueOf((String) args[4]);
 				double distanceFalloff = (double) args[5];
-
-				CommandSender callee = CommandUtils.getCallee(sender);
-
 				String extraData = (String) args[6];
+
+				CommandSender callee = getFinalCallee(sender, distanceFalloff);
 				doCircle(callee, ringMode, center, radius, countPerMeter, particle, distanceFalloff, parseExtraData(extraData, particle));
 			})
 			.register();
@@ -140,17 +138,13 @@ public class ParticleUtilsCommand {
 				new LocationArgument("center"),
 				new DoubleArgument("radius"),
 				new IntegerArgument("count"),
-				new StringArgument("particle").includeSuggestions(
-					ArgumentSuggestions.strings(Arrays.stream(Particle.values()).map(Enum::name).collect(Collectors.toList()))
-				),
-				new DoubleArgument("distance falloff"),
+				particleArgument,
+				distanceFalloffArgument,
 				new DoubleArgument("particle speed"),
 				new IntegerArgument("pulses"),
 				new IntegerArgument("telegraph duration"),
 				new IntegerArgument("pulse start offset"),
-				new TextArgument("extra data").includeSuggestions(
-					ArgumentSuggestions.strings("null", "\"255,0,0,2.5\"", "REDSTONE_BLOCK")
-				)
+				extraDataArgument
 			)
 			.executes((sender, args) -> {
 				Location center = (Location) args[0];
@@ -162,13 +156,23 @@ public class ParticleUtilsCommand {
 				int pulses = (int) args[6];
 				int telegraphDuration = (int) args[7];
 				int pulseStartOffset = (int) args[8];
-
-				CommandSender callee = CommandUtils.getCallee(sender);
-
 				String extraData = (String) args[9];
+
+				CommandSender callee = getFinalCallee(sender, distanceFalloff);
 				doCircleTelegraph(callee, center, radius, count, particle, distanceFalloff, particleSpeed, pulses, telegraphDuration, pulseStartOffset, parseExtraData(extraData, particle));
 			})
 			.register();
+	}
+
+	// Can return null if distanceFalloff is 0 (or lower), which will be interpreted as
+	// wanting to spawn the full amount of particles, bypassing player/entity PEB settings.
+	// Useful for puzzles!
+	private static @Nullable CommandSender getFinalCallee(CommandSender sender, double distanceFalloff) {
+		if (distanceFalloff <= 0) {
+			return null;
+		}
+
+		return CommandUtils.getCallee(sender);
 	}
 
 	private static @Nullable Object parseExtraData(String extraData, Particle particle) {
@@ -216,12 +220,14 @@ public class ParticleUtilsCommand {
 	private static void spawnAsActive(CommandSender callee, AbstractPartialParticle<? extends AbstractPartialParticle<?>> particle) {
 		if (callee instanceof Player player) {
 			particle.spawnAsPlayerActive(player);
+		} else if (callee instanceof Entity entity) {
+			particle.spawnAsEntityActive(entity);
 		} else {
-			particle.spawnAsEntityActive((Entity) callee);
+			particle.spawnFull();
 		}
 	}
 
-	private static void doLine(CommandSender callee, Location start, Location end, double countPerMeter, Particle particle, double distanceFalloff, @Nullable Object data) {
+	private static void doLine(@Nullable CommandSender callee, Location start, Location end, double countPerMeter, Particle particle, double distanceFalloff, @Nullable Object data) {
 		PPLine line = new PPLine(particle, start, end).countPerMeter(countPerMeter).distanceFalloff(distanceFalloff);
 
 		if (data != null) {
@@ -231,7 +237,7 @@ public class ParticleUtilsCommand {
 		spawnAsActive(callee, line);
 	}
 
-	private static void doRectangle(CommandSender callee, Location start, double dx, double dz, double countPerMeter, Particle particle, double distanceFalloff, @Nullable Object data) {
+	private static void doRectangle(@Nullable CommandSender callee, Location start, double dx, double dz, double countPerMeter, Particle particle, double distanceFalloff, @Nullable Object data) {
 		PPLine[] sides = {
 			new PPLine(particle, start, start.clone().add(dx, 0, 0)).countPerMeter(countPerMeter).distanceFalloff(distanceFalloff),
 			new PPLine(particle, start.clone().add(dx, 0, 0), start.clone().add(dx, 0, dz)).countPerMeter(countPerMeter).distanceFalloff(distanceFalloff),
@@ -246,7 +252,7 @@ public class ParticleUtilsCommand {
 		Arrays.stream(sides).forEach(side -> spawnAsActive(callee, side));
 	}
 
-	private static void doCircle(CommandSender callee, boolean ringMode, Location center, double radius, double countPerMeter, Particle particle, double distanceFalloff, @Nullable Object data) {
+	private static void doCircle(@Nullable CommandSender callee, boolean ringMode, Location center, double radius, double countPerMeter, Particle particle, double distanceFalloff, @Nullable Object data) {
 		PPCircle circle = new PPCircle(particle, center, radius).countPerMeter(countPerMeter).distanceFalloff(distanceFalloff).ringMode(ringMode);
 
 		if (data != null) {
@@ -256,7 +262,7 @@ public class ParticleUtilsCommand {
 		spawnAsActive(callee, circle);
 	}
 
-	private static void doCircleTelegraph(CommandSender callee, Location center, double radius, int count, Particle particle, double distanceFalloff, double particleSpeed, int pulses, int telegraphDuration, int pulseStartOffset, @Nullable Object data) {
+	private static void doCircleTelegraph(@Nullable CommandSender callee, Location center, double radius, int count, Particle particle, double distanceFalloff, double particleSpeed, int pulses, int telegraphDuration, int pulseStartOffset, @Nullable Object data) {
 		// Input Validation
 		int finalPulseStartOffset = Math.max(pulseStartOffset, 0);
 		int finalTelegraphDuration = Math.max(telegraphDuration, 1);
