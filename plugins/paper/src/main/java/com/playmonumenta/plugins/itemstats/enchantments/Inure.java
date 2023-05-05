4e1f489fa261708393bf1de223e06fb4512dd8e1
Fwap a Durp <derpderp0000@gmail.com>
2023-05-05 03:10:34 +0100

    inure buff (#3303)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/enchantments/Inure.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/enchantments/Inure.java
@@ -8,6 +8,8 @@ import com.playmonumenta.plugins.events.DamageEvent.DamageType;
 import com.playmonumenta.plugins.itemstats.Enchantment;
 import com.playmonumenta.plugins.utils.ItemStatUtils.EnchantmentType;
 import java.util.NavigableSet;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -21,6 +23,7 @@ public class Inure implements Enchantment {
 	private static final String INURE_PROJ_NAME = "ProjectileInureEffect";
 	private static final String INURE_MAGIC_NAME = "MagicInureEffect";
 	private static final String INURE_BLAST_NAME = "BlastInureEffect";
+	private static final String INURE_MEMORY_NAME = "MemoryInureEffect";
 
 	@Override
 	public String getName() {
@@ -64,14 +67,19 @@ public class Inure implements Enchantment {
 		NavigableSet<Effect> proj = plugin.mEffectManager.getEffects(player, INURE_PROJ_NAME);
 		NavigableSet<Effect> magic = plugin.mEffectManager.getEffects(player, INURE_MAGIC_NAME);
 		NavigableSet<Effect> blast = plugin.mEffectManager.getEffects(player, INURE_BLAST_NAME);
-		if (event.getType() == DamageType.MELEE && melee != null) {
+		NavigableSet<Effect> memory = plugin.mEffectManager.getEffects(player, INURE_MEMORY_NAME);
+		if ((event.getType() == DamageType.MELEE && melee != null) ||
+			(event.getType() == DamageType.PROJECTILE && proj != null) ||
+			(event.getType() == DamageType.MAGIC && magic != null) ||
+			(event.getType() == DamageType.BLAST && blast != null)) {
+			// max inure
+			player.playSound(player.getLocation(), Sound.BLOCK_SOUL_SOIL_HIT, SoundCategory.HOSTILE, 0.7f, 1.0f);
+			plugin.mEffectManager.addEffect(player, INURE_MEMORY_NAME, new OnHitTimerEffect(PAST_HIT_DURATION_TIME));
 			return plugin.mItemStatManager.getEnchantmentLevel(player, EnchantmentType.INURE) * ARMOR_BONUS_PER_LEVEL;
-		} else if (event.getType() == DamageType.PROJECTILE && proj != null) {
-			return plugin.mItemStatManager.getEnchantmentLevel(player, EnchantmentType.INURE) * ARMOR_BONUS_PER_LEVEL;
-		} else if (event.getType() == DamageType.MAGIC && magic != null) {
-			return plugin.mItemStatManager.getEnchantmentLevel(player, EnchantmentType.INURE) * ARMOR_BONUS_PER_LEVEL;
-		} else if (event.getType() == DamageType.BLAST && blast != null) {
-			return plugin.mItemStatManager.getEnchantmentLevel(player, EnchantmentType.INURE) * ARMOR_BONUS_PER_LEVEL;
+		} else if (memory != null) {
+			player.playSound(player.getLocation(), Sound.BLOCK_SOUL_SOIL_BREAK, SoundCategory.HOSTILE, 0.7f, 1.0f);
+			plugin.mEffectManager.clearEffects(player, INURE_MEMORY_NAME);
+			return plugin.mItemStatManager.getEnchantmentLevel(player, EnchantmentType.INURE) * ARMOR_BONUS_PER_LEVEL / 2;
 		} else {
 			return 0;
 		}
