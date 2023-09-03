9ca80f73ca3ea4c726150356dea4b1b270deb317
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2023-09-03 00:16:30 -0700

    fix abyssal not working properly with projectiles (#3604)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/enchantments/Abyssal.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/enchantments/Abyssal.java
@@ -42,9 +42,8 @@ public class Abyssal implements Enchantment {
 		if (event.getType() == DamageEvent.DamageType.TRUE) {
 			return;
 		}
-		int level = plugin.mItemStatManager.getEnchantmentLevel(player, EnchantmentType.ABYSSAL);
 		if (EntityUtils.isInWater(enemy) || EntityUtils.isInWater(player) || ZoneUtils.hasZoneProperty(player.getLocation(), ZoneUtils.ZoneProperty.ABYSSAL_FORCED)) {
-			double multiplier = 1 + CharmManager.calculateFlatAndPercentValue(player, CHARM_DAMAGE, DAMAGE_BONUS_PER_LEVEL * level);
+			double multiplier = 1 + CharmManager.calculateFlatAndPercentValue(player, CHARM_DAMAGE, DAMAGE_BONUS_PER_LEVEL * value);
 			event.setDamage(event.getDamage() * multiplier);
 		}
 	}
