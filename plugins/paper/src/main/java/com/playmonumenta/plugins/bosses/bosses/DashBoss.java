fab35c3f7e9d36ff92fc3250848abc96ca04c53e
slyyam <118639502+slyyam@users.noreply.github.com>
2023-09-28 16:10:06 -0400

    EntityTargets, detection, preferTarget, and minRange added to boss_dash (#3665)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/DashBoss.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/DashBoss.java
@@ -2,6 +2,7 @@ package com.playmonumenta.plugins.bosses.bosses;
 
 import com.playmonumenta.plugins.bosses.SpellManager;
 import com.playmonumenta.plugins.bosses.parameters.BossParam;
+import com.playmonumenta.plugins.bosses.parameters.EntityTargets;
 import com.playmonumenta.plugins.bosses.parameters.ParticlesList;
 import com.playmonumenta.plugins.bosses.parameters.SoundsList;
 import com.playmonumenta.plugins.bosses.spells.SpellDash;
@@ -12,7 +13,6 @@ import org.bukkit.plugin.Plugin;
 
 public class DashBoss extends BossAbilityGroup {
 	public static final String identityTag = "boss_dash";
-	public static final int detectionRange = 30;
 
 	public static class Parameters extends BossParameters {
 		@BossParam(help = "The base cooldown between each swing. Default: 100")
@@ -23,6 +23,14 @@ public class DashBoss extends BossAbilityGroup {
 		public double JUMP_VELOCITY = 1;
 		@BossParam(help = "The x/z velocity of the jump. Default: 1")
 		public double VELOCITY = 1;
+		@BossParam(help = "detection range")
+		public int DETECTION = 30;
+		@BossParam(help = "minimum range the boss should be to cast this ability")
+		public double MIN_RANGE = 1.0;
+		@BossParam(help = "targets of the spell")
+		public EntityTargets TARGET = EntityTargets.GENERIC_PLAYER_TARGET;
+		@BossParam(help = "prefers aggro target")
+		public boolean PREFER_TARGET = true;
 		@BossParam(help = "The sound of the jump. Default: Pillager Celebrate")
 		public SoundsList SOUND_START = SoundsList.fromString("[(ENTITY_PILLAGER_CELEBRATE,1,1.1)]");
 		@BossParam(help = "The sound played during the flight. Default: Pillager Celebrate")
@@ -41,12 +49,13 @@ public class DashBoss extends BossAbilityGroup {
 		super(plugin, identityTag, boss);
 
 		mParams = Parameters.getParameters(boss, identityTag, new Parameters());
-
+		if (mParams.TARGET == EntityTargets.GENERIC_PLAYER_TARGET) {
+			mParams.TARGET = new EntityTargets(EntityTargets.TARGETS.PLAYER, mParams.MIN_RANGE, false, EntityTargets.Limit.DEFAULT, List.of(EntityTargets.PLAYERFILTER.HAS_LINEOFSIGHT));
+		}
 		SpellManager activeSpells = new SpellManager(List.of(
-			new SpellDash(plugin, boss, mParams.COOLDOWN, mParams.JUMP_VELOCITY, mParams.VELOCITY,
-				mParams.SOUND_START, mParams.SOUND_LAND, mParams.PARTICLE_START, mParams.PARTICLE_AIR, mParams.PARTICLE_LAND)
+				new SpellDash(plugin, boss, mParams.COOLDOWN, mParams.MIN_RANGE, mParams.TARGET, mParams.PREFER_TARGET, mParams.JUMP_VELOCITY, mParams.VELOCITY, mParams.SOUND_START, mParams.SOUND_LAND, mParams.PARTICLE_START, mParams.PARTICLE_AIR, mParams.PARTICLE_LAND)
 		));
 
-		super.constructBoss(activeSpells, Collections.emptyList(), detectionRange, null, mParams.DELAY);
+		super.constructBoss(activeSpells, Collections.emptyList(), mParams.DETECTION, null, mParams.DELAY);
 	}
 }
--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/SpellDash.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/SpellDash.java
@@ -1,23 +1,25 @@
 package com.playmonumenta.plugins.bosses.spells;
 
+import com.playmonumenta.plugins.bosses.parameters.EntityTargets;
 import com.playmonumenta.plugins.bosses.parameters.ParticlesList;
 import com.playmonumenta.plugins.bosses.parameters.SoundsList;
-import com.playmonumenta.plugins.utils.EntityUtils;
 import com.playmonumenta.plugins.utils.ParticleUtils;
 import java.util.AbstractMap;
 import java.util.List;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Mob;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 public class SpellDash extends Spell {
-
 	public final Plugin mPlugin;
 	public final LivingEntity mBoss;
 	public final int mCooldown;
+	public final double mMinRange;
+	public final EntityTargets mTarget;
+	public final boolean mPreferTarget;
 	public final double mJumpVelocity;
 	public final double mVelocity;
 	public final SoundsList mSoundsStart;
@@ -26,12 +28,15 @@ public class SpellDash extends Spell {
 	public final ParticlesList mParticlesAir;
 	public final ParticlesList mParticlesLand;
 
-	public SpellDash(Plugin plugin, LivingEntity boss, int cooldown, double jumpVelocity, double velocity,
+	public SpellDash(Plugin plugin, LivingEntity boss, int cooldown, double minRange, EntityTargets target, boolean preferTarget, double jumpVelocity, double velocity,
 					 SoundsList soundsStart, SoundsList soundsLand,
 					 ParticlesList particlesStart, ParticlesList particlesAir, ParticlesList particlesLand) {
 		mPlugin = plugin;
 		mBoss = boss;
 		mCooldown = cooldown;
+		mMinRange = minRange;
+		mTarget = target;
+		mPreferTarget = preferTarget;
 		mJumpVelocity = jumpVelocity;
 		mVelocity = velocity;
 		mSoundsStart = soundsStart;
@@ -44,28 +49,33 @@ public class SpellDash extends Spell {
 	@Override
 	public void run() {
 		Location bossLoc = mBoss.getLocation();
-		Player target = EntityUtils.getNearestPlayer(bossLoc, 5);
-		if (target == null) {
+		List<? extends LivingEntity> targets = mTarget.getTargetsList(mBoss);
+		if (targets == null) {
 			return;
 		}
+		LivingEntity dashTarget = targets.get(0);
+		if (mPreferTarget) {
+			dashTarget = ((Mob) mBoss).getTarget();
+		}
+		if (dashTarget != null && mBoss.getLocation().distance(dashTarget.getLocation()) >= Math.max(0.05, mMinRange)) {
+			Vector velocity = bossLoc.toVector().subtract(dashTarget.getLocation().toVector()).normalize();
+			velocity.multiply(new Vector(mVelocity, 1, mVelocity));
+			velocity.setY(mJumpVelocity);
+			mBoss.setVelocity(velocity);
 
-		Vector velocity = bossLoc.toVector().subtract(target.getLocation().toVector()).normalize();
-		velocity.multiply(new Vector(mVelocity, 1, mVelocity));
-		velocity.setY(mJumpVelocity);
-		mBoss.setVelocity(velocity);
-
-		// Aesthetics
-		playJumpAesthetics();
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				if (mBoss.isOnGround()) {
-					playLandingAesthetics();
-					this.cancel();
+			// Aesthetics
+			playJumpAesthetics();
+			new BukkitRunnable() {
+				@Override
+				public void run() {
+					if (mBoss.isOnGround()) {
+						playLandingAesthetics();
+						this.cancel();
+					}
+					playAirAesthetics();
 				}
-				playAirAesthetics();
-			}
-		}.runTaskTimer(mPlugin, 1, 1);
+			}.runTaskTimer(mPlugin, 1, 1);
+		}
 	}
 
 	@Override
