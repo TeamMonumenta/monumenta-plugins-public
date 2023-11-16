7ae14c2b9769207751e7331e130be1ba7e4d1d7d
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2023-11-16 06:15:17 -0800

    fix tuathan blessing always applying effects to the user

--- plugins/paper/src/main/java/com/playmonumenta/plugins/effects/TuathanBlessing.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/effects/TuathanBlessing.java
@@ -3,6 +3,7 @@ package com.playmonumenta.plugins.effects;
 import com.google.gson.JsonObject;
 import com.playmonumenta.plugins.Plugin;
 import com.playmonumenta.plugins.utils.PlayerUtils;
+import java.util.List;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
@@ -22,10 +23,13 @@ public class TuathanBlessing extends ZeroArgumentEffect {
 
 	@Override
 	public void entityTickEffect(Entity entity, boolean fourHertz, boolean twoHertz, boolean oneHertz) {
-		if (oneHertz) {
-			Plugin.getInstance().mEffectManager.addEffect(entity, SPEED_EFFECT, new PercentSpeed(25, AMOUNT, SPEED_EFFECT).displaysTime(false));
-			for (Player player : PlayerUtils.playersInRange(entity.getLocation(), RADIUS, false)) {
-				Plugin.getInstance().mEffectManager.addEffect(player, DEFENSE_EFFECT, new PercentDamageReceived(25, -1 * AMOUNT).displaysTime(false));
+		if (oneHertz && entity instanceof Player player) {
+			List<Player> otherPlayers = PlayerUtils.otherPlayersInRange(player, RADIUS, false);
+			for (Player p : otherPlayers) {
+				Plugin.getInstance().mEffectManager.addEffect(p, DEFENSE_EFFECT, new PercentDamageReceived(25, -1 * AMOUNT).displaysTime(false));
+			}
+			if (!otherPlayers.isEmpty()) {
+				Plugin.getInstance().mEffectManager.addEffect(entity, SPEED_EFFECT, new PercentSpeed(25, AMOUNT, SPEED_EFFECT).displaysTime(false));
 			}
 		}
 	}
