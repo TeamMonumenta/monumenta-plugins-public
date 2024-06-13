78b020d505fc2aa04d0d89f42c6a128a1e797af9
FrozenEarth <39888817+FrozenEarth-git@users.noreply.github.com>
2024-06-13 18:32:52 -0500

    more params for boss_description

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/DescriptionBoss.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/DescriptionBoss.java
@@ -6,6 +6,7 @@ import com.playmonumenta.libraryofsouls.SoulsDatabase;
 import com.playmonumenta.libraryofsouls.bestiary.BestiaryManager;
 import com.playmonumenta.plugins.bosses.SpellManager;
 import com.playmonumenta.plugins.bosses.parameters.BossParam;
+import com.playmonumenta.plugins.events.DamageEvent;
 import com.playmonumenta.plugins.utils.MetadataUtils;
 import com.playmonumenta.plugins.utils.PlayerUtils;
 import java.util.Collections;
@@ -16,6 +17,7 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -30,21 +32,49 @@ public class DescriptionBoss extends BossAbilityGroup {
 		public int RANGE = 20;
 		@BossParam(help = "number of kills needed to stop seeing the description")
 		public int KILL_REQ = 1;
+		@BossParam(help = "whether to send description on spawn (disable to only show description when player hits the mob)")
+		public boolean ON_SPAWN = true;
+		@BossParam(help = "whether to send description on hurt (should always be true)")
+		public boolean ON_HURT = true;
 	}
+
+	private final Parameters mParams;
+	private boolean mSentOnce;
 
 	public DescriptionBoss(Plugin plugin, LivingEntity boss) {
 		super(plugin, identityTag, boss);
-		Parameters p = BossParameters.getParameters(boss, identityTag, new Parameters());
+		mParams = BossParameters.getParameters(boss, identityTag, new Parameters());
+		mSentOnce = false;
 
-		SoulEntry soul = SoulsDatabase.getInstance().getSoul(p.LOS_NAME);
+		if (mParams.ON_SPAWN) {
+			sendDescription();
+		}
+
+		super.constructBoss(SpellManager.EMPTY, Collections.emptyList(), mParams.RANGE, null);
+	}
+
+	@Override
+	public void onHurtByEntity(DamageEvent event, Entity damager) {
+		if (mParams.ON_HURT && damager instanceof Player player && player.getLocation().distance(mBoss.getLocation()) < mParams.RANGE) {
+			sendDescription();
+		}
+	}
+
+	public void sendDescription() {
+		if (mSentOnce) {
+			return;
+		}
+
+		SoulEntry soul = SoulsDatabase.getInstance().getSoul(mParams.LOS_NAME);
 		if (soul != null) {
-			List<Player> players = PlayerUtils.playersInRange(boss.getLocation(), p.RANGE, true);
+			List<Player> players = PlayerUtils.playersInRange(mBoss.getLocation(), mParams.RANGE, true);
 			for (Player player : players) {
-				if (BestiaryManager.getKillsForMob(player, soul) < p.KILL_REQ) {
-					List<Component> description = LibraryOfSoulsAPI.getDescription(p.LOS_NAME);
+				if (BestiaryManager.getKillsForMob(player, soul) < mParams.KILL_REQ) {
+					List<Component> description = LibraryOfSoulsAPI.getDescription(mParams.LOS_NAME);
+					// only once per tick, as we don't want it to send twice if spawner spawns multiple mobs
 					if (description != null && MetadataUtils.checkOnceThisTick(mPlugin, player, "MobDescriptionFor" + player.getName())) {
 						player.sendMessage(Component.text("New mob encountered! ", NamedTextColor.GREEN, TextDecoration.ITALIC)
-							.append(Component.text(boss.getName(), NamedTextColor.GREEN, TextDecoration.BOLD))
+							.append(Component.text(mBoss.getName(), NamedTextColor.GREEN, TextDecoration.BOLD))
 							.append(Component.text(":", NamedTextColor.GREEN)));
 						Component message = Component.empty();
 						Iterator<Component> iter = description.iterator();
@@ -56,11 +86,10 @@ public class DescriptionBoss extends BossAbilityGroup {
 						}
 						player.sendMessage(message);
 						player.playSound(player.getLocation(), Sound.ITEM_BOOK_PAGE_TURN, SoundCategory.MASTER, 1f, 1f);
+						mSentOnce = true;
 					}
 				}
 			}
 		}
-
-		super.constructBoss(SpellManager.EMPTY, Collections.emptyList(), p.RANGE, null);
 	}
 }
