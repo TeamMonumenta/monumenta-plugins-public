35c52ad000172fbc5bb95dbb237353c0ce2915df
Timothy Southwick <NickNackGus@gmail.com>
2024-07-18 22:02:46 -0400

    Audit Log Speedup

--- plugins/paper/src/main/java/com/playmonumenta/plugins/integrations/MonumentaNetworkRelayIntegration.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/integrations/MonumentaNetworkRelayIntegration.java
@@ -2,7 +2,13 @@ package com.playmonumenta.plugins.integrations;
 
 import com.google.gson.JsonObject;
 import com.playmonumenta.networkrelay.NetworkRelayAPI;
+import com.playmonumenta.plugins.Plugin;
+import com.playmonumenta.plugins.utils.MessagingUtils;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.Nullable;
 
 public class MonumentaNetworkRelayIntegration {
@@ -13,9 +19,12 @@ public class MonumentaNetworkRelayIntegration {
 	public static final String AUDIT_LOG_MARKET_CHANNEL = "Monumenta.Automation.MarketAuditLog";
 	public static final String AUDIT_LOG_REPORT_CHANNEL = "Monumenta.Automation.ReportAuditLog";
 	public static final String ADMIN_ALERT_CHANNEL = "Monumenta.Automation.AdminNotification";
+	private static final int CHARS_PER_MESSAGE_GROUP = 1950;
 
 	private final Logger mLogger;
 	private static @Nullable MonumentaNetworkRelayIntegration INSTANCE = null;
+	private Map<String, StringBuilder> mLogBuffer = new HashMap<>();
+	private @Nullable BukkitRunnable mLogBufferRunnable = null;
 
 	public MonumentaNetworkRelayIntegration(Logger logger) {
 		logger.info("Enabling MonumentaNetworkRelay integration");
@@ -23,15 +32,86 @@ public class MonumentaNetworkRelayIntegration {
 		INSTANCE = this;
 	}
 
+	public static void disable() {
+		MonumentaNetworkRelayIntegration instance = INSTANCE;
+		if (instance == null) {
+			return;
+		}
+
+		instance.mLogger.info("Disabling MonumentaNetworkRelay integration");
+
+		if (instance.mLogBufferRunnable != null) {
+			instance.mLogBufferRunnable.cancel();
+			instance.mLogBufferRunnable = null;
+		}
+
+		INSTANCE = null;
+	}
+
 	private static void sendAuditLogMessage(String message, String channel) {
-		if (INSTANCE != null) {
+		MonumentaNetworkRelayIntegration instance = INSTANCE;
+		if (instance != null) {
+			// Ensure we're running sync
+			Bukkit.getScheduler().runTask(Plugin.getInstance(), () -> {
+				StringBuilder builder = instance.mLogBuffer.computeIfAbsent(channel, k -> new StringBuilder());
+
+				if (builder.length() + message.length() + 2 >= CHARS_PER_MESSAGE_GROUP) {
+					sendAuditLogMessageImmediate(builder.toString(), channel);
+					builder.setLength(0);
+				}
+
+				if (!builder.isEmpty()) {
+					builder.append("\n");
+				}
+				builder.append(message);
+
+				if (instance.mLogBufferRunnable == null) {
+					instance.mLogBufferRunnable = new BukkitRunnable() {
+						@Override
+						public void run() {
+							instance.processBuffer();
+						}
+					};
+					instance.mLogBufferRunnable.runTaskTimer(Plugin.getInstance(), 1L, 1L);
+				}
+			});
+		}
+	}
+
+	private void processBuffer() {
+		MonumentaNetworkRelayIntegration instance = INSTANCE;
+		if (instance == null) {
+			return;
+		}
+
+		if (mLogBufferRunnable != null) {
+			mLogBufferRunnable.cancel();
+			mLogBufferRunnable = null;
+		}
+		Map<String, StringBuilder> logBuffer = mLogBuffer;
+		mLogBuffer = new HashMap<>();
+
+		for (Map.Entry<String, StringBuilder> entry : logBuffer.entrySet()) {
+			String channel = entry.getKey();
+			String message = entry.getValue().toString();
+			if (message.isEmpty()) {
+				continue;
+			}
+
+			sendAuditLogMessageImmediate(message, channel);
+		}
+	}
+
+	private static void sendAuditLogMessageImmediate(String message, String channel) {
+		MonumentaNetworkRelayIntegration instance = INSTANCE;
+		if (instance != null) {
 			JsonObject data = new JsonObject();
 			data.addProperty("message", message);
 			try {
 				NetworkRelayAPI.sendMessage("automation-bot", channel, data);
 			} catch (Exception ex) {
-				INSTANCE.mLogger.severe("Failed to send audit log message: " + ex.getMessage());
-				ex.printStackTrace();
+				instance.mLogger.severe("Failed to send audit log message: " + ex.getMessage());
+				MessagingUtils.sendStackTrace(Bukkit.getConsoleSender(), ex);
 			}
 		}
 	}
@@ -57,25 +137,27 @@ public class MonumentaNetworkRelayIntegration {
 	}
 
 	public static void broadcastCommand(String command) {
-		if (INSTANCE != null) {
+		MonumentaNetworkRelayIntegration instance = INSTANCE;
+		if (instance != null) {
 			try {
 				NetworkRelayAPI.sendBroadcastCommand(command);
 			} catch (Exception ex) {
-				INSTANCE.mLogger.severe("Failed to send broadcast message: " + ex.getMessage());
-				ex.printStackTrace();
+				instance.mLogger.severe("Failed to send broadcast message: " + ex.getMessage());
+				MessagingUtils.sendStackTrace(Bukkit.getConsoleSender(), ex);
 			}
 		}
 	}
 
 	public static void sendAdminMessage(String message) {
-		if (INSTANCE != null) {
+		MonumentaNetworkRelayIntegration instance = INSTANCE;
+		if (instance != null) {
 			JsonObject data = new JsonObject();
 			data.addProperty("message", message);
 			try {
 				NetworkRelayAPI.sendMessage("automation-bot", ADMIN_ALERT_CHANNEL, data);
 			} catch (Exception ex) {
-				INSTANCE.mLogger.severe("Failed to send admin alert message: " + ex.getMessage());
-				ex.printStackTrace();
+				instance.mLogger.severe("Failed to send admin alert message: " + ex.getMessage());
+				MessagingUtils.sendStackTrace(Bukkit.getConsoleSender(), ex);
 			}
 		}
 	}
