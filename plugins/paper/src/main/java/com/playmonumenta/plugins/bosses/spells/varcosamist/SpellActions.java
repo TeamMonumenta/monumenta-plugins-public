0ddc9fa6660a910a98134a8f57366bbc8f67c861
Spy21DD <spy21dd@gmail.com>
2024-08-06 11:33:54 -0500

    More Varcosa Anticheese (#4613)
    * More Varcosa Anticheese
    
    Significantly increases the damage players receive for being too high/too low in Black Mist Varcosa's arena and fixes a bug with the knockback
    
    * Update SpellActions.java
    
    It's really hard to counter how much healing players get from R2 gear, so add Antiheal, up damage, and reduce damage tick frequency

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/varcosamist/SpellActions.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/varcosamist/SpellActions.java
@@ -2,6 +2,7 @@ package com.playmonumenta.plugins.bosses.spells.varcosamist;
 
 import com.playmonumenta.plugins.Plugin;
 import com.playmonumenta.plugins.bosses.spells.SpellPlayerAction;
+import com.playmonumenta.plugins.effects.PercentHeal;
 import com.playmonumenta.plugins.utils.BossUtils;
 import com.playmonumenta.plugins.utils.PlayerUtils;
 import com.playmonumenta.scriptedquests.utils.MetadataUtils;
@@ -13,22 +14,13 @@ import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
-
 
 public class SpellActions {
-	//Not technically a spell - just putting the repeated actions in a seperate class
+	// Not technically a spell - just putting the repeated actions in a seperate class
 	public static SpellPlayerAction getTooLowAction(LivingEntity boss, Location center) {
 		return new SpellPlayerAction(boss, 50, (player, tick) -> {
-			if (player.getLocation().getBlock().isLiquid() || (player.getLocation().getBlockY() <= center.getY() - 3 && tick % 20 == 0)) {
-				Vector velocity = player.getVelocity();
-				BossUtils.bossDamagePercent(boss, player, 0.025);
-				player.setVelocity(velocity);
-				// Only show the message every 30s
-				if (!MetadataUtils.happenedInRecentTicks(player, "PlayerIntoDepthsVarcosaMetakey", 600)) {
-					MetadataUtils.checkOnceThisTick(Plugin.getInstance(), player, "PlayerIntoDepthsVarcosaMetakey"); // Mark this tick
-					player.sendMessage(Component.text("Into the depths with ye! The deep gods take yer soul!", NamedTextColor.RED));
-				}
+			if (tick % 10 == 0 && (player.getLocation().getBlock().isLiquid() || player.getLocation().getBlockY() <= center.getY() - 3)) {
+				damagePlayer(boss, player);
 			}
 		});
 	}
@@ -38,22 +30,17 @@ public class SpellActions {
 		return new SpellPlayerAction(boss, 50, (player, tick) -> {
 			double playerY = player.getLocation().getY();
 
-			// If standing on heightened ground or going up a climbable to cheese,
-			// enforce stricter 2-block threshold.
-			// Eg disallow pillaring up, like Kaul's Lightning Storm
+			// Assume player is standing on ground/cheese pillar or a climbable block
 			int yThreshold = 2;
 
 			// If truly in the air (not on ground and not climbing - like the Thunder Step requirement),
-			// have relaxed 5-block threshold.
-			// Eg riptide, recoil, jump boost, knocked upwards
+			// use a relaxed 5-block threshold
 			if (PlayerUtils.isFreeFalling(player)) {
 				yThreshold = 5;
 			}
 
-			if (playerY >= arenaSurfaceY + yThreshold) {
-				Vector previousVelocity = player.getVelocity();
-				BossUtils.bossDamagePercent(boss, player, 0.025);
-				player.setVelocity(previousVelocity);
+			if (tick % 10 == 0 && playerY >= arenaSurfaceY + yThreshold) {
+				damagePlayer(boss, player);
 			}
 		});
 	}
@@ -73,5 +60,18 @@ public class SpellActions {
 				}
 			}
 		};
+	}
+
+	private static void damagePlayer(LivingEntity boss, Player player) {
+		final String ANTIHEAL_SRC = "VarcosaOOBAntiHeal";
+
+		// Deals flat and percent damage to prevent players from surviving for long periods
+		Plugin.getInstance().mEffectManager.addEffect(player, ANTIHEAL_SRC, new PercentHeal(3 * 20, -1.0));
+		BossUtils.bossDamagePercent(boss, player, 0.1, null, false, null, false);
+		BossUtils.bossDamagePercent(boss, player, 2.0, null, true, null, false);
+		if (!MetadataUtils.happenedInRecentTicks(player, "PlayerIntoDepthsVarcosaMetakey", 600)) {
+			MetadataUtils.checkOnceThisTick(Plugin.getInstance(), player, "PlayerIntoDepthsVarcosaMetakey"); // Mark this tick
+			player.sendMessage(Component.text("Into the depths with ye! The deep gods take yer soul!", NamedTextColor.RED));
+		}
 	}
 }
