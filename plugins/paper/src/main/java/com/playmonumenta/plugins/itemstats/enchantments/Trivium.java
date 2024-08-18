126ebffd69ec0c1e6ead051af1ba8d6a5a3c1b8c
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2024-08-18 01:40:30 -0700

    fix exception in trivium (#4698)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/enchantments/Trivium.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/itemstats/enchantments/Trivium.java
@@ -9,6 +9,7 @@ import com.playmonumenta.plugins.utils.DamageUtils;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Sound;
@@ -22,7 +23,7 @@ public class Trivium implements Enchantment {
 
 	private final double DAMAGE_PER_LEVEL = 0.1;
 
-	private HashMap<Player, HashMap<ClassAbility, List<DamageEvent>>> mDamageInTick = new HashMap<>();
+	private final Map<Player, TriviumInstance> mDamageInTick = new HashMap<>();
 	private @Nullable BukkitTask mRunDamageTask = null;
 
 	@Override
@@ -50,47 +51,64 @@ public class Trivium implements Enchantment {
 			if (ca == ClassAbility.ARCANE_STRIKE_ENHANCED) {
 				ca = ClassAbility.ARCANE_STRIKE;
 			}
-			mDamageInTick.computeIfAbsent(player, key -> new HashMap<>()).computeIfAbsent(ca, key -> new ArrayList<>()).add(event);
+			mDamageInTick.computeIfAbsent(player, key -> new TriviumInstance(value)).addEvent(ca, event);
 
 			if (mRunDamageTask == null || !Bukkit.getScheduler().isQueued(mRunDamageTask.getTaskId())) {
-				mRunDamageTask = Bukkit.getScheduler().runTask(plugin, () -> {
-					HashMap<Player, HashMap<ClassAbility, List<DamageEvent>>> damageInTick = mDamageInTick;
-					mDamageInTick = new HashMap<>();
-					damageInTick.forEach((p, map) -> {
-						map.forEach((ability, eventList) -> {
-							if (eventList.size() >= 3) {
-								// If this tick had more than 3 of the same damage event of the same ability...
-								// Proc Trivium, damage entity for 10% more gear magic damage per level.
-								Location loc = player.getLocation().zero();
-								for (DamageEvent e : eventList) {
-									loc.add(e.getDamagee().getLocation());
-									double gearDamageMultiplier = e.getGearDamageMultiplier();
-									double multiplierWithTrivium = gearDamageMultiplier + (DAMAGE_PER_LEVEL * value);
-									double multiplier = multiplierWithTrivium / gearDamageMultiplier - 1;
-									DamageUtils.damage(p, e.getDamagee(), DamageEvent.DamageType.TRUE, e.getDamage() * multiplier, null, true, false);
-								}
-								// Find the average location of all entities hit
-								loc.multiply((double) 1 / eventList.size());
-								p.playSound(
-									loc,
-									Sound.ENTITY_ILLUSIONER_CAST_SPELL,
-									SoundCategory.PLAYERS,
-									0.8f,
-									2f
-								);
-								p.playSound(
-									loc,
-									Sound.ENTITY_BLAZE_SHOOT,
-									SoundCategory.PLAYERS,
-									0.8f,
-									2f
-								);
-							}
-						});
-					});
-					mRunDamageTask = null;
-				});
+				mRunDamageTask = Bukkit.getScheduler().runTask(plugin, this::task);
 			}
+		}
+	}
+
+	private void task() {
+		mDamageInTick.forEach((p, instance) -> dealDamage(p, instance.mMap, instance.mValue));
+		mDamageInTick.clear();
+		mRunDamageTask = null;
+	}
+
+	private void dealDamage(Player p, Map<ClassAbility, List<DamageEvent>> map, double value) {
+		map.forEach((ability, eventList) -> {
+			eventList = eventList.stream().filter(e -> e.getDamagee().getWorld() == p.getWorld()).toList();
+			if (eventList.size() >= 3) {
+				// If this tick had more than 3 of the same damage event of the same ability...
+				// Proc Trivium, damage entity for 10% more gear magic damage per level.
+				Location loc = p.getLocation().zero();
+				for (DamageEvent e : eventList) {
+					loc.add(e.getDamagee().getLocation());
+					double gearDamageMultiplier = e.getGearDamageMultiplier();
+					double multiplierWithTrivium = gearDamageMultiplier + (DAMAGE_PER_LEVEL * value);
+					double multiplier = multiplierWithTrivium / gearDamageMultiplier - 1;
+					DamageUtils.damage(p, e.getDamagee(), DamageEvent.DamageType.TRUE, e.getDamage() * multiplier, null, true, false);
+				}
+				// Find the average location of all entities hit
+				loc.multiply((double) 1 / eventList.size());
+				p.playSound(
+					loc,
+					Sound.ENTITY_ILLUSIONER_CAST_SPELL,
+					SoundCategory.PLAYERS,
+					0.8f,
+					2f
+				);
+				p.playSound(
+					loc,
+					Sound.ENTITY_BLAZE_SHOOT,
+					SoundCategory.PLAYERS,
+					0.8f,
+					2f
+				);
+			}
+		});
+	}
+
+	private static class TriviumInstance {
+		private final double mValue;
+		private final Map<ClassAbility, List<DamageEvent>> mMap = new HashMap<>();
+
+		private TriviumInstance(double value) {
+			mValue = value;
+		}
+
+		private void addEvent(ClassAbility ca, DamageEvent event) {
+			mMap.computeIfAbsent(ca, key -> new ArrayList<>()).add(event);
 		}
 	}
 }
