d184d5e603872bc979ef58c4ee017f2224200105
Peter Güttinger <njol@gmx.ch>
2024-08-21 10:25:54 +0200

    Quieter Arcane Enhancement without losing feedback for caster (#4704)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/cosmetics/skills/alchemist/harbinger/ArcaneEnhancementsCS.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/cosmetics/skills/alchemist/harbinger/ArcaneEnhancementsCS.java
@@ -9,6 +9,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -39,9 +40,31 @@ public class ArcaneEnhancementsCS extends EsotericEnhancementsCS {
 		return "Arcane Aberration";
 	}
 
+	/**
+	 * Plays a sound from the aberration's location while making sure the caster always hears it (as long as they can see the creeper)
+	 */
+	private static void playSound(Player player, LivingEntity aberration, float volume, float pitch) {
+		if (player.getWorld() == aberration.getWorld() && player.getLocation().distanceSquared(aberration.getLocation()) > 5 * 5) {
+			// if far from the caster, play normal sound for other players and a closer one for the caster so that they can always hear it
+			for (Player p : aberration.getTrackedPlayers()) {
+				if (p != player) {
+					// Normal sound for other players
+					p.playSound(aberration.getLocation(), Sound.ENTITY_ALLAY_DEATH, SoundCategory.PLAYERS, volume, pitch);
+				} else {
+					// Play sound at a point 5 meters from the caster in the direction of the aberration
+					Location soundLoc = player.getLocation().add(aberration.getLocation().toVector().subtract(player.getLocation().toVector()).normalize().multiply(5));
+					player.playSound(soundLoc, Sound.ENTITY_ALLAY_DEATH, SoundCategory.PLAYERS, volume, pitch);
+				}
+			}
+		} else {
+			// If close just play the sound normally
+			aberration.getWorld().playSound(aberration.getLocation(), Sound.ENTITY_ALLAY_DEATH, SoundCategory.PLAYERS, volume, pitch);
+		}
+	}
+
 	@Override
 	public void esotericSummonEffect(Player player, Creeper aberration) {
-		aberration.getWorld().playSound(aberration.getLocation(), Sound.ENTITY_ALLAY_DEATH, 1.5f, 0.8f);
+		playSound(player, aberration, 0.75f, 0.8f);
 
 		// only few particles, spawning an aberration requires 2 potions or an amalgam, so there's lots of particles already
 		new PartialParticle(Particle.SOUL, aberration.getEyeLocation().add(0, -0.2, 0), 10)
@@ -64,7 +87,7 @@ public class ArcaneEnhancementsCS extends EsotericEnhancementsCS {
 
 	@Override
 	public void explosionEffects(Player player, LivingEntity aberration, double radius) {
-		aberration.getWorld().playSound(aberration.getLocation(), Sound.ENTITY_ALLAY_DEATH, 1.5f, 1.3f);
+		playSound(player, aberration, 0.9f, 1.3f);
 
 		Location loc = aberration.getLocation().add(0, 0.25, 0);
 		ArcanePotionsCS.drawSingleCircle(player, loc, radius, null, Particle.SCRAPE);
@@ -81,7 +104,7 @@ public class ArcaneEnhancementsCS extends EsotericEnhancementsCS {
 
 	@Override
 	public void expireEffects(Player player, LivingEntity aberration) {
-		aberration.getWorld().playSound(aberration.getLocation(), Sound.ENTITY_ALLAY_DEATH, 1, 0.5f);
+		playSound(player, aberration, 0.75f, 0.5f);
 	}
 
 }
