6446498a03f86c674c2cf2923c57cdbc6a8b9f33
KickingCoyote <151552160+KickingCoyote@users.noreply.github.com>
2024-08-22 23:26:16 +0200

    Added spawn velocity params to DeathSummonBoss (#4702)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/DeathSummonBoss.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/DeathSummonBoss.java
@@ -6,6 +6,7 @@ import com.playmonumenta.plugins.bosses.parameters.LoSPool;
 import com.playmonumenta.plugins.bosses.parameters.ParticlesList;
 import com.playmonumenta.plugins.bosses.parameters.SoundsList;
 import java.util.Collections;
+import java.util.Random;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
@@ -13,6 +14,7 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Mob;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.Nullable;
 
 public class DeathSummonBoss extends BossAbilityGroup {
@@ -38,6 +40,18 @@ public class DeathSummonBoss extends BossAbilityGroup {
 		@BossParam(help = "Number of mobs summoned")
 		public int MOB_COUNT = 1;
 
+		@BossParam(help = "Minimum horizontal spawn velocity")
+		public double H_MIN_VELOCITY = 0;
+
+		@BossParam(help = "Maximum horizontal spawn velocity")
+		public double H_MAX_VELOCITY = 0;
+
+		@BossParam(help = "Minimum vertical spawn velocity")
+		public double V_MIN_VELOCITY = 0;
+
+		@BossParam(help = "Maximum vertical spawn velocity")
+		public double V_MAX_VELOCITY = 0;
+
 	}
 
 	private final Parameters mParam;
@@ -54,8 +68,19 @@ public class DeathSummonBoss extends BossAbilityGroup {
 		Location loc = mBoss.getLocation();
 		mParam.PARTICLES.spawn(mBoss, loc.clone().add(0, 0.5, 0));
 		mParam.SOUNDS.play(loc);
+		Random r = new Random();
 		for (int i = 0; i < mParam.MOB_COUNT; i++) {
 			Entity entity = mParam.POOL.spawn(loc);
+
+			//Applies a random velocity inside the given constraints to each entity when spawning
+			//The velocity will be applied after the AI is returned as mobs with noAI are unaffected by velocity
+			if (entity != null && (mParam.H_MAX_VELOCITY != 0 || mParam.H_MIN_VELOCITY != 0 || mParam.V_MIN_VELOCITY != 0 || mParam.V_MAX_VELOCITY != 0)) {
+				entity.setVelocity(new Vector(
+					r.nextDouble() * (mParam.H_MAX_VELOCITY - mParam.H_MIN_VELOCITY) + mParam.H_MIN_VELOCITY,
+					r.nextDouble() * (mParam.V_MAX_VELOCITY - mParam.V_MIN_VELOCITY) + mParam.V_MIN_VELOCITY,
+					r.nextDouble() * (mParam.H_MAX_VELOCITY - mParam.H_MIN_VELOCITY) + mParam.H_MIN_VELOCITY
+				));
+			}
 			if (entity instanceof LivingEntity livingEntity) {
 				livingEntity.setRotation(loc.getYaw(), loc.getPitch());
 				livingEntity.setAI(false);
