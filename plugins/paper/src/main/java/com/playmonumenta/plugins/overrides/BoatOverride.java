b2a44dfd09ebb4295ec826d3226c8e50484cc0bc
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2024-09-12 20:14:37 -0500

    Fix no placing boats zone not considering dispensers (#4782)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/overrides/BoatOverride.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/overrides/BoatOverride.java
@@ -5,6 +5,7 @@ import com.playmonumenta.plugins.utils.LocationUtils;
 import com.playmonumenta.plugins.utils.ZoneUtils;
 import org.bukkit.FluidCollisionMode;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Boat;
 import org.bukkit.entity.Player;
@@ -25,17 +26,31 @@ public class BoatOverride extends BaseOverride {
 			return false;
 		}
 
+		Location loc = block.getLocation();
 		if (ZoneUtils.hasZoneProperty(player, ZoneUtils.ZoneProperty.NO_PLACING_BOATS)
-				|| ZoneUtils.hasZoneProperty(block.getLocation(), ZoneUtils.ZoneProperty.NO_PLACING_BOATS)) {
+				|| ZoneUtils.hasZoneProperty(loc, ZoneUtils.ZoneProperty.NO_PLACING_BOATS)) {
 			return false;
 		}
 
 		// Must place boats on water or ice
-		if (!LocationUtils.isValidBoatLocation(block.getLocation())) {
+		if (!LocationUtils.isValidBoatLocation(loc)) {
 			return false;
 		}
 
 		// Must not spam boats
-		return block.getWorld().getNearbyEntitiesByType(Boat.class, block.getLocation(), 10).size() <= 7;
+		return !tooManyBoatsNearby(loc);
+	}
+
+	@Override
+	public boolean blockDispenseInteraction(Plugin plugin, Block block, ItemStack dispensed) {
+		Location loc = block.getLocation();
+		if (ZoneUtils.hasZoneProperty(loc, ZoneUtils.ZoneProperty.NO_PLACING_BOATS)) {
+			return false;
+		}
+		return !tooManyBoatsNearby(loc);
+	}
+
+	private boolean tooManyBoatsNearby(Location loc) {
+		return loc.getWorld().getNearbyEntitiesByType(Boat.class, loc, 10).size() > 7;
 	}
 }
