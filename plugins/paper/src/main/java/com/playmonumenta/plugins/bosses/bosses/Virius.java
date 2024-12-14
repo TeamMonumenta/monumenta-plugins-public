80192265a52115b048a4b9e4f5fa20cd88d37d77
Spy21DD <spy21dd@gmail.com>
2024-12-14 09:52:20 -0600

    Virius Revamp

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/Virius.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/Virius.java
@@ -1,86 +1,145 @@
 package com.playmonumenta.plugins.bosses.bosses;
 
+import com.playmonumenta.plugins.Constants;
 import com.playmonumenta.plugins.bosses.BossBarManager;
 import com.playmonumenta.plugins.bosses.SpellManager;
 import com.playmonumenta.plugins.bosses.spells.SpellBaseLaser;
+import com.playmonumenta.plugins.bosses.spells.SpellBaseSeekingProjectile;
 import com.playmonumenta.plugins.bosses.spells.SpellChangeFloor;
 import com.playmonumenta.plugins.effects.BaseMovementSpeedModifyEffect;
+import com.playmonumenta.plugins.events.DamageEvent;
 import com.playmonumenta.plugins.events.DamageEvent.DamageType;
+import com.playmonumenta.plugins.managers.GlowingManager;
 import com.playmonumenta.plugins.particle.PartialParticle;
 import com.playmonumenta.plugins.utils.BossUtils;
+import com.playmonumenta.plugins.utils.DamageUtils;
 import com.playmonumenta.plugins.utils.EntityUtils;
+import com.playmonumenta.plugins.utils.MovementUtils;
+import com.playmonumenta.plugins.utils.PlayerUtils;
 import java.util.Arrays;
 import java.util.Collections;
 import net.kyori.adventure.bossbar.BossBar;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
+import org.bukkit.World;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.Nullable;
 
-public class Virius extends SerializedLocationBossAbilityGroup {
+public final class Virius extends SerializedLocationBossAbilityGroup {
 	public static final String identityTag = "boss_virius";
-	public static final int detectionRange = 50;
+	public static final int detectionRange = 30;
 
-	public Virius(Plugin plugin, LivingEntity boss, Location spawnLoc, Location endLoc) {
+	private static final int BASE_HEALTH = 300;
+	private static final double SCALING_X = 0.4;
+	private static final double SCALING_Y = 0.3;
+
+	private final double mDefenseScaling;
+
+	public Virius(final Plugin plugin, final LivingEntity boss, final Location spawnLoc, final Location endLoc) {
 		super(plugin, identityTag, boss, spawnLoc, endLoc);
-		mBoss.setRemoveWhenFarAway(false);
 
-		mBoss.addScoreboardTag("Boss");
+		final int mPlayerCount = PlayerUtils.playersInRange(mSpawnLoc, detectionRange, true).size();
+		mDefenseScaling = BossUtils.healthScalingCoef(mPlayerCount, SCALING_X, SCALING_Y);
 
-		SpellManager activeSpells = new SpellManager(Arrays.asList(
-			new SpellChangeFloor(plugin, mBoss, mSpawnLoc, detectionRange, 3, Material.MAGMA_BLOCK, 800),
-			new SpellBaseLaser(plugin, boss, detectionRange, 100, false, false, 160,
-				// Tick action per player
-				(LivingEntity player, int ticks, boolean blocked) -> {
+		final SpellManager activeSpells = new SpellManager(Arrays.asList(
+			new SpellChangeFloor(mPlugin, mBoss, mSpawnLoc, detectionRange, 3, Material.MAGMA_BLOCK,
+				Constants.TICKS_PER_SECOND * 20),
+			new SpellBaseSeekingProjectile(mPlugin, mBoss, false, Constants.TICKS_PER_SECOND * 6,
+				(int) (Constants.TICKS_PER_SECOND * 1.5), 0.6, 0, Constants.TICKS_PER_SECOND * 3,
+				0.5, true, true, 0, true,
+				/* Targets */
+				() -> PlayerUtils.playersInRange(mBoss.getLocation(), detectionRange, false),
+				/* Init aesthetics */
+				(final World world, final Location loc, final int ticks) -> {
+					world.playSound(loc, Sound.BLOCK_ENCHANTMENT_TABLE_USE, SoundCategory.HOSTILE, 1, 1);
+					GlowingManager.startGlowing(mBoss, NamedTextColor.RED, (int) (Constants.TICKS_PER_SECOND * 1.5), GlowingManager.BOSS_SPELL_PRIORITY);
+				},
+				/* Launch aesthetic */
+				(final World world, final Location loc, final int ticks) ->
+					world.playSound(loc, Sound.ENTITY_FIREWORK_ROCKET_LAUNCH, SoundCategory.HOSTILE, 1, 1.5f),
+				/* Projectile aesthetics */
+				(final World world, final Location loc, final int ticks) -> {
+					new PartialParticle(Particle.SPELL_INSTANT, loc, 3, 0.05, 0.05, 0.05, 0.03).spawnAsEntityActive(mBoss);
+					if (ticks % (Constants.TICKS_PER_SECOND * 2) == 0) {
+						world.playSound(loc, Sound.ENTITY_BLAZE_BURN, SoundCategory.HOSTILE, 0.5f, 0.5f);
+					}
+				},
+				/* Hit action */
+				(final World world, final @Nullable LivingEntity target, final Location loc, final @Nullable Location prevLoc) -> {
+					if (target != null) {
+						DamageUtils.damage(mBoss, target, new DamageEvent.Metadata(DamageType.MAGIC, null, null,
+							"Hasty Bolt"), 7, false, true, true);
+					}
+					new PartialParticle(Particle.SOUL_FIRE_FLAME, loc, 20, 0, 0, 0, 0.2).spawnAsEntityActive(mBoss);
+					world.playSound(loc, Sound.ENTITY_GENERIC_EXPLODE, SoundCategory.HOSTILE, 0.5f, 0.5f);
+				}),
+			new SpellBaseLaser(mPlugin, mBoss, detectionRange, Constants.TICKS_PER_SECOND * 3, false,
+				false, Constants.TICKS_PER_SECOND * 8,
+				/* Tick action per player */
+				(final LivingEntity player, final int ticks, final boolean blocked) -> {
 					player.getWorld().playSound(player.getLocation(), Sound.UI_TOAST_IN, SoundCategory.HOSTILE, 2, 0.5f + (ticks / 80f) * 1.5f);
-					boss.getLocation().getWorld().playSound(boss.getLocation(), Sound.UI_TOAST_IN, SoundCategory.HOSTILE, 2, 0.5f + (ticks / 80f) * 1.5f);
+					mBoss.getLocation().getWorld().playSound(mBoss.getLocation(), Sound.UI_TOAST_IN, SoundCategory.HOSTILE, 2, 0.5f + (ticks / 80f) * 1.5f);
 					if (ticks == 0) {
-						com.playmonumenta.plugins.Plugin.getInstance().mEffectManager.addEffect(boss, BaseMovementSpeedModifyEffect.GENERIC_NAME,
+						com.playmonumenta.plugins.Plugin.getInstance().mEffectManager.addEffect(mBoss, BaseMovementSpeedModifyEffect.GENERIC_NAME,
 							new BaseMovementSpeedModifyEffect(110, -0.75));
 					}
 				},
-				// Particles generated by the laser
-				(Location loc) -> {
-					new PartialParticle(Particle.CLOUD, loc, 1, 0.02, 0.02, 0.02, 0).spawnAsEntityActive(boss);
-					new PartialParticle(Particle.WATER_SPLASH, loc, 1, 0.04, 0.04, 0.04, 1).spawnAsEntityActive(boss);
+				/* Laser particles */
+				(final Location loc) -> {
+					new PartialParticle(Particle.CLOUD, loc, 1, 0.02, 0.02, 0.02, 0).spawnAsEntityActive(mBoss);
+					new PartialParticle(Particle.WATER_SPLASH, loc, 1, 0.04, 0.04, 0.04, 1).spawnAsEntityActive(mBoss);
 				},
-				// Damage generated at the end of the attack
-				(LivingEntity player, Location loc, boolean blocked) -> {
+				/* Hit action */
+				(final LivingEntity player, final Location loc, final boolean blocked) -> {
 					loc.getWorld().playSound(loc, Sound.ENTITY_DRAGON_FIREBALL_EXPLODE, SoundCategory.HOSTILE, 1f, 1.5f);
-					new PartialParticle(Particle.WATER_WAKE, loc, 300, 0.8, 0.8, 0.8, 0).spawnAsEntityActive(boss);
+					new PartialParticle(Particle.WATER_WAKE, loc, 20, 0.4, 0.4, 0.4, 0.02).spawnAsEntityActive(mBoss);
 					if (!blocked) {
-						BossUtils.blockableDamage(boss, player, DamageType.MAGIC, 20);
+						DamageUtils.damage(mBoss, player, new DamageEvent.Metadata(DamageType.MAGIC, null, null,
+							"Memory Laser"), 16, false, true, true);
 					}
 				})
 		));
 
-		BossBarManager bossBar = new BossBarManager(mBoss, detectionRange, BossBar.Color.RED, BossBar.Overlay.PROGRESS, null);
-
-		super.constructBoss(activeSpells, Collections.emptyList(), detectionRange, bossBar);
+		final BossBarManager bossBar = new BossBarManager(mBoss, detectionRange, BossBar.Color.RED, BossBar.Overlay.PROGRESS, null);
+		constructBoss(activeSpells, Collections.emptyList(), detectionRange, bossBar);
 	}
 
 	@Override
 	public void init() {
-		int bossTargetHp = 0;
-		int playerCount = BossUtils.getPlayersInRangeForHealthScaling(mBoss, detectionRange);
-		int hpDelta = 256;
-		while (playerCount > 0) {
-			bossTargetHp = bossTargetHp + hpDelta;
-			hpDelta = hpDelta / 2;
-			playerCount--;
-		}
-		EntityUtils.setAttributeBase(mBoss, Attribute.GENERIC_MAX_HEALTH, bossTargetHp);
-		mBoss.setHealth(bossTargetHp);
+		EntityUtils.setAttributeBase(mBoss, Attribute.GENERIC_MOVEMENT_SPEED, 0.25);
+		EntityUtils.setAttributeBase(mBoss, Attribute.GENERIC_KNOCKBACK_RESISTANCE, 0.3);
+		EntityUtils.setAttributeBase(mBoss, Attribute.GENERIC_MAX_HEALTH, BASE_HEALTH);
+
+		mBoss.addScoreboardTag("Boss");
+		mBoss.setRemoveWhenFarAway(false);
+		mBoss.setInvulnerable(false);
+		mBoss.setAI(true);
+		mBoss.setHealth(BASE_HEALTH);
 	}
 
 	@Override
-	public void death(@Nullable EntityDeathEvent event) {
+	public void death(final @Nullable EntityDeathEvent event) {
 		mEndLoc.getBlock().setType(Material.REDSTONE_BLOCK);
+	}
+
+	@Override
+	public void onHurt(final DamageEvent event) {
+		event.setDamage(event.getFlatDamage() / mDefenseScaling);
+	}
+
+	@Override
+	public void onDamage(final DamageEvent event, final LivingEntity damagee) {
+		if (event.getType() == DamageType.MELEE) {
+			MovementUtils.knockAway(mBoss.getLocation(), damagee, 1.0f, 0.5f, true);
+			EntityUtils.applyFire(com.playmonumenta.plugins.Plugin.getInstance(), Constants.TICKS_PER_SECOND * 4,
+				damagee, mBoss);
+		}
 	}
 }
--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/SpellChangeFloor.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/SpellChangeFloor.java
@@ -1,17 +1,18 @@
 package com.playmonumenta.plugins.bosses.spells;
 
+import com.playmonumenta.plugins.Constants;
+import com.playmonumenta.plugins.bosses.TemporaryBlockChangeManager;
 import com.playmonumenta.plugins.particle.PartialParticle;
 import com.playmonumenta.plugins.utils.FastUtils;
 import com.playmonumenta.plugins.utils.PlayerUtils;
-import java.util.ArrayList;
-import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.List;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
-import org.bukkit.block.BlockState;
+import org.bukkit.block.Block;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -19,44 +20,29 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class SpellChangeFloor extends Spell {
-	private Plugin mPlugin;
-	private LivingEntity mBoss;
-	private Location mCenterLoc;
-	private int mRange;
-	private int mRadius;
-	private Material mMaterial;
-	private int mFloorDuration;
+public final class SpellChangeFloor extends Spell {
+	private final Plugin mPlugin;
+	private final LivingEntity mBoss;
+	private final Location mCenterLoc;
+	private final int mRange;
+	private final int mRadius;
+	private final Material mMaterial;
+	private final int mFloorDuration;
 
-	private final EnumSet<Material> mIgnoredMats = EnumSet.of(
-		Material.AIR,
-		Material.CAVE_AIR,
-		Material.VOID_AIR,
-		Material.COMMAND_BLOCK,
-		Material.CHAIN_COMMAND_BLOCK,
-		Material.REPEATING_COMMAND_BLOCK,
-		Material.BARRIER,
-		Material.BEDROCK,
-		Material.OBSIDIAN,
-		Material.CHEST,
-		Material.SPAWNER
-	);
-
-
-	public SpellChangeFloor(Plugin plugin, LivingEntity launcher, Location centerLoc, int range, int radius, Material material, int floorduration) {
+	public SpellChangeFloor(final Plugin plugin, final LivingEntity launcher, final Location centerLoc, final int range,
+							final int radius, final Material material, final int floorduration) {
 		mPlugin = plugin;
 		mBoss = launcher;
 		mCenterLoc = centerLoc;
 		mRange = range;
 		mRadius = radius;
 		mMaterial = material;
-		mIgnoredMats.add(material);
 		mFloorDuration = floorduration;
 	}
 
 	@Override
 	public void run() {
-		List<Player> players = PlayerUtils.playersInRange(mCenterLoc, mRange, true);
+		final List<Player> players = PlayerUtils.playersInRange(mCenterLoc, mRange, true);
 		if (!players.isEmpty()) {
 			launch(players.get(FastUtils.RANDOM.nextInt(players.size())));
 		}
@@ -64,73 +50,48 @@ public class SpellChangeFloor extends Spell {
 
 	@Override
 	public int cooldownTicks() {
-		return 160; // 8 seconds
+		return Constants.TICKS_PER_SECOND * 8;
 	}
 
-	public void launch(Player target) {
-		/*
-		 * First phase - play sound effect
-		 * Second phase - convert top layer of ground under player to mMaterial, particles
-		 * Third phase - cleanup converted blocks
-		 */
-		final int PHASE1_TICKS = 60;
-		final int PHASE2_TICKS = mFloorDuration;
+	private void launch(final Player target) {
+		final HashSet<Block> changedBlocks = new HashSet<>();
 
-		BukkitRunnable runnable = new BukkitRunnable() {
+		target.playSound(target.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, SoundCategory.HOSTILE, 1f, 4f);
+		mBoss.getLocation().getWorld().playSound(mBoss.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, SoundCategory.HOSTILE, 1f, 5f);
+		new PartialParticle(Particle.LAVA, mBoss.getLocation(), 1, 0.8, 0.8, 0.8, 0).spawnAsEntityActive(mBoss);
+		mBoss.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 30, 3));
+
+		// Get a list of blocks that should be changed
+		for (int dx = -mRadius; dx < mRadius; dx++) {
+			for (int dy = -mRadius; dy < mRadius; dy++) {
+				for (int dz = -mRadius; dz < mRadius; dz++) {
+					final Block block = target.getLocation().add(dx, dy, dz).getBlock();
+					if (FastUtils.RANDOM.nextInt(16) > 6) {
+						changedBlocks.add(block);
+					}
+				}
+			}
+		}
+
+		final BukkitRunnable runnable = new BukkitRunnable() {
 			int mTicks = 0;
-			List<BlockState> mBlocksToRestore = new ArrayList<BlockState>();
 
 			@Override
 			public void run() {
-				if (mTicks > 0 && mTicks < PHASE2_TICKS) {
+				mTicks++;
+				if (mTicks < mFloorDuration) {
 					// Particles over the changed blocks
-					for (BlockState state : mBlocksToRestore) {
-						Location loc = state.getLocation().add(0.5f, 1f, 0.5f);
+					for (final Block block : changedBlocks) {
+						final Location loc = block.getLocation().add(0.5f, 1f, 0.5f);
 						new PartialParticle(Particle.DRAGON_BREATH, loc, 1, 0.3, 0.3, 0.3, 0).spawnAsEntityActive(mBoss);
 					}
+
+					return;
 				}
 
-				if (mTicks == 0) {
-					target.playSound(target.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, SoundCategory.HOSTILE, 1f, 4f);
-					mBoss.getLocation().getWorld().playSound(mBoss.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, SoundCategory.HOSTILE, 1f, 5f);
-					new PartialParticle(Particle.LAVA, mBoss.getLocation(), 1, 0.8, 0.8, 0.8, 0).spawnAsEntityActive(mBoss);
-					mBoss.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 30, 3));
-
-					// Get a list of blocks that should be changed
-					for (int dx = -mRadius; dx < mRadius; dx++) {
-						for (int dy = -mRadius; dy < mRadius; dy++) {
-							for (int dz = -mRadius; dz < mRadius; dz++) {
-								BlockState state = target.getLocation().add(dx, dy, dz).getBlock().getState();
-								if (!mIgnoredMats.contains(state.getType()) &&
-									    !state.getType().isInteractable() &&
-									    FastUtils.RANDOM.nextInt(16) > 6) {
-									mBlocksToRestore.add(state);
-								}
-							}
-						}
-					}
-				} else if (mTicks == PHASE1_TICKS) {
-					// Set the blocks to the specified material
-					for (BlockState state : mBlocksToRestore) {
-						state.getLocation().getBlock().setType(mMaterial);
-					}
-
-					/*
-					 * This is weird! But on purpose. The change floor spell can only be
-					 * cancelled before it changes the floor blocks. Once it changes them,
-					 * it must run until it changes the blocks back to what they should be
-					 */
-					mActiveRunnables.remove(this);
-				} else if (mTicks == PHASE2_TICKS) {
-					// Restore the block states saved earlier
-					for (BlockState state : mBlocksToRestore) {
-						state.update(true);
-					}
-				} else if (mTicks > PHASE2_TICKS) {
-					this.cancel();
-					// Don't need to remove runnable here, already done
-				}
-				mTicks++;
+				// Set the blocks to the specified material
+				changedBlocks.forEach(block -> TemporaryBlockChangeManager.INSTANCE.changeBlock(block, mMaterial, mFloorDuration));
+				this.cancel();
 			}
 		};
 
