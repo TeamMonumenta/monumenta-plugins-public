23eea3a5535ed1466cf0f9e77ec0b2c04c06b2cc
Spy21DD <spy21dd@gmail.com>
2025-01-01 16:37:56 -0600

    Actually fix the bug this time (#5007)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/SpellChangeFloor.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/SpellChangeFloor.java
@@ -32,14 +32,14 @@ public final class SpellChangeFloor extends Spell {
 	private final int mFloorDuration;
 
 	public SpellChangeFloor(final Plugin plugin, final LivingEntity launcher, final Location centerLoc, final int range,
-							final int radius, final Material material, final int floorduration) {
+							final int radius, final Material material, final int floorDuration) {
 		mPlugin = plugin;
 		mBoss = launcher;
 		mCenterLoc = centerLoc;
 		mRange = range;
 		mRadius = radius;
 		mMaterial = material;
-		mFloorDuration = floorduration;
+		mFloorDuration = floorDuration;
 	}
 
 	@Override
@@ -58,20 +58,19 @@ public final class SpellChangeFloor extends Spell {
 	private void launch(final Location targetLoc) {
 		final HashSet<Block> changedBlocks = new HashSet<>();
 
-		mBoss.getWorld().playSound(targetLoc, Sound.ENTITY_ENDER_DRAGON_GROWL, SoundCategory.HOSTILE, 1f, 2f);
-		mBoss.getWorld().playSound(mBoss.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, SoundCategory.HOSTILE, 1f, 2f);
-		new PartialParticle(Particle.LAVA, mBoss.getLocation(), 1, 0.8, 0.8, 0.8, 0).spawnAsEntityActive(mBoss);
 		com.playmonumenta.plugins.Plugin.getInstance().mEffectManager.addEffect(mBoss, SLOWNESS_SRC,
 			new BaseMovementSpeedModifyEffect(30, -0.6));
+		mBoss.getWorld().playSound(targetLoc, Sound.ENTITY_ENDER_DRAGON_GROWL, SoundCategory.HOSTILE, 1f, 2f);
+		mBoss.getWorld().playSound(mBoss.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, SoundCategory.HOSTILE, 1f, 2f);
+		new PartialParticle(Particle.LAVA, mBoss.getLocation(), 5, 0.8, 0.8, 0.8, 0).spawnAsEntityActive(mBoss);
 
-		// Get a list of blocks that should be changed
 		final Location blockLoc = targetLoc.clone();
 		for (double dx = targetLoc.getX() - mRadius; dx <= targetLoc.getX() + mRadius; dx++) {
-			for (double dy = targetLoc.getX() - mRadius; dy <= targetLoc.getX() + mRadius; dy++) {
-				for (double dz = targetLoc.getX() - mRadius; dz <= targetLoc.getX() + mRadius; dz++) {
+			for (double dy = targetLoc.getY() - mRadius; dy <= targetLoc.getY() + mRadius; dy++) {
+				for (double dz = targetLoc.getZ() - mRadius; dz <= targetLoc.getZ() + mRadius; dz++) {
 					final Block block = blockLoc.set(dx, dy, dz).getBlock();
 					/* Special case for obsidian because of Azacor's arena */
-					if (FastUtils.RANDOM.nextInt(16) > 6 && !(BlockUtils.isMechanicalBlock(block.getType()) && block.getType() != Material.OBSIDIAN)) {
+					if (FastUtils.RANDOM.nextInt(16) > 6 && !(BlockUtils.isMechanicalBlock(block.getType()) || block.getType() == Material.OBSIDIAN)) {
 						changedBlocks.add(block);
 					}
 				}
@@ -83,20 +82,23 @@ public final class SpellChangeFloor extends Spell {
 
 			@Override
 			public void run() {
-				mTicks += 5;
-				if (mTicks < mFloorDuration) {
-					// Particles over the changed blocks
-					for (final Block block : changedBlocks) {
-						final Location loc = block.getLocation().add(0.5f, 1f, 0.5f);
-						new PartialParticle(Particle.DRAGON_BREATH, loc, 4, 0.3, 0.3, 0.3, 0).spawnAsEntityActive(mBoss);
-					}
+				for (final Block block : changedBlocks) {
+					final Location loc = block.getLocation().add(0.5f, 0.5f, 0.5f);
+					new PartialParticle(Particle.DRAGON_BREATH, loc, 4, 0.3, 0.3, 0.3, 0).spawnAsEntityActive(mBoss);
+				}
 
+				/* Set the blocks to the specified material */
+				if (mTicks == Constants.TICKS_PER_SECOND * 3) {
+					changedBlocks.forEach(block -> TemporaryBlockChangeManager.INSTANCE.changeBlock(block, mMaterial, mFloorDuration));
+				}
+
+				/* The particles are intentionally canceled 60 ticks early as an indicator that the floor will change back */
+				if (mTicks >= mFloorDuration) {
+					this.cancel();
 					return;
 				}
 
-				// Set the blocks to the specified material
-				changedBlocks.forEach(block -> TemporaryBlockChangeManager.INSTANCE.changeBlock(block, mMaterial, mFloorDuration));
-				this.cancel();
+				mTicks += 5;
 			}
 		};
 
