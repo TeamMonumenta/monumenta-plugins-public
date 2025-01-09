7d1ff9ca0e4ea793b0a00623cd9612b196068d4d
KickingCoyote <151552160+KickingCoyote@users.noreply.github.com>
2025-01-10 00:40:54 +0100

    Added Particle Param to BossMobRising (#5010)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/MobRisingBoss.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/MobRisingBoss.java
@@ -1,16 +1,16 @@
 package com.playmonumenta.plugins.bosses.bosses;
 
 import com.playmonumenta.plugins.Plugin;
+import com.playmonumenta.plugins.bosses.parameters.BossParam;
 import com.playmonumenta.plugins.bosses.parameters.EntityTargets;
 import com.playmonumenta.plugins.bosses.parameters.LoSPool;
+import com.playmonumenta.plugins.bosses.parameters.ParticlesList;
 import com.playmonumenta.plugins.bosses.parameters.SoundsList;
 import com.playmonumenta.plugins.bosses.spells.Spell;
 import com.playmonumenta.plugins.bosses.spells.SpellBaseSummon;
-import com.playmonumenta.plugins.particle.PartialParticle;
 import com.playmonumenta.plugins.utils.ZoneUtils;
 import java.util.ArrayList;
 import org.bukkit.Location;
-import org.bukkit.Particle;
 import org.bukkit.entity.LivingEntity;
 
 public class MobRisingBoss extends BossAbilityGroup {
@@ -19,26 +19,39 @@ public class MobRisingBoss extends BossAbilityGroup {
 
 	public static class Parameters extends BossParameters {
 
+		@BossParam(help = "not written")
 		public int DETECTION = 100;
+		@BossParam(help = "not written")
 		public int DELAY = 80;
+		@BossParam(help = "not written")
 		public int COOLDOWN = 160;
+		@BossParam(help = "not written")
 		public int DURATION = 80;
+		@BossParam(help = "not written")
 		public double RANGE = 20;
+		@BossParam(help = "not written")
 		public boolean CAN_BE_STOPPED = false;
+		@BossParam(help = "not written")
 		public boolean CAN_MOVE = false;
+		@BossParam(help = "not written")
 		public boolean SINGLE_TARGET = false;
+		@BossParam(help = "not written")
 		public int MOB_NUMBER = 0;
+		@BossParam(help = "not written")
 		public float DEPTH = 2.5f;
-
+		@BossParam(help = "not written")
 		public boolean SELF_GLOWING = true;
+		@BossParam(help = "not written")
 		public boolean SUMMON_GLOWING = true;
 
+		@BossParam(help = "not written")
 		public LoSPool MOB_POOL = LoSPool.EMPTY;
-
+		@BossParam(help = "not written")
 		public EntityTargets TARGETS = EntityTargets.GENERIC_SELF_TARGET;
-
+		@BossParam(help = "not written")
 		public SoundsList SOUNDS = SoundsList.EMPTY;
-
+		@BossParam(help = "not written")
+		public ParticlesList PARTICLES = ParticlesList.fromString("[(SPELL_INSTANT,2,0.5,0.5,0.5,0)]");
 	}
 
 	public MobRisingBoss(Plugin plugin, LivingEntity boss) {
@@ -57,9 +70,7 @@ public class MobRisingBoss extends BossAbilityGroup {
 				p.CAN_BE_STOPPED,
 				p.CAN_MOVE,
 				p.SINGLE_TARGET,
-				() -> {
-					return p.MOB_NUMBER;
-				},
+				() -> p.MOB_NUMBER,
 				() -> {
 					if (ZoneUtils.hasZoneProperty(boss.getLocation(), ZoneUtils.ZoneProperty.NO_SUMMONS)) {
 						return new ArrayList<>();
@@ -81,7 +92,7 @@ public class MobRisingBoss extends BossAbilityGroup {
 						p.SOUNDS.play(bos.getLocation());
 					}
 
-					new PartialParticle(Particle.SPELL_INSTANT, loc, 2, 0.5, 0.5, 0.5, 0).spawnAsEntityActive(boss);
+					p.PARTICLES.spawn(boss, boss.getLocation());
 
 					if (p.SELF_GLOWING && ticks >= p.DURATION) {
 						bos.setGlowing(false);
@@ -92,7 +103,7 @@ public class MobRisingBoss extends BossAbilityGroup {
 					if (p.SUMMON_GLOWING && ticks == 0) {
 						mob.setGlowing(true);
 					}
-					new PartialParticle(Particle.SPELL_INSTANT, loc, 2, 0.5, 0.5, 0.5, 0).spawnAsEntityActive(boss);
+					p.PARTICLES.spawn(boss, boss.getLocation());
 
 					if (p.SUMMON_GLOWING && ticks >= p.DURATION) {
 						mob.setGlowing(false);
