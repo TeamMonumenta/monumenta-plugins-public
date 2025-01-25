78538b8dbababf9cc3d7b7d34187751a124927bc
Jade <128477870+catgirljade@users.noreply.github.com>
2025-01-25 21:01:23 +0800

    Frenzy Base Skill Cosmetic (#5069)
    * Tame the dog
    
    * Meow
    
    * Update FrenzyCS.java

--- plugins/paper/src/main/java/com/playmonumenta/plugins/cosmetics/skills/warrior/FrenzyCS.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/cosmetics/skills/warrior/FrenzyCS.java
@@ -2,8 +2,15 @@ package com.playmonumenta.plugins.cosmetics.skills.warrior;
 
 import com.playmonumenta.plugins.classes.ClassAbility;
 import com.playmonumenta.plugins.cosmetics.skills.CosmeticSkill;
+import com.playmonumenta.plugins.particle.PartialParticle;
+import org.bukkit.Color;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 
 public class FrenzyCS implements CosmeticSkill {
@@ -19,14 +26,32 @@ public class FrenzyCS implements CosmeticSkill {
 	}
 
 	public void frenzyLevelOne(Player player) {
-
+		Location mLoc = player.getLocation();
+		new PartialParticle(Particle.CLOUD, mLoc, 6)
+			.delta(0.4)
+			.extra(0.03)
+			.spawnAsPlayerActive(player);
+		player.playSound(mLoc, Sound.ENTITY_BLAZE_AMBIENT, 0.2f, 1.25f);
+		player.playSound(mLoc, Sound.ENTITY_ILLUSIONER_PREPARE_BLINDNESS, 0.6f, 1.5f);
+		player.playSound(mLoc, Sound.ENTITY_BLAZE_SHOOT, SoundCategory.PLAYERS, 0.4f, 1.7f);
 	}
 
 	public void frenzyLevelTwo(Player player) {
-
+		Location mLoc = player.getLocation();
+		new PartialParticle(Particle.CLOUD, mLoc, 10)
+			.delta(0.4)
+			.extra(0.1)
+			.spawnAsPlayerActive(player);
+		player.playSound(mLoc, Sound.ENTITY_BLAZE_AMBIENT, 0.2f, 2f);
+		player.playSound(mLoc, Sound.ENTITY_ILLUSIONER_PREPARE_BLINDNESS, 0.7f, 1.65f);
+		player.playSound(mLoc, Sound.ENTITY_BLAZE_SHOOT, SoundCategory.PLAYERS, 0.7f, 2f);
+		player.playSound(mLoc, Sound.ENTITY_PLAYER_ATTACK_KNOCKBACK, SoundCategory.PLAYERS, 0.9f, 0.1f);
 	}
 
 	public void frenzyEnhancement(Player player) {
-
+		new PartialParticle(Particle.REDSTONE, player.getLocation().add(new Vector(0, 0.4, 0)), 15)
+			.data(new Particle.DustOptions(Color.RED, 1f))
+			.delta(0.4)
+			.spawnAsPlayerActive(player);
 	}
 }
