798c50e12f9d726f52e1b2e6f198e9e814fdb7cc
Tom Longhurst <clitchedglock94@gmail.com>
2025-03-05 19:52:07 -0700

    Add parameters for sound and particles on boss_rage (#5191)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/RageBoss.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/bosses/RageBoss.java
@@ -1,6 +1,8 @@
 package com.playmonumenta.plugins.bosses.bosses;
 
 import com.playmonumenta.plugins.bosses.parameters.BossParam;
+import com.playmonumenta.plugins.bosses.parameters.ParticlesList;
+import com.playmonumenta.plugins.bosses.parameters.SoundsList;
 import com.playmonumenta.plugins.bosses.spells.Spell;
 import com.playmonumenta.plugins.bosses.spells.SpellRage;
 import org.bukkit.entity.LivingEntity;
@@ -31,6 +33,19 @@ public class RageBoss extends BossAbilityGroup {
 		public boolean CC_IMMUNE_BUFF = false;
 		@BossParam(help = "whether the mob can move while charging the nova or not")
 		public boolean CAN_MOVE = false;
+
+		@BossParam(help = "Sounds played when the boss begins charging the ability.")
+		public SoundsList SOUND_CHARGE = SoundsList.fromString("[(ENTITY_ZOMBIE_VILLAGER_CONVERTED, 1.0, 0.6)]");
+		@BossParam(help = "Particles summoned in the air as the boss is charging the ability.")
+		public ParticlesList PARTICLE_CHARGE = ParticlesList.fromString("[(SPELL_WITCH, 1)]");
+		@BossParam(help = "Particles summoned in a circle around the boss as the ability is charging.")
+		public ParticlesList PARTICLE_CHARGE_CIRCLE = ParticlesList.fromString("[(CRIT_MAGIC, 12, 0.25, 0.25, 0.25, 0.1)]");
+		@BossParam(help = "Sounds played when the boss finishes casting the ability.")
+		public SoundsList SOUND_FINISH = SoundsList.fromString("[(BLOCK_BEACON_ACTIVATE, 1.5, 1.5), (ENTITY_RAVAGER_HURT, 1.5, 0.5), (ENTITY_BLAZE_SHOOT, 1.5, 0.75)]");
+		@BossParam(help = "Particles summoned on the boss when the boss finishes casting the ability.")
+		public ParticlesList PARTICLE_FINISH = ParticlesList.fromString("[(SPELL_WITCH, 15, 0.25, 0.45, 0.25, 1), (VILLAGER_ANGRY, 5, 0.35, 0.5, 0.35, 0)]");
+		@BossParam(help = "Particles summoned that explode in a circle when the boss finishes casting the ability.")
+		public ParticlesList PARTICLE_FINISH_CIRCLE = ParticlesList.fromString("[(SPELL_WITCH, 24, 0.1, 0.1, 0.1, 0.3), (BUBBLE_POP, 48, 0.25, 0.25, 0.25, 0.1)]");
 	}
 
 	public RageBoss(Plugin plugin, LivingEntity boss) {
@@ -38,7 +53,7 @@ public class RageBoss extends BossAbilityGroup {
 
 		Parameters p = BossParameters.getParameters(boss, identityTag, new Parameters());
 
-		Spell spell = new SpellRage(plugin, boss, p.RADIUS, p.DELAY, p.COOLDOWN, p.BUFF_DURATION, p.STRENGTH_POWER, p.SPEED_POWER, p.KBR_POWER, p.CC_IMMUNE_BUFF, p.CAN_MOVE);
+		Spell spell = new SpellRage(plugin, boss, p.RADIUS, p.DELAY, p.COOLDOWN, p.BUFF_DURATION, p.STRENGTH_POWER, p.SPEED_POWER, p.KBR_POWER, p.CC_IMMUNE_BUFF, p.CAN_MOVE, p.SOUND_CHARGE, p.PARTICLE_CHARGE, p.PARTICLE_CHARGE_CIRCLE, p.SOUND_FINISH, p.PARTICLE_FINISH, p.PARTICLE_FINISH_CIRCLE);
 
 		super.constructBoss(spell, detectionRange);
 	}
