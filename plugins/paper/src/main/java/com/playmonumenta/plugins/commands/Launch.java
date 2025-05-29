4b155ea1965f0736b4de84090673ae9e16ef2a22
KickingCoyote <151552160+KickingCoyote@users.noreply.github.com>
2025-05-29 18:12:07 +0200

    Yaw pitch param version of launch command (#5453)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/commands/Launch.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/commands/Launch.java
@@ -1,11 +1,14 @@
 package com.playmonumenta.plugins.commands;
 
+import com.playmonumenta.plugins.utils.FastUtils;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.CommandPermission;
+import dev.jorel.commandapi.arguments.BooleanArgument;
 import dev.jorel.commandapi.arguments.DoubleArgument;
 import dev.jorel.commandapi.arguments.EntitySelectorArgument;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
+
 
 public class Launch extends GenericCommand {
 	public static void register() {
@@ -17,6 +20,11 @@ public class Launch extends GenericCommand {
 		DoubleArgument xArg = new DoubleArgument("x");
 		DoubleArgument yArg = new DoubleArgument("y");
 		DoubleArgument zArg = new DoubleArgument("z");
+		DoubleArgument yawArg = new DoubleArgument("yaw");
+		DoubleArgument pitchArg = new DoubleArgument("pitch");
+		DoubleArgument magnitudeArg = new DoubleArgument("magnitude");
+		BooleanArgument relativeArg = new BooleanArgument("relative yaw");
+
 
 		new CommandAPICommand("launch")
 			.withPermission(perms)
@@ -44,6 +52,30 @@ public class Launch extends GenericCommand {
 				v.setX(args.getByArgument(xArg));
 				v.setY(args.getByArgument(yArg));
 				v.setZ(args.getByArgument(zArg));
+				player.setVelocity(v);
+			})
+			.register();
+
+		new CommandAPICommand("launch")
+			.withPermission(perms)
+			.withArguments(playerArg)
+			.withArguments(yawArg)
+			.withArguments(pitchArg)
+			.withArguments(magnitudeArg)
+			.withArguments(relativeArg)
+			.executes((sender, args) -> {
+				Player player = args.getByArgument(playerArg);
+				double yaw = Math.toRadians(args.getByArgument(yawArg));
+				double pitch = Math.toRadians(args.getByArgument(pitchArg));
+
+				if (args.getByArgument(relativeArg)) {
+					yaw += Math.toRadians(player.getYaw());
+				}
+
+				double m = args.getByArgument(magnitudeArg);
+				Vector v = new Vector(-FastUtils.sin(yaw) * FastUtils.cos(pitch), -FastUtils.sin(pitch), FastUtils.cos(yaw) * FastUtils.cos(pitch));
+				v.normalize().multiply(m);
+
 				player.setVelocity(v);
 			})
 			.register();
