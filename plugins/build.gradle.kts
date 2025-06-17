dc28541973df955dcf617767f550c6757f17955e
Timothy Southwick <NickNackGus@gmail.com>
2025-06-17 12:50:07 -0400

    Use version 3.2 of our gradle plugin so the deploy task actually works

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -1,7 +1,7 @@
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 plugins {
-	id("com.playmonumenta.gradle-config") version "3.1"
+	id("com.playmonumenta.gradle-config") version "3.2"
 }
 
 monumenta {
