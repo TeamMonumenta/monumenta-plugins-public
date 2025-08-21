32f2f10ccc28d498d084078e7cb30757dc35ed8f
honeypint <122322065+honeypint@users.noreply.github.com>
2025-08-20 22:07:58 -0400

    Tweak Decorated Pots to only be fillable in plots shards or the specific zone property (#5632)
    To prevent the common issue of people losing their items in pots.
    Leaving it open in plots shards to allow people to work with them. (Also checks for adventure mode in those shards to prevent guildplots shenanigans, hopefully)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/overrides/DecoratedPotOverride.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/overrides/DecoratedPotOverride.java
@@ -1,6 +1,7 @@
 package com.playmonumenta.plugins.overrides;
 
 import com.playmonumenta.plugins.Plugin;
+import com.playmonumenta.plugins.server.properties.ServerProperties;
 import com.playmonumenta.plugins.utils.ZoneUtils;
 import org.bukkit.GameMode;
 import org.bukkit.block.Block;
@@ -14,7 +15,7 @@ public class DecoratedPotOverride extends BaseOverride {
 	@Override
 	public boolean rightClickBlockInteraction(Plugin plugin, Player player, Action action, @Nullable ItemStack item, Block block, PlayerInteractEvent event) {
 		return player.getGameMode() == GameMode.CREATIVE ||
-			!ZoneUtils.hasZoneProperty(block.getLocation(), ZoneUtils.ZoneProperty.ADVENTURE_MODE) ||
-			ZoneUtils.hasZoneProperty(block.getLocation(), ZoneUtils.ZoneProperty.CAN_DEPOSIT_INTO_POTS);
+			ZoneUtils.hasZoneProperty(block.getLocation(), ZoneUtils.ZoneProperty.CAN_DEPOSIT_INTO_POTS) ||
+			(ServerProperties.getShardName().contains("plots") && !(player.getGameMode() == GameMode.ADVENTURE)); // checks for adventure mode in plots for the case of no-access guildplots
 	}
 }
