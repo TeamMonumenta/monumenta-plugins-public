db2ba1269295054e0febd6922324bba2fee7b783
Chiinox <matt.gobbitt@gmail.com>
2025-09-04 17:59:05 +0100

    HOTFIX: fix enhancement working outside r3 (#5671)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/rogue/MagicDodging.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/abilities/rogue/MagicDodging.java
@@ -9,11 +9,14 @@ import com.playmonumenta.plugins.cosmetics.skills.rogue.DodgingCS;
 import com.playmonumenta.plugins.effects.PercentSpeed;
 import com.playmonumenta.plugins.events.DamageEvent;
 import com.playmonumenta.plugins.itemstats.abilities.CharmManager;
+import com.playmonumenta.plugins.server.properties.ServerProperties;
 import com.playmonumenta.redissync.utils.ScoreboardUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.entity.*;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
 public class MagicDodging extends Ability {
@@ -23,7 +26,7 @@ public class MagicDodging extends Ability {
 			.scoreboardId("Dodging")
 			.shorthandName("Mdg")
 			.simpleDescription("Dodge a magic attack that would otherwise hit you.")
-			.canUse(p -> ScoreboardUtils.getScoreboardValue(p.getName(), "Dodging") > 2)
+			.canUse(p -> ScoreboardUtils.getScoreboardValue(p.getName(), "Dodging") > 2 && ServerProperties.getAbilityEnhancementsEnabled(p))
 			.cooldown((int) (Dodging.DODGING_COOLDOWN_1 * Dodging.MAGIC_DODGING_COOLDOWN_MULTIPLIER), (int) (Dodging.DODGING_COOLDOWN_2 * Dodging.MAGIC_DODGING_COOLDOWN_MULTIPLIER), Dodging.CHARM_COOLDOWN, Dodging.CHARM_MAGIC_DODGING_COOLDOWN);
 
 	private final double mSpeed;
