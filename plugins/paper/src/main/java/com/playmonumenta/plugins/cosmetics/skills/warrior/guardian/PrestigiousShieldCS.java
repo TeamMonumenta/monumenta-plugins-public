9c380a8f9edb41b4117e5eb538d40a47e6f59f98
Jade <128477870+catgirljade@users.noreply.github.com>
2026-01-01 11:46:26 +0800

    Shield Wall as display entities (#5900)

--- plugins/paper/src/main/java/com/playmonumenta/plugins/cosmetics/skills/warrior/guardian/PrestigiousShieldCS.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/cosmetics/skills/warrior/guardian/PrestigiousShieldCS.java
@@ -3,6 +3,7 @@ package com.playmonumenta.plugins.cosmetics.skills.warrior.guardian;
 import com.playmonumenta.plugins.cosmetics.skills.PrestigeCS;
 import com.playmonumenta.plugins.particle.PPCircle;
 import com.playmonumenta.plugins.particle.PartialParticle;
+import com.playmonumenta.plugins.utils.DisplayEntityUtils;
 import com.playmonumenta.plugins.utils.ParticleUtils;
 import java.util.List;
 import org.bukkit.Color;
@@ -13,6 +14,7 @@ import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.Nullable;
 
@@ -94,13 +96,8 @@ public class PrestigiousShieldCS extends ShieldWallCS implements PrestigeCS {
 	}
 
 	@Override
-	public Particle baseParticle() {
-		return Particle.WAX_ON;
-	}
-
-	@Override
-	public Particle replaceParticle(double angleRatio, double heightRatio) {
-		return Math.abs(1 - (Math.abs(angleRatio - 0.5) * 2) - heightRatio) <= 0.2 ? Particle.FIREWORKS_SPARK : baseParticle();
+	public ItemStack shieldItem() {
+		return DisplayEntityUtils.generateRPItem(Material.WHITE_STAINED_GLASS_PANE, "Prestigious Shield");
 	}
 
 	@Override
