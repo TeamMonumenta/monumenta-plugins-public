649bd0788aff88d0391ced52fdf7ac64cb8c9667
Byron Marohn <combustible@live.com>
2026-02-25 18:14:47 -0800

    Remove duplicate pmd/checkstyle config which now is in the MM gradle plugin (#5976)
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- plugins/buildSrc/src/main/kotlin/com.playmonumenta.plugins.java-conventions.gradle.kts
+++ plugins/buildSrc/src/main/kotlin/com.playmonumenta.plugins.java-conventions.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
 	`java-library`
-	`maven-publish`
-	checkstyle
-	pmd
 }
 
 repositories {
@@ -17,25 +14,3 @@ repositories {
 group = "com.playmonumenta.plugins"
 java.sourceCompatibility = JavaVersion.VERSION_17
 java.targetCompatibility = JavaVersion.VERSION_17
-
-tasks.withType<JavaCompile> {
-	options.encoding = "UTF-8"
-	options.compilerArgs.add("-Xlint:unchecked")
-	options.compilerArgs.add("-Xlint:deprecation")
-}
-
-pmd {
-	isConsoleOutput = true
-	toolVersion = "7.11.0"
-	ruleSets = listOf("$rootDir/pmd-ruleset.xml")
-	setIgnoreFailures(true)
-}
-
-checkstyle {
-	toolVersion = "10.17.0"
-}
-
-tasks.withType<Checkstyle>().configureEach {
-	minHeapSize = "200m"
-	maxHeapSize = "1g"
-}
